97c596d917c1a0c0c5f7eb63a7b7043050c74c16
Carlos Felix <carlos.felix@agile6.com>
2020-09-10 12:02:06 -0400

    OY2-65 Changes to run in offline mode

---
+++ services/app-api/README.md
@@ -0,0 +1,14 @@
+## Running Locally
+Pre-requisites: You need to have configured Node, npm, serverless globaly.
+
+Install the serverless Dynamo DB offline package by running the following command:
+
+```
+sls dynamodb install
+```
+
+Run the software locally by running the following command:
+
+```
+sls offline start
+```
--- services/app-api/libs/dynamodb-lib.js
+++ services/app-api/libs/dynamodb-lib.js
@@ -1,6 +1,15 @@
 import AWS from "aws-sdk";
 
-const client = new AWS.DynamoDB.DocumentClient();
+//Setup the database connection for localhost when running in offline mode.
+let dbOptions = {};
+if(process.env.IS_OFFLINE) {
+  dbOptions = {
+    region: 'localhost',
+    endpoint: 'http://localhost:8000'
+  };
+}
+
+const client = new AWS.DynamoDB.DocumentClient(dbOptions);
 
 export default {
   get   : (params) => client.get(params).promise(),
