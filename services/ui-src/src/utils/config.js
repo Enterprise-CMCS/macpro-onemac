15c9e359ad7d4f75776e23be0923bd9c98857b96
Carlos Felix <63804190+carlosfelix2@users.noreply.github.com>
2020-09-22 15:32:15 -0400

    OY2-20 added optional attachments (#26)
    Updated uploads component styling to matched agreed styling from 9/17/2020 meeting.
    Added optional uploads.
    Updated jsdoc with more information.
    Updated email template with upload titles.
    Minor fixes to build tools.
    Made the failure error message shown to the user a bit less techie.
    Made some changes to get to an offline run with a local s3, but further refactoring will be needed. Right now we can start the offline s3 bucket, but the UI code needs to be changed to use it, which is complicated due to the amplify library not allowing different endpoints.

--- services/ui-src/src/utils/config.js
+++ services/ui-src/src/utils/config.js
@@ -1,5 +1,5 @@
 export default {
-    MAX_ATTACHMENT_SIZE: 5000000,
+    MAX_ATTACHMENT_SIZE_MB: 50,
     s3: {
         REGION: window._env_.S3_ATTACHMENTS_BUCKET_REGION,
         BUCKET: window._env_.S3_ATTACHMENTS_BUCKET_NAME
@@ -13,5 +13,6 @@ export default {
         USER_POOL_ID: window._env_.COGNITO_USER_POOL_ID,
         APP_CLIENT_ID: window._env_.COGNITO_CLIENT_ID,
         IDENTITY_POOL_ID: window._env_.COGNITO_IDENTITY_POOL_ID
-    }
+    },
+    IS_OFFLINE: window._env_.IS_OFFLINE
 };
