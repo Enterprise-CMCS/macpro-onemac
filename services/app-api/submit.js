87083a478d99ba74c538a86cc63a75ec56435a57
Carlos Felix <carlos.felix@agile6.com>
2020-10-09 10:32:13 -0400

    OY2-108 Added log to backend

--- services/app-api/submit.js
+++ services/app-api/submit.js
@@ -53,9 +53,9 @@ export const main = handler(async (event, context) => {
     }
 
     console.log("Successfully submitted amendment:", data);
-
     context.succeed(data);
   } else {
+    console.log("Invalid submission with missing fields.", data);
     context.fail("Invalid submission with missing fields.");
   }
 });
