2d5f48bdfd8ba560ebe7bc823d6c65e31d440906
Carlos Felix <carlos.felix@agile6.com>
2020-10-19 10:10:25 -0400

    OY2-569 Fixed field not updating

--- services/ui-src/src/changeRequest/SpaRai.js
+++ services/ui-src/src/changeRequest/SpaRai.js
@@ -7,27 +7,27 @@ import { TextField } from "@cmsgov/design-system";
 import { onError } from "../libs/errorLib";
 import { useHistory } from "react-router-dom";
 import { CHANGE_REQUEST_TYPES } from "./changeRequestTypes";
-import ChangeRequestDataApi from "../libs/ChangeRequestDataApi";
+import ChangeRequestDataApi from "../utils/ChangeRequestDataApi";
 import { ROUTES } from "../Routes";
 
 export default function SpaRai() {
   // The attachment list
-  const requiredUploads = ["RAI Response"];
+  const requiredUploads = ['RAI Response'];
   const optionalUploads = [
-    "CMS Form 179",
-    "SPA Pages",
-    "Cover Letter",
-    "Existing state plan pages",
-    "Tribal Consultation",
-    "Public Notice",
-    "Standard Funding Questions (SFQs)",
-    "Other",
+    'CMS Form 179',
+    'SPA Pages',
+    'Cover Letter',
+    'Existing state plan pages',
+    'Tribal Consultation',
+    'Public Notice',
+    'Standard Funding Questions (SFQs)',
+    'Other',
   ];
 
   // The form field names
   const FIELD_NAMES = {
-    TRANSMITTAL_NUMBER: "transmittalNumber",
-    SUMMARY: "summary",
+    TRANSMITTAL_NUMBER: 'transmittalNumber',
+    SUMMARY: 'summary',
   };
 
   // True when the required attachments have been selected.
@@ -38,7 +38,7 @@ export default function SpaRai() {
   const [isLoading, setIsLoading] = useState(false);
 
   // True if the form is read only.
-  const [isReadOnly, setReadOnly] = useState(true);
+  const [isReadOnly, setReadOnly] = useState(false);
 
   // The browser history, so we can redirect to the home page
   const history = useHistory();
@@ -52,8 +52,8 @@ export default function SpaRai() {
   // The record we are using for the form.
   const [changeRequest, setChangeRequest] = useState({
     type: CHANGE_REQUEST_TYPES.SPA_RAI,
-    summary: "",
-    transmittalNumber: "", //This is needed to be able to control the field
+    summary: '',
+    transmittalNumber: '', //This is needed to be able to control the field
   });
 
   useEffect(() => {
@@ -62,7 +62,7 @@ export default function SpaRai() {
      */
     async function fetchChangeRequest() {
       if (!id) {
-        throw new Error("ID not specified for fetchChangeRequest");
+        throw new Error('ID not specified for fetchChangeRequest');
       }
 
       try {
@@ -71,7 +71,7 @@ export default function SpaRai() {
         setReadOnly(true);
       } catch (e) {
         onError(
-          "There was an error fetching your change request.  Please try again"
+          'There was an error fetching your change request.  Please try again'
         );
       }
     }
@@ -106,19 +106,10 @@ export default function SpaRai() {
    * @param {Object} event the event
    */
   async function handleInputChange(event) {
+    console.log("event")
     if (event && event.target) {
-      let updatedRecord = changeRequest;
-      switch (event.target.name) {
-        case FIELD_NAMES.TRANSMITTAL_NUMBER:
-          updatedRecord.transmittalNumber = event.target.value;
-          break;
-        case FIELD_NAMES.SUMMARY:
-          updatedRecord.summary = event.target.value;
-          break;
-        default:
-          //nothing to do here
-          break;
-      }
+      let updatedRecord = {...changeRequest}; // You need a new object to be able to update the state
+      updatedRecord[event.target.name] = event.target.value;
       setChangeRequest(updatedRecord);
       // Need to use the current data to validate and not what is in the state
       validateForm(updatedRecord, areUploadsReady);
@@ -139,28 +130,28 @@ export default function SpaRai() {
       await ChangeRequestDataApi.submit(changeRequest, uploadedList);
       history.push(ROUTES.DASHBOARD);
     } catch (error) {
-      onError("There was an error submitting your request.  Please try again.");
-      console.log(error);
+      onError('There was an error submitting your request.  Please try again.');
+      console.log('There was an error submitting a request.', error);
       setIsLoading(false);
     }
   }
 
   // Render the component.
   return (
-    <div className="form-container">
+    <div className='form-container'>
       <form onSubmit={handleSubmit}>
         <h3>SPA Details</h3>
         <label htmlFor={FIELD_NAMES.TRANSMITTAL_NUMBER}>
-          SPA ID<span className="required-mark">*</span>
+          SPA ID<span className='required-mark'>*</span>
         </label>
         <p>Enter the State Plan Amendment transmittal number for this RAI</p>
         <input
-          type="text"
-          required
+          type='text' 
+          required={!isReadOnly}
           id={FIELD_NAMES.TRANSMITTAL_NUMBER}
           name={FIELD_NAMES.TRANSMITTAL_NUMBER}
           onChange={handleInputChange}
-          readOnly={isReadOnly}
+          disabled={isReadOnly}
           value={changeRequest.transmittalNumber}
         ></input>
         <h3>Attachments</h3>
@@ -178,16 +169,16 @@ export default function SpaRai() {
         <br />
         <TextField
           name={FIELD_NAMES.SUMMARY}
-          label="Summary"
-          fieldClassName="summary-field"
+          label='Summary'
+          fieldClassName='summary-field'
           multiline
           onChange={handleInputChange}
         ></TextField>
         <LoaderButton
           block
-          type="submit"
-          bsSize="large"
-          bsStyle="primary"
+          type='submit'
+          bsSize='large'
+          bsStyle='primary'
           isLoading={isLoading}
           disabled={!isFormReady}
         >
