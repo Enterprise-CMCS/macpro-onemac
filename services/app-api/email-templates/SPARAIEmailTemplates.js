73050bc8662bab87caeefa26795756fe6a42652b
Kristin Grue <70273267+kristin-at-theta@users.noreply.github.com>
2020-10-22 11:12:16 -0400

    OY2 104 new waiver email to state user (#41)
    Updated the Readme to add the s3 installation details that I somehow missed.
    Installing the s3 local stuff also updated the serverless.yml and the package.json files
    Pulled AWS SES sending details/functions into email-lib.js
    Created email-templates and put the known email templates in as classes
    created getEmailTemplates to assign the appropriate class based on the data.type from the POST
    Added the Waiver Email to the State User - includes a 90-day date (which is actually 91 days out?)
    Added the Waiver Email to the CMS User
    Fixed “Is Urgent” in the SPA emails - it was always sending “No”

---
+++ services/app-api/email-templates/SPARAIEmailTemplates.js
@@ -0,0 +1,75 @@
+import { getLinksHtml, get90thDay } from "./email-util";
+
+/**
+ * SPA RAI submission specific email generation functions.
+ * @class
+ */
+class SPARAIEmailTemplates {
+
+    /**
+    * returns or creates the singleton instance.
+    * @constructor
+     */
+    constructor(){
+
+        if(! SPARAIEmailTemplates.instance){
+            SPARAIEmailTemplates.instance = this;
+        }
+
+        return SPARAIEmailTemplates.instance;
+    }
+
+    /**
+     * SPA RAI submission email to CMS details wrapped in generic function name.
+     * @param {Object} data from the form submission.
+     * @returns {Object} email parameters in generic format.
+     */
+    getCMSEmail(data) {
+        const cmsEmail = {};
+
+        cmsEmail.ToAddresses = [process.env.reviewerEmail];
+        cmsEmail.Subject = "New SPA RAI " + data.transmittalNumber + " submitted";
+        cmsEmail.HTML = `
+    <p>The SPA and Waiver Submission Form received a Waiver Submission:</p>
+    <p><b>State or territory</b>: ${data.territory}
+    <br><b>Name</b>: ${data.user.firstName} ${data.user.lastName}
+    <br><b>Email Address</b>: ${data.user.email}
+    <br><b>ID</b>: ${data.transmittalNumber}
+    <p>Files:</p>
+    <p>${getLinksHtml(data.uploads)}</p>
+    <p>If these files seem suspicious, do not open them, and instead forward this email to <a href="mailto:CMS_IT_Service_Desk@cms.hhs.gov">CMS_IT_Service_Desk@cms.hhs.gov</a>.</p>
+    <p>Thank you!</p>
+    `;
+        return cmsEmail;
+    }
+
+    /**
+     * SPA RAI submission confimation email to State User wrapped in
+     * generic function name.
+     * @param {Object} data from the form submission.
+     * @returns {Object} email parameters in generic format.
+     */
+    getStateEmail(data) {
+        const stateEmail = {};
+
+        stateEmail.ToAddresses = [data.user.email];
+        stateEmail.Subject = "Your Waiver " + data.waiverNumber + " has been submitted to CMS";
+        stateEmail.HTML = `
+    <p>This response confirms the receipt of your 1915(b) waiver/1915(c) Appendix K Amendment:</p>
+    <p><b>State or territory</b>: ${data.territory}
+    <br><b>Waiver #</b>: ${data.waiverNumber}
+    <br><b>Submitter name</b>: ${data.user.firstName} ${data.user.lastName}
+    <br><b>Submitter email</b>: ${data.user.email}</p>
+    <p>Files:</p>
+    <p>${getLinksHtml(data.uploads)}</p>
+    <p>You can expect a formal response to your submission to be issued within 90 days, 
+    on ${get90thDay(data.createdDate)}. If you have any questions, please contact spa@cms.hhs.gov or your state lead.</p>
+    `;
+        return stateEmail;
+    }
+
+}
+
+const instance = new SPARAIEmailTemplates();
+Object.freeze(instance);
+export default instance;
