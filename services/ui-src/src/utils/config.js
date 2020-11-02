10009d9dafdf49cde2cbf87f653250e5e5b98d42
z <zlewis.guest@kr.af.mil>
2020-11-02 15:22:57 -0600

    WIP Static Okta Test

--- services/ui-src/src/utils/config.js
+++ services/ui-src/src/utils/config.js
@@ -1,20 +1,20 @@
 export default {
-    MAX_ATTACHMENT_SIZE: 5000000,
-    s3: {
-        REGION: window._env_.S3_ATTACHMENTS_BUCKET_REGION,
-        BUCKET: window._env_.S3_ATTACHMENTS_BUCKET_NAME
-    },
-    apiGateway: {
-        REGION: window._env_.API_REGION,
-        URL: window._env_.API_URL
-    },
-    cognito: {
-        REGION: window._env_.COGNITO_REGION,
-        USER_POOL_ID: window._env_.COGNITO_USER_POOL_ID,
-        APP_CLIENT_ID: window._env_.COGNITO_USER_POOL_CLIENT_ID,
-        APP_CLIENT_DOMAIN: window._env_.COGNITO_USER_POOL_CLIENT_DOMAIN,
-        IDENTITY_POOL_ID: window._env_.COGNITO_IDENTITY_POOL_ID,
-        REDIRECT_SIGNIN: window._env_.COGNITO_REDIRECT_SIGNIN,
-        REDIRECT_SIGNOUT: window._env_.COGNITO_REDIRECT_SIGNOUT
-    }
+  MAX_ATTACHMENT_SIZE_MB: 50,
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "uploads-oy2-62-user-entered-spa-attachmentsbucket-19mow7z65ezh3"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "http://localhost:3001"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_dlVhoTlYZ",
+    APP_CLIENT_ID: "4rpjmbt5css7h0mqvs2lm577f2",
+    IDENTITY_POOL_ID: "us-east-1:eb8619ee-7a34-438c-87ca-4df26051649d",
+    APP_CLIENT_DOMAIN: "demookta8c722025-8c722025-dev.auth.us-east-1.amazoncognito.com",
+    REDIRECT_SIGNIN: "http://localhost:3000",
+    REDIRECT_SIGNOUT: "http://localhost:3000"
+  }
 };
