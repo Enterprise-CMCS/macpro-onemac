745b93abe52a5180c3a83deeb3b1b2e986708d61
Carlos Felix <carlos.felix@agile6.com>
2020-11-05 10:32:41 -0500

    carlos-oy220 script change test

--- services/output.sh
+++ services/output.sh
@@ -4,12 +4,12 @@ set -e
 help='This script is run with the format  ./output.sh <target service name> <serverless output variable name> <stage name (optional, default dev)>'
 example='ex.  ./output.sh ui CloudFrontEndpointUrl'
 
-: ${1?ERROR: 'You must specify the target service.'
-$help
-$example}
-: ${2?ERROR: "You must specify the variable you want to fetch from serverless' output"
-$help
-$example}
+#: ${1?ERROR: 'You must specify the target service.'
+#$help
+#$example}
+#: ${2?ERROR: "You must specify the variable you want to fetch from serverless' output"
+##$help
+#$example}
 
 service=${1}
 output=${2}
