b87d29a0226cb75b50c5acd6a48157289604fa25
Carlos Felix <carlos.felix@agile6.com>
2020-11-09 10:15:41 -0500

    OY2-220 Hide dev login page

--- services/ui-src/src/containers/DevLogin.js
+++ services/ui-src/src/containers/DevLogin.js
@@ -4,67 +4,71 @@ import { FormGroup, FormControl, ControlLabel } from "react-bootstrap";
 import LoaderButton from "../components/LoaderButton";
 import { useAppContext } from "../libs/contextLib";
 import { useFormFields } from "../libs/hooksLib";
-import AlertBar from "../components/AlertBar"
-import {ALERTS_MSG} from "../libs/alert-messages";
+import AlertBar from "../components/AlertBar";
+import { ALERTS_MSG } from "../libs/alert-messages";
+import config from "../utils/config";
 import "./DevLogin.css";
 
 export default function DevLogin() {
-    const { userHasAuthenticated } = useAppContext();
-    const [isLoading, setIsLoading] = useState(false);
-    const [fields, handleFieldChange] = useFormFields({
-        email: "",
-        password: ""
-    });
+  const { userHasAuthenticated } = useAppContext();
+  const [isLoading, setIsLoading] = useState(false);
+  const [fields, handleFieldChange] = useFormFields({
+    email: "",
+    password: "",
+  });
+  const showDevLogin = config.ALLOW_DEV_LOGIN === "true";
 
-    function validateForm() {
-        return fields.email.length > 0 && fields.password.length > 0;
+  function validateForm() {
+    return fields.email.length > 0 && fields.password.length > 0;
+  }
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    setIsLoading(true);
+
+    try {
+      await Auth.signIn(fields.email, fields.password);
+      userHasAuthenticated(true);
+    } catch (error) {
+      console.log("Error while logging in.", error);
+      AlertBar.alert(ALERTS_MSG.LOGIN_ERROR);
+      setIsLoading(false);
     }
+  }
 
-    async function handleSubmit(event) {
-        event.preventDefault();
-
-        setIsLoading(true);
-
-        try {
-            await Auth.signIn(fields.email, fields.password);
-            userHasAuthenticated(true);
-        } catch (error) {
-            console.log("Error while logging in.", error);
-            AlertBar.alert(ALERTS_MSG.LOGIN_ERROR);
-            setIsLoading(false);
-        }
-    }
-
-    return (
-        <div className="Login">
-            <form onSubmit={handleSubmit}>
-                <FormGroup controlId="email" bsSize="large">
-                    <ControlLabel>Email</ControlLabel>
-                    <FormControl
-                        autoFocus
-                        type="email"
-                        value={fields.email}
-                        onChange={handleFieldChange}
-                    />
-                </FormGroup>
-                <FormGroup controlId="password" bsSize="large">
-                    <ControlLabel>Password</ControlLabel>
-                    <FormControl
-                        type="password"
-                        value={fields.password}
-                        onChange={handleFieldChange}
-                    />
-                </FormGroup>
-                <LoaderButton
-                    block
-                    type="submit"
-                    bsSize="large"
-                    isLoading={isLoading}
-                    disabled={!validateForm()}
-                >
-                    Login
-                </LoaderButton>
-            </form>
-        </div>
-    );
+  return (
+    <div className="Login">
+      {showDevLogin && (
+        <form onSubmit={handleSubmit}>
+          <FormGroup controlId="email" bsSize="large">
+            <ControlLabel>Email</ControlLabel>
+            <FormControl
+              autoFocus
+              type="email"
+              value={fields.email}
+              onChange={handleFieldChange}
+            />
+          </FormGroup>
+          <FormGroup controlId="password" bsSize="large">
+            <ControlLabel>Password</ControlLabel>
+            <FormControl
+              type="password"
+              value={fields.password}
+              onChange={handleFieldChange}
+            />
+          </FormGroup>
+          <LoaderButton
+            block
+            type="submit"
+            bsSize="large"
+            isLoading={isLoading}
+            disabled={!validateForm()}
+          >
+            Login
+          </LoaderButton>
+        </form>
+      )}
+    </div>
+  );
 }
