34f0d4a201d315f40aa8cfd7990115d7fce0fa19
Zach Lewis <72087844+zachclarity@users.noreply.github.com>
2020-11-09 10:06:56 -0600

    OY2-220 OKTA login integration (#65)
    Use OKTA for normal user login.
    Kept Cognito login for development work. This uses build generated users that include any emails from people that have checked in code as seen by the branch (run the command "git log --pretty=format:'%ae' | grep -v github.com | sort -u"). It also adds user1@cms.hhs.local, user2@cms.hhs.local, user3@cms.hhs.local, user4@cms.hhs.local, user5@cms.hhs.local. All users use the password "Passw0rd!". Note these users and login method are NOT available in master or production.
    Logging in takes you to the dashboard.
    Disabled Cognito user signup to make sure no one can sign up.
    The old login page was repurposed to be a dev login page.
    A new environment variable called ALLOW_DEV_LOGIN is provided by the build (deploy.sh) and used by the UI to display the dev login button.

--- services/ui-src/src/components/Header.js
+++ services/ui-src/src/components/Header.js
@@ -1,88 +1,115 @@
-import React from "react"
-import { Link, useHistory } from "react-router-dom"
-import { Dropdown, MenuItem } from "react-bootstrap"
-import { Auth } from "aws-amplify"
-import { Button } from '@cmsgov/design-system'
+import React from "react";
+import { Link, useHistory } from "react-router-dom";
+import { Auth } from "aws-amplify";
+import { Button, FormLabel } from "@cmsgov/design-system";
+import { ROUTES } from "../Routes";
+import medicaidLogo from "../images/medicaidLogo.png";
+import flagIcon from "../images/flagIcon.png";
+import config from "../utils/config";
+import "./Header.scss";
 
-import { ROUTES } from "../Routes"
-import medicaidLogo from "../images/medicaidLogo.png"
-import flagIcon from "../images/flagIcon.png"
-import "./Header.scss"
+/**
+ * Get the sign in URL used with OKTA.
+ * @returns the signin URL
+ */
+function getSignInUrl() {
+  const authConfig = Auth.configure();
+  const { domain, redirectSignIn, responseType } = authConfig.oauth;
+  const clientId = authConfig.userPoolWebClientId;
+  const url = `https://${domain}/oauth2/authorize?identity_provider=Okta&redirect_uri=${redirectSignIn}&response_type=${responseType}&client_id=${clientId}`;
+  return url;
+}
+
+/**
+ * Logout the user.
+ */
+function logout() {
+  const authConfig = Auth.configure();
+  Auth.signOut();
+  window.location.href = authConfig.oauth.redirectSignOut;
+}
 
 /**
  * Component containing header
  * @param {Object} props - component properties
  */
 function Header(props) {
-    const history = useHistory()
-
-    /**
-     * Renders a branding bar
-     */
-    function renderBrandingBar() {
-        return (
-            <div tabIndex="0">
-                <div className="usaBanner">
-                    <img src={flagIcon} alt="united states flag" />
-                    An offical website of the United States government
-                </div>
-                <div className="headerLogo">
-                    <a href="https://www.medicaid.gov/">
-                        <img src={medicaidLogo} alt="Medicaid.gov-Keeping America Healthy" />
-                    </a>
-                </div>
-            </div>
-        )
-    }
-
-    /**
-     * Renders account related buttons based on whether the user is authenticated or not authenticated
-     */
-    function renderAccountButtons() {
-        if (props.isAuthenticated) {
-            return (
-                <Dropdown id="account info">
-                    <Dropdown.Toggle className="accountDropdown">
-                        Account
-                    </Dropdown.Toggle>
-                    <Dropdown.Menu>
-                        <MenuItem href={ROUTES.PROFILE}>Profile</MenuItem>
-                        <MenuItem href={ROUTES.LOGIN} onClick={() => Auth.signOut()}>Logout</MenuItem>
-                    </Dropdown.Menu>
-                </Dropdown>
-            )
-        } else {
-            return(
-                <div className="navElements">
-                    <Button onClick={() => history.push(ROUTES.SIGNUP)} inversed>Sign Up</Button>
-                    <Button onClick={() => history.push(ROUTES.LOGIN)} inversed>Login</Button>
-                </div>
-            )
-        }
-    }
-
-    /**
-     * Renders a navigation bar
-     */
-    function renderNavBar() {
-        return (
-            <div className="navbarContainer">
-                <div className="navElements">
-                    <Link to={ROUTES.HOME}>About</Link>
-                    <Link to={ROUTES.DASHBOARD}>Dashboard</Link>
-                    <Link to={ROUTES.FAQ}>FAQ</Link>
-                </div>
-                {renderAccountButtons()}
-            </div>
-        )
-    }
-
+  const history = useHistory();
+  /**
+   * Renders a branding bar
+   */
+  function renderBrandingBar() {
     return (
-        <div className="headerContainer">
-            {renderBrandingBar()}
-            {renderNavBar()}
+      <div tabIndex="0">
+        <div className="usaBanner">
+          <img src={flagIcon} alt="united states flag" />
+          An offical website of the United States government
         </div>
-    )
+        <div className="headerLogo">
+          <a href="https://www.medicaid.gov/">
+            <img
+              src={medicaidLogo}
+              alt="Medicaid.gov-Keeping America Healthy"
+            />
+          </a>
+        </div>
+      </div>
+    );
+  }
+
+  /**
+   * Renders account related buttons based on whether the user is authenticated or not authenticated
+   */
+  function renderAccountButtons() {
+    let showDevLogin = config.ALLOW_DEV_LOGIN === "true";
+    if (props.isAuthenticated) {
+      return (
+        <div className="navElements">
+          <FormLabel inversed>
+            <Button onClick={() => logout()} inversed>
+              Logout
+            </Button>
+          </FormLabel>
+        </div>
+      );
+    } else {
+      return (
+        <div className="navElements">
+          <Button onClick={() => (window.location = getSignInUrl())} inversed>
+            Login
+          </Button>
+          {showDevLogin && (
+            <Button onClick={() => history.push(ROUTES.DEVLOGIN)} inversed>
+              Development Login
+            </Button>
+          )}
+        </div>
+      );
+    }
+  }
+
+  /**
+   * Renders a navigation bar
+   */
+  function renderNavBar() {
+    return (
+      <div className="navbarContainer">
+        <div className="navElements">
+          <Link to={ROUTES.HOME}>About</Link>
+          <Link to={ROUTES.DASHBOARD}>Dashboard</Link>
+          <Link to={ROUTES.FAQ}>FAQ</Link>
+        </div>
+        {renderAccountButtons()}
+      </div>
+    );
+  }
+
+  return (
+    <div className="headerContainer">
+      {renderBrandingBar()}
+      {renderNavBar()}
+    </div>
+  );
 }
 
-export default Header
+export default Header;
