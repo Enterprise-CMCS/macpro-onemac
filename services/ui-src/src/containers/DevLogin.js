9416338957b52d40895323e30d43b8147be28a49
zach lewis <zlewis@clarityinnovates.com>
2020-11-19 08:38:22 -0600

    Add auto testing ids for demo #3

--- services/ui-src/src/containers/DevLogin.js
+++ services/ui-src/src/containers/DevLogin.js
@@ -62,7 +62,7 @@ export default function DevLogin() {
               onChange={handleFieldChange}
             />
           </FormGroup>
-          <LoaderButton id={"devLoginBtn"}
+          <LoaderButton id={"loginDevUserBtn"}
             block
             type="submit"
             bsSize="large"
