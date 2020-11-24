abda89b0812ae7023f85cad77c1432c55a3094e3
Zach Lewis <72087844+zachclarity@users.noreply.github.com>
2020-11-24 10:43:30 -0600

    Oy2 279 field validation SPA ID and Waiver ID (#81)
    * oy279 WIP: initial Add Formik Check
    
    * WIP: oy279 - Test Basic Validation
    
    * shorter branch name serverless fix
    
    * WIP: oy279 add simple format check.
    
    * WIP: Format check Test
    
    * oy179-valid transmittal check
    
    * oy2-279 ready for pull testing
    
    * oy2-279 ready for pull testing minor typo
    
    * Refactor Alert Messages
    
    * Refactor Regex routine
    
    * oy279 - Change style to use design.cms styles.
    
    * oy279 - Refactor common regex strings
    
    * oy279 Refactor #3
    
    * oy279 Waiver Reformat code
    
    * oy279 Waiver Reformat code #4
    
    * oy279 Waiver Reformat code #5
    
    * oy279 update spa id format
    
    * oy279 update spa id format #2
    
    * Add auto testing ids for demo
    
    * Add auto testing ids for demo #2
    
    * Add auto testing ids for demo #3
    
    * Add auto testing ids for demo #4
    
    * Remove State Code Validation
    
    * Remove State Code Validation #2
    
    * Remove State Code Validation #3
    
    * auto test dashboard id
    
    * auto test dashboard id #2
    
    * auto test dashboard id #3
    
    * styling changes
    
    * Fix RAI error
    
    * Fix WaverExtension remove State Code
    
    * more style changes
    
    * Add Submit Validation reset on Error changed Ids
    
    * Resolve SetForm Ready Logic
    
    * Resolve SetForm Ready Logic #2 false check
    
    * Resolve SetForm Ready Logic #3 false check
    
    * Resolve SetForm Ready Logic #4 false check RAI
    
    * Resolve disable logic for RAI template
    
    * OY2-279 submit button on key press
    
    Co-authored-by: Carlos Felix <carlos.felix@agile6.com>

--- services/ui-src/src/utils/form-utils.js
+++ services/ui-src/src/utils/form-utils.js
@@ -11,16 +11,74 @@ import React from "react";
  * @param {Array} optionsList array of objects with label and value for each option
  */
 export function renderOptionsList(optionsList) {
-  if(!optionsList || !Array.isArray(optionsList)) {
-      throw new Error("Options list must be an array of items.");
-  }
+    if (!optionsList || !Array.isArray(optionsList)) {
+        throw new Error("Options list must be an array of items.");
+    }
 
-  let retval = optionsList.map((item, i) => {
-    return (
-      <option key={i} value={item.value}>
-        {item.label}
-      </option>
-    );
-  });
-  return retval;
+    let retval = optionsList.map((item, i) => {
+        return (
+            <option key={i} value={item.value}>
+                {item.label}
+            </option>
+        );
+    });
+    return retval;
 }
+
+/**
+ * Validate SPA Id Transmittal Number Format
+ * @return
+ *
+ */
+export function validateSpaId(spaId) {
+
+    let errorMessage = undefined
+    let SpaTransmittalNumberFormatErrorMessage = "SS-YY-NNNN or SS-YY-NNNN-xxxx"
+    let RegexFormatString = "(^[A-Z]{2}-[0-9]{2}-[0-9]{4}-[a-zA-Z0-9]{4}$)|(^[A-Z]{2}-[0-9]{2}-[0-9]{4}$)"
+
+    if (!spaId) {
+        errorMessage = 'Transmittal Number Required !';
+    } else if (!isValidFieldFormat(spaId, RegexFormatString)) {
+        errorMessage = `The SPA ID must be in the format of ${SpaTransmittalNumberFormatErrorMessage} !`;
+    }
+    return errorMessage
+}
+
+/**
+ * Validate Wavier Id Transmittal Number Format
+ * @return
+ */
+export function validateWavierId(wavierId) {
+
+    let errorMessage = undefined
+    let RegexFormatString = "(^[A-Z]{2}[.][0-9]{2}[.]R[0-9]{2}[.]M[0-9]{2}$)|(^[A-Z]{2}[.][0-9]{4}[.]R[0-9]{2}[.][0-9]{2}$)"
+
+    let WaiverTransmittalNumberFormatErrorMessage = "SS.##.R##.M## or SS.####.R##.##"
+
+    if (!wavierId) {
+        errorMessage = 'Transmittal Number Required !';
+    } else if (!isValidFieldFormat(wavierId, RegexFormatString)) {
+        errorMessage = `The Waiver ID must be in the format of ${WaiverTransmittalNumberFormatErrorMessage} !`;
+    }
+    return errorMessage
+}
+
+/**
+ * Validate Field
+ * @param {value} Transmittal Number Field Entered on Change Event.
+ */
+export function isValidFieldFormat(fieldValue, regexFormatString) {
+
+    let fieldValidationRegEx = new RegExp(regexFormatString)
+    let result = false;
+
+    if (fieldValue && fieldValue.match(fieldValidationRegEx)) {
+        result = true
+    } else {
+        result = false
+    }
+
+    return result;
+
+};
+
