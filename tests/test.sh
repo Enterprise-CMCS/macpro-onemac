ca82d6f447fa6e7dd618e52d8fdd866719808d8f
Ishmeal Baawuah <ibaawuah@Ishmeals-MBP.fios-router.home>
2020-12-04 10:17:19 -0500

    fixing test.sh

--- tests/test.sh
+++ tests/test.sh
@@ -2,7 +2,6 @@
 
 set -e
 
-
 install_deps() {
   if [ "$CI" == "true" ]; then # If we're in a CI system
     if [ ! -d "node_modules" ]; then # If we don't have any node_modules (CircleCI cache miss scenario), run yarn install --frozen-lockfile.  Otherwise, we're all set, do nothing.
@@ -12,7 +11,6 @@ install_deps() {
     npm install
   fi
 }
-
 
 pushd nightwatch
 install_deps
