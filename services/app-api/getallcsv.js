9d6e24e6c74e38fb127a0a2ad72ca96393a0103c
zach lewis <zlewis@clarityinnovates.com>
2020-12-04 06:37:47 -0600

    initial setup query test #1

---
+++ services/app-api/getallcsv.js
@@ -0,0 +1,29 @@
+import handler from "./libs/handler-lib";
+import dynamoDb from "./libs/dynamodb-lib";
+
+export const main = handler(async (event, context) => {
+  // If this invokation is a prewarm, do nothing and return.
+  if(event.source == "serverless-plugin-warmup" ) {
+    console.log("Warmed up!");
+    return null;
+  }
+
+  const params = {
+    TableName: process.env.tableName
+  };
+
+  const result = await dynamoDb.get(params);
+  if ( ! result.Item) {
+    throw new Error("Item not found.");
+  } else {
+    //Clear out the s3 URLs.  The UI will generate a temp one.
+    if(result.Item.uploads) {
+      result.Item.uploads.forEach((upload) => {
+        upload.url = null;
+      });
+    }
+  }
+  console.log('Sending back result:', JSON.stringify(result,null,2));
+  // Return the retrieved item
+  return result.Item;
+});
