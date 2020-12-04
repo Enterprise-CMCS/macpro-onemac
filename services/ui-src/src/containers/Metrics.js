69fce6e8994eb42fc454a11e30bb4ae68f5b42af
zach lewis <zlewis@clarityinnovates.com>
2020-12-04 11:09:30 -0600

    initial setup query test #5

--- services/ui-src/src/containers/Metrics.js
+++ services/ui-src/src/containers/Metrics.js
@@ -2,11 +2,16 @@ import React, { useEffect } from "react";
 import AlertBar from "../components/AlertBar";
 import { ALERTS_MSG } from "../libs/alert-messages";
 import ChangeRequestDataApi from "../utils/ChangeRequestDataApi";
+import {useAppContext} from "../libs/contextLib";
 
 export default function Metrics() {
+  const { isAuthenticated } = useAppContext();
+
   useEffect(() => {
     async function onLoad() {
-
+      if (!isAuthenticated) {
+        return;
+      }
       try {
         const metrics = await ChangeRequestDataApi.listAll();
         console.log(metrics);
@@ -17,7 +22,7 @@ export default function Metrics() {
     }
 
     onLoad();
-  });
+  },[isAuthenticated]);
   return (
     <div className="form-container">
      Downloaded Metrics
