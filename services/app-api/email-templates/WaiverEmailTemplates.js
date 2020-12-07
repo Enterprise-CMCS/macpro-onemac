09bc9caff58e39345501d3f27fa07d9956ff1e16
Kristin_Grue <k.grue@theta-llc.com>
2020-12-07 14:40:33 -0500

    submittedAt does not exist before CMS email sent

--- services/app-api/email-templates/WaiverEmailTemplates.js
+++ services/app-api/email-templates/WaiverEmailTemplates.js
@@ -24,7 +24,7 @@ class WaiverEmailTemplates {
             <br><b>Waiver #</b>: ${data.transmittalNumber}
             <br><b>Action Type</b>: ${data.actionType}
             <br><b>Waiver Authority</b>: ${data.waiverAuthority}
-            <br><b>90th day deadline</b>: ${get90thDay(data.submittedAt)}
+            <br><b>90th day deadline</b>: ${get90thDay(data.createdAt)}
         </p>
         <p>
             <b>Summary</b>:
@@ -59,7 +59,7 @@ class WaiverEmailTemplates {
             <br><b>Waiver #</b>: ${data.transmittalNumber}
             <br><b>Submitter name</b>: ${data.user.firstName} ${data.user.lastName}
             <br><b>Submitter email</b>: ${data.user.email}
-            <br><b>90th day deadline</b>: ${get90thDay(data.submittedAt)}
+            <br><b>90th day deadline</b>: ${get90thDay(data.createdAt)}
         </p>
         <p>
             <b>Summary</b>:
@@ -68,7 +68,7 @@ class WaiverEmailTemplates {
         <p>
             <br>
             This response confirms the receipt of your Waiver request or your response to a Waiver Request for Additional Information (RAI)). 
-            You can expect a formal response to your submittal to be issued within 90 days, before ${get90thDay(data.submittedAt)}.
+            You can expect a formal response to your submittal to be issued within 90 days, before ${get90thDay(data.createdAt)}.
         </p>
         <p>
             This mailbox is for the submittal of Section 1915(b) and 1915(c) non-web-based Waivers and responses to Requests for Additional 
