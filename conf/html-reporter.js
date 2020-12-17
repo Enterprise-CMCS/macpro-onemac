7b427669b6b00e5a97d529bbdcfa01d9bba9551f
Gabriel Lockett <glockett@collabralink.com>
2020-12-17 15:23:41 -0500

    Updates: Added a pause of 1000ms(1 second) during regression to prevent race conditions between the webdriver child process and the NightWatch runner parent process. This is a known and documented webdriver event.
    Verified tests run with all commands and runs headless by default.
    
    - Removed test_runner.bat and test_runner.sh as they are no longer required.

--- conf/html-reporter.js
+++
@@ -1,8 +0,0 @@
-const HtmlReporter = require('nightwatch-html-reporter');
-const reporter = new HtmlReporter({
-	openBrowser: true,
-	reportsDirectory: __dirname + '/reports'
-});
-module.exports = {
-	reporter: reporter.fn
-};
