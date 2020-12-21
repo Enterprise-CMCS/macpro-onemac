5873d12dacb24f4c540dd3ae5c146398d0d74899
zach lewis <zlewis@clarityinnovates.com>
2020-12-21 11:50:08 -0600

    fix array list #2

--- services/app-api/libs/metrics-lib.js
+++ services/app-api/libs/metrics-lib.js
@@ -1,35 +1,37 @@
-
 /**
  * Transforms DynamoDB Scan into Metrics .
  * @param {Object} json dynamodb scan data
  */
+
 export function generateMetrics(scanData) {
 
     let submissions = 0;
     let stateMap = new Map();
     let userMap = new Map();
     let submissionTypeMap = new Map();
-    scanData.filter(function(item){
+    scanData.filter(function (item) {
         submissions = submissions + 1;
-        let stateCode = item.transmittalNumber.substring(0,2);
-        stateMap[stateCode] = 1 + (stateMap[stateCode] === undefined ? 0: stateMap[stateCode]);
-        userMap[item.user.email] = 1 + (userMap[item.user.email] === undefined ? 0: userMap[item.user.email] );
-        submissionTypeMap[item.type] = 1 + (submissionTypeMap[item.type] === undefined ? 0: submissionTypeMap[item.type]);
+        let stateCode = item.transmittalNumber.substring(0, 2);
+        stateMap[stateCode] = 1 + (stateMap[stateCode] === undefined ? 0 : stateMap[stateCode]);
+        userMap[item.user.email] = 1 + (userMap[item.user.email] === undefined ? 0 : userMap[item.user.email]);
+        submissionTypeMap[item.type] = 1 + (submissionTypeMap[item.type] === undefined ? 0 : submissionTypeMap[item.type]);
         return true;
     });
 
-    var result =  {};
+    var result = {};
     result["totalSubmissions"] = submissions;
     result["totalUniqueUserSubmissions"] = Object.keys(userMap).length;
     var stateTotals = [];
-    Object.keys(stateMap).forEach(function(key) {
-        stateTotals.push({ key : stateMap[key] });
+    Object.keys(stateMap).forEach(function (stateCode) {
+        let temp = "{"  + stateCode + ":"  + stateMap[stateCode] +  "}";
+        stateTotals.push(temp);
     });
     result["stateTotals"] = stateTotals;
 
     var typeTotals = [];
-    Object.keys(submissionTypeMap).forEach(function(key) {
-        typeTotals.push({  key : submissionTypeMap[key] });
+    Object.keys(submissionTypeMap).forEach(function (type) {
+        let temp = "{" + type + ":" + submissionTypeMap[type] + "}";
+        typeTotals.push(temp);
     });
     result["submissionTypeTotals"] = typeTotals;
 
