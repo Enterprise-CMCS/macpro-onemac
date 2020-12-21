5b71183e745e00a1ae9fe99e8bd4ba295caf9515
zach lewis <zlewis@clarityinnovates.com>
2020-12-21 09:35:30 -0600

    metrics test #2

--- services/app-api/libs/metrics-lib.js
+++ services/app-api/libs/metrics-lib.js
@@ -5,29 +5,29 @@
  */
 export function generateMetrics(scanData) {
 
-    let submissions = 0
-    let stateMap = new Map()
-    let userMap = new Map()
+    let submissions = 0;
+    let stateMap = new Map();
+    let userMap = new Map();
     let submissionTypeMap = new Map();
     scanData.filter(function(item){
         submissions = submissions + 1;
-        let stateCode = item.transmittalNumber.substring(0,2)
-        stateMap[stateCode] = 1 + (stateMap[stateCode] === undefined ? 0: stateMap[stateCode])
-        userMap[item.user.email] = 1 + (userMap[item.user.email] === undefined ? 0: userMap[item.user.email] )
-        submissionTypeMap[item.type] = 1 + (submissionTypeMap[item.type] === undefined ? 0: submissionTypeMap[item.type])
+        let stateCode = item.transmittalNumber.substring(0,2);
+        stateMap[stateCode] = 1 + (stateMap[stateCode] === undefined ? 0: stateMap[stateCode]);
+        userMap[item.user.email] = 1 + (userMap[item.user.email] === undefined ? 0: userMap[item.user.email] );
+        submissionTypeMap[item.type] = 1 + (submissionTypeMap[item.type] === undefined ? 0: submissionTypeMap[item.type]);
         return true;
     });
 
-    var result =  {}
-    result["totalSubmissiona"] = submissions
-    result["totalUniqueUserSubmissions"] = Object.keys(userMap).length
+    var result =  {};
+    result["totalSubmissiona"] = submissions;
+    result["totalUniqueUserSubmissions"] = Object.keys(userMap).length;
     Object.keys(stateMap).forEach(function(key) {
-        result["totalForTerritory-" +  key] = stateMap[key]
-    })
+        result["totalForTerritory-" +  key] = stateMap[key];
+    });
 
     Object.keys(submissionTypeMap).forEach(function(key) {
-        result["totalForSubmissionType-(" +  key + ")"] = submissionTypeMap[key]
-    })
+        result["totalForSubmissionType-(" +  key + ")"] = submissionTypeMap[key];
+    });
 
-    return result
+    return result;
 }
