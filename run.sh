3c588bfbe85e35d0685bf63db1f52db79ee8cd89
Ishmeal Baawuah <ibaawuah@ip-10-232-36-218.ec2.internal>
2021-01-25 13:49:48 -0500

    echoeing branch name

--- run.sh
+++ run.sh
@@ -8,5 +8,6 @@ if [ "$branch_name" ==  "develop" ]  || [ "$branch_name" = "master" ]
 then
   ./test.sh
 else
+  echo "This branch is not develop or master, it is $branch_name branch"
   ./test.sh --dev
 fi
