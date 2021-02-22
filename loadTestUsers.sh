272571055514187239340d1031cb98082e68e2bb
zach lewis <zlewis@clarityinnovates.com>
2021-02-22 14:06:35 -0600

    fix state format

--- loadTestUsers.sh
+++ loadTestUsers.sh
@@ -13,14 +13,14 @@ testStates=(AL AL VA VA AL VA)
 #
 # Check if Table already Loaded, Do not load a second time
 #
-lineCount=`aws dynamodb scan --table-name $userTable | wc -l`
+lineCount=1 #`aws dynamodb scan --table-name $userTable | wc -l`
 if [ $lineCount -gt -6 ]
 then
   i=0
   for user in `cat $userList`
   do
-    echo 'DEBUG: aws dynamodb put-item --table-name '$userTable' --item {  "userId": { "S": "'${user}'" }, "status": { "S": "'${testUserStatuses[$i]}'" }, "userRole": { "S": "'${testUserRoles[$i]}'" },  "stateCodes": { "SS": [ "MI","'${testStates[i]}'" ] }  }'
-    echo '{  "userId": { "S": "'${user}'" }, "status": { "S": "'${testUserStatuses[$i]}'" }, "userRole": { "S": "'${testUserRoles[$i]}'" },  "stateCodes": { "SS": [ "MI","'${testStates[i]}'" ] }  }' > user.json
+    echo 'DEBUG: aws dynamodb put-item --table-name $userTable --item  {  "userId": { "S": "'${user}'" }, "userRole": { "S": "'${testUserRoles[$i]}'" },"stateCodes": { "L": [ { "M": { "MI":{ "S":"'${testUserStatuses[$i]}'" } } }, { "M": { "'${testStates[i]}'":{ "S":"'${testUserStatuses[$i]}'" } } }] } } '
+    echo '{  "userId": { "S": "'${user}'" }, "userRole": { "S": "'${testUserRoles[$i]}'" },"stateCodes": { "L": [ { "M": { "MI":{ "S":"'${testUserStatuses[$i]}'" } } }, { "M": { "'${testStates[i]}'":{ "S":"'${testUserStatuses[$i]}'" } } }] } } ' > user.json
     aws dynamodb put-item --table-name $userTable --item file://user.json
     i=`expr $i + 1`
     if [ $i -gt 5 ]
