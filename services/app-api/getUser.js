d3b289a20db1c080a4984310ed558dbcc6a8292d
zach lewis <zlewis@clarityinnovates.com>
2021-02-25 14:20:07 -0600

    amy PR requests cloudformation

--- services/app-api/getUser.js
+++ services/app-api/getUser.js
@@ -14,7 +14,7 @@ export const main = handler(async (event, context) => {
     // 'Key' defines the partition key and sort key of the item to be retrieved
     // - 'userId': Identity Pool identity id of the authenticated user
     Key: {
-      userId: event.requestContext.identity.cognitoIdentityId,
+      id: event.requestContext.identity.cognitoIdentityId,
     },
   };
 
