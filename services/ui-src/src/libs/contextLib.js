f3e21ec14920a99c5be6bed490b69ddc4a59f56d
Hasan Farook <hasanfar@gmail.com>
2021-02-26 13:40:09 -0500

    removed logintypecontext - not needed

--- services/ui-src/src/libs/contextLib.js
+++ services/ui-src/src/libs/contextLib.js
@@ -1,7 +1,6 @@
 import { useContext, createContext } from "react";
 
 export const AppContext = createContext(null);
-export const LoginTypeContext = createContext(null);
 
 export function useAppContext() {
     return useContext(AppContext);
