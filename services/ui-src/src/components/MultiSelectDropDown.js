37ef3500b6461957ec4a52311576ee114ca7ad46
zach lewis <zlewis@clarityinnovates.com>
2021-03-04 15:01:47 -0600

    css closer #1

--- services/ui-src/src/components/MultiSelectDropDown.js
+++ services/ui-src/src/components/MultiSelectDropDown.js
@@ -18,19 +18,20 @@ const MultiSelectDropDown = ({options, header, subheader, cancelFn, submitFn}) =
     }
 
     return (
-        <div className="multi-select-dropdown-container">
-            <div>
+        <>
+            <div className="multi-select-dropdown-container">
                 <p className="multi-select-title">{header} <svg width="19" height="18" viewBox="0 0 19 18" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M11.06 6L12 6.94L2.92 16H2V15.08L11.06 6ZM14.66 0C14.41 0 14.15 0.1 13.96 0.29L12.13 2.12L15.88 5.87L17.71 4.04C18.1 3.65 18.1 3 17.71 2.63L15.37 0.29C15.17 0.09 14.92 0 14.66 0ZM11.06 3.19L0 14.25V18H3.75L14.81 6.94L11.06 3.19Z" fill="#0071BC"/>
                 </svg></p>
                 <p className="multi-select-header">{subheader}</p>
-                <div>
+                <div >
                     <Select id="MultiSelect" className="fa fa-search"
                             placeholder="Select ..."
-                            dropdownHeight="190px"
+                            dropdownHeight="185px"
                             clearable="false"
                             searchable="true"
                             searchBy="label"
+
                             multi="true"
                             keepOpen="true"
                             onChange={handleOnchange}
@@ -38,17 +39,11 @@ const MultiSelectDropDown = ({options, header, subheader, cancelFn, submitFn}) =
                     />
 
                 </div>
-            </div>
-            <div className="ReactDropdownButtons">
-                <button onClick={cancelFn} className="reactDropdownCancelButton" type="button">
-                    Cancel
-                </button>
-                &nbsp; &nbsp;
-                <button onClick={submitFn(value)} className="reactDropdownSubmitButton" type="button">
+                <button onClick={submitFn(value)} className="multi-select-dropdown-submit-button" type="button">
                     Submit
                 </button>
             </div>
-        </div>
+        </>
     )
 }
 
