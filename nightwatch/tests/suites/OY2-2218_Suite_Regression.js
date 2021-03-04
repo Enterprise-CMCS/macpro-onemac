36450800fbb7767bb28596de1bce837a417acbad
Kristin_Grue <k.grue@theta-llc.com>
2021-03-04 13:46:37 -0500

    updated id formats and reinstated testing

--- nightwatch/tests/suites/OY2-2218_Suite_Regression.js
+++ nightwatch/tests/suites/OY2-2218_Suite_Regression.js
@@ -75,7 +75,7 @@ module.exports = {
         const renewWaiver = require('../cases/OY2-5866_Submit_Renewal_Waiver');
         steps.forEach(step => renewWaiver[step](browser));
     },
-/*
+
     "Submit a 'Respond to 1915(b) Waiver RAI'": function (browser, steps = [
         "Click on Respond to 1915(b) Waiver RAI",
         "Enter Waiver Number",
@@ -86,7 +86,7 @@ module.exports = {
         const waiverRAI = require('../cases/OY2-2218_Test_SPA_Respond_To_1915b_Waiver_RAI');
         steps.forEach(step => waiverRAI[step](browser));
     },
-
+/*
     "Submit a Temporary Request Extension": function (browser, steps = [
         "Click on 'Request Temporary Extension form - 1915(b) and 1915(c)'",
         "Enter Waiver Number",
