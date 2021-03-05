bdc2a4b9e74cdcb141d1231bd688ff64cb087caf
zach lewis <zlewis@clarityinnovates.com>
2021-03-05 16:02:13 -0600

    design Style Changes

--- services/ui-src/src/containers/ComponentPage.js
+++ services/ui-src/src/containers/ComponentPage.js
@@ -3,15 +3,15 @@ import MultiSelectDropDown from "../components/MultiSelectDropDown";
 
 export default function ComponentPage() {
 
-    const  options  = [
-        { label:  'Maryland', value:  'MD'  },
-        { label:  'Alabama', value:  'AL'  },
-        { label:  'Texas', value:  'TX'  },
-        { label:  'Oregon', value:  'OR'  },
-        { label:  'Option 1', value:  'option_1'  },
-        { label:  'Option 2', value:  'option_2'  },
-        { label:  'Option 3', value:  'option_3'  },
-        { label:  'Option 4', value:  'option_4'  },
+    const options = [
+        {label: 'Maryland', value: 'MD'},
+        {label: 'Alabama', value: 'AL'},
+        {label: 'Texas', value: 'TX'},
+        {label: 'Oregon', value: 'OR'},
+        {label: 'Option 1', value: 'option_1'},
+        {label: 'Option 2', value: 'option_2'},
+        {label: 'Option 3', value: 'option_3'},
+        {label: 'Option 4', value: 'option_4'},
     ]
 
     const [value, setValue] = useState([])
@@ -20,16 +20,20 @@ export default function ComponentPage() {
         console.log("handledCancel Called ... Value: " + value)
     }
 
-    return (
-        <>
+    return (<>
             <div id="title_bar" className="page-title-bar"><h1>Component Page</h1></div>
-            <div><MultiSelectDropDown
-                options={options}
-                header="Passed In Value when used."
-                subheader="Passed In Value when used."
-                submitFn={val => setValue(val)}
-                cancelFn={handleCancel}
-            /></div>
+
+            <div>
+                <div style={{position: "relative"}}>
+                    <div><h2>MultiSelect Component</h2></div>
+                    <MultiSelectDropDown
+                        options={options}
+                        header="Passed In Value when used."
+                        subheader="Passed In Value when used."
+                        submitFn={val => setValue(val)}
+                        cancelFn={handleCancel}/>
+                </div>
+            </div>
         </>
     );
 }
