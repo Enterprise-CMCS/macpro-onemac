da0d89024a9c51d5d53e14b4ffa20d4013036e3f
Hasan Farook <hasanfar@gmail.com>
2021-03-09 12:56:28 -0500

    created cardButton component

--- services/ui-src/src/containers/ComponentPage.js
+++ services/ui-src/src/containers/ComponentPage.js
@@ -1,9 +1,13 @@
 import React from "react";
+import CardButton from "../components/cardButton";
 
 export default function ComponentPage() {
     return (
         <>
             <div id="title_bar" className="page-title-bar"><h1>Component Page</h1></div>
+            <div>
+                <CardButton type='spa'></CardButton>
+            </div>
         </>
     );
 }
