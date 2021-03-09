ff78ffc6911b535affdb55317b6d4f7a6f4c5f12
Hasan Farook <hasanfar@gmail.com>
2021-03-09 12:52:36 -0500

    added svgs and created util function + Json

---
+++ services/ui-src/src/utils/cardButtonTypes.js
@@ -0,0 +1,11 @@
+import * as cardButtonTypes from './cardButtonTypeData.json';
+
+const getCardButtonPropsByType = (type) => {
+    const result = cardButtonTypes.default[type];
+    if (!result) {
+        throw new Error("Invalid CardButton type value.");
+    }
+    return result;
+}
+
+export default getCardButtonPropsByType;
