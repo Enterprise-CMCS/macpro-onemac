8dc440db8a788d436692cd4c94322c123a6579a3
George Kaplan <gkaplan@fearless.tech>
2021-03-10 15:04:44 -0500

    add empty list component to component page, remove multiselect temporarily

--- services/ui-src/src/containers/ComponentPage.js
+++ services/ui-src/src/containers/ComponentPage.js
@@ -1,29 +1,43 @@
 import React, { useState } from "react";
 import CardButton from "../components/cardButton";
-import {territoryList} from "../libs/territoryLib";
+import { EmptyList } from "../components/EmptyList.js";
+import { territoryList } from "../libs/territoryLib";
 import MultiSelectDropDown from "../components/MultiSelectDropDown";
 export default function ComponentPage() {
-    const options = territoryList;
-    const [value, setValue] = useState([])
+  const options = territoryList;
+  const [value, setValue] = useState([]);
 
-    const handleCancel = () => {
-        console.log("handledCancel Called ... Value: " + value)
-    }
+  const handleCancel = () => {
+    console.log("handledCancel Called ... Value: " + value);
+  };
 
-    return (<>
-        <div id="title_bar" className="page-title-bar"><h1>Component Page</h1></div>
-        <section class="ds-l-container preview__grid">
-            <div class="ds-l-row">
-                <div className="ds-l-col--6">
-                    <div><h2>SPA Card Button</h2></div>
-                    <CardButton type='spa'></CardButton>
-                </div>
-                <div className="ds-l-col--6">
-                    <div><h2>Waiver Card Button</h2></div>
-                    <CardButton type='waiver'></CardButton>
-                </div>
+  return (
+    <>
+      <div id="title_bar" className="page-title-bar">
+        <h1>Component Page</h1>
+      </div>
+      <section className="ds-l-container preview__grid">
+        <div className="ds-l-row">
+          <div className="ds-l-col--6">
+            <div>
+              <h2>SPA Card Button</h2>
             </div>
-            </section>
+            <CardButton type="spa"></CardButton>
+          </div>
+          <div className="ds-l-col--6">
+            <div>
+              <h2>Waiver Card Button</h2>
+            </div>
+            <CardButton type="waiver"></CardButton>
+          </div>
+        </div>
+        <div className="ds-1-row">
+          <div className="ds-1-col--6">
+            <h3>Empty List placeholder</h3>
+            <EmptyList message="This is an example of an empty list." />
+          </div>
+        </div>
+      </section>
     </>
-    );
+  );
 }
