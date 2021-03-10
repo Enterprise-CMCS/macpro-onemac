73281294e54dd50d6372c89dcb730a187a3974ac
Kristin_Grue <k.grue@theta-llc.com>
2021-03-10 15:09:22 -0500

    small change to build environment

--- services/ui-src/src/App.js
+++ services/ui-src/src/App.js
@@ -3,19 +3,30 @@ import Routes from "./Routes";
 import Header from "./components/Header";
 import { AppContext } from "./libs/contextLib";
 import { Auth } from "aws-amplify";
+import ChangeRequestDataApi from "./utils/ChangeRequestDataApi";
 
 function App() {
   const [isAuthenticating, setIsAuthenticating] = useState(true);
   const [isAuthenticated, userHasAuthenticated] = useState(false);
   const [isLoggedInAsDeveloper, developerLoggedIn] = useState(false);
+  const [userProfile, setUserProfile] = useState();
 
   useEffect(() => {
     onLoad();
   }, []);
 
   async function onLoad() {
+    let userAuthenticationStatus = false;
+    let isDev = false;
+    let userProfile;
+
     try {
-      await Auth.currentAuthenticatedUser();
+      const authUser = await Auth.currentAuthenticatedUser();
+      userAuthenticationStatus = true;
+      const userData = await ChangeRequestDataApi.userProfile(authUser.signInUserSession.idToken.payload.email);
+      console.log("authUser is :", authUser);
+      console.log("userData is :", userData);
+      if (userData.isDev === "true") isDev=true;
       userHasAuthenticated(true);
     } catch (error) {
       if (error !== "not authenticated") {
@@ -25,15 +36,18 @@ function App() {
         );
       }
     }
+    setUserProfile(userProfile);
+    userHasAuthenticated(true);
     setIsAuthenticating(false);
+    developerLoggedIn(isDev);
   }
 
   return (
     !isAuthenticating && (
       <div>
         <AppContext.Provider
-          value={{ isLoggedInAsDeveloper, developerLoggedIn, isAuthenticated, userHasAuthenticated }}>
-          <Header isAuthenticated={isAuthenticated} />
+          value={{ isLoggedInAsDeveloper, developerLoggedIn, isAuthenticated, userHasAuthenticated, userProfile, setUserProfile }}>
+          <Header />
           <Routes />
         </AppContext.Provider>
       </div>
