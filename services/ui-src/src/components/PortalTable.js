30209d426ada77fdbc6a78fdd7b0141456b974be
zach lewis <zlewis@clarityinnovates.com>
2021-03-15 12:29:59 -0500

    Add demo table popup

--- services/ui-src/src/components/PortalTable.js
+++ services/ui-src/src/components/PortalTable.js
@@ -1,11 +1,19 @@
 import * as React from 'react';
 import { DataGrid } from '@material-ui/data-grid';
 
+function handleCellClick(row)
+{
+    if (row.row) {
+        console.log("Clicked:" + JSON.stringify(row.row) + " -- " + row.column);
+    } else {
+        console.log("ignore")
+    }
+}
 
 export default function PortalTable({rows,columns}) {
     return (
         <div style={{ height: 400, width: '100%' }}>
-            <DataGrid rows={rows} columns={columns} pageSize={5} checkboxSelection />
+            <DataGrid rows={rows} columns={columns} pageSize={5} disableColumnMenu="false" disableSelectionOnClick="false" />
         </div>
     );
 }
--- services/ui-src/src/containers/ComponentPage.js
+++ services/ui-src/src/containers/ComponentPage.js
@@ -1,8 +1,10 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 import CardButton from "../components/cardButton";
 import {territoryList} from "../libs/territoryLib";
-import MultiSelectDropDown from "../components/MultiSelectDropDown";
 import PortalTable from "../components/PortalTable";
+import TablePopover from "../components/PopOver";
+import PopOver from "../components/PopOver";
+
 export default function ComponentPage() {
     const options = territoryList;
     const [value, setValue] = useState([])
@@ -11,59 +13,130 @@ export default function ComponentPage() {
         console.log("handledCancel Called ... Value: " + value)
     }
     const columns = [
-        { field: 'id', headerName: 'State User', width: 70 },
-        { field: 'firstName', headerName: 'Email', width: 130 },
-        { field: 'lastName', headerName: 'State', width: 130 },
-        { field: 'lastName', headerName: 'Status', width: 130 },
         {
-            field: 'Personnel Action',
-            headerName: 'Age',
-            type: 'number',
-            width: 90,
+            field: 'stateUser',
+            headerName: 'State User',
+            width: 150,
+            renderCell: (params) => (
+                <>
+                    <div className="portalTable">
+                        {params.value}
+                    </div>
+                </>
+            ),
         },
         {
-            field: 'fullName',
-            headerName: 'Full name',
-            description: 'This column has a value getter and is not sortable.',
-            sortable: false,
-            width: 160,
-            valueGetter: (params) =>
-                `${params.getValue('firstName') || ''} ${params.getValue('lastName') || ''}`,
+            field: 'stateUser',
+            headerName: 'State User',
+            width: 150,
+            renderCell: (params) => (
+                <>
+                    <div className="portalTable">
+                        {params.value}
+                    </div>
+                </>
+            ),
+        },
+        {
+            field: 'email',
+            headerName: 'Email Address',
+            width: 150,
+            renderCell: (params) => (
+                <>
+                    <div className="portalTable">
+                        {params.value}
+                    </div>
+                </>
+            ),
+        },
+        {
+            field: 'stateCode',
+            headerName: 'State',
+            width: 100,
+            renderCell: (params) => (
+                <>
+                    <div className="portalTable">
+                        {params.value}
+                    </div>
+                </>
+            ),
+        },
+        {
+            field: 'status',
+            headerName: 'Status',
+            width: 100,
+            renderCell: (params) => (
+                <>
+                    <div className="portalTable">
+                        {params.value}
+                    </div>
+                </>
+            ),
+        },
+        {
+            field: 'id',
+            headerName: 'Personnel Actions',
+            width: 150,
+            disableColumnSelector: false,
+            renderCell: (params) => (
+                <>
+                    <PopOver selectedRow={params.value}/>
+                </>
+            ),
         },
     ];
 
     const rows = [
-        { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-        { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-        { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-        { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-        { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-        { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-        { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-        { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-        { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+        {
+            stateUser: "Elliot Alderson",
+            email: "elliot.alderson@state.state.gov",
+            stateCode: "MD",
+            status: "pending",
+            id: 1,
+        },
+        {
+            stateUser: "Angela Moss",
+            email: "angela.moss@state.state.gov",
+            stateCode: "NY",
+            status: "active",
+            id: 2,
+        },
+        {
+            stateUser: "Tyrell Wellick",
+            email: "tyrell.wellick@state.state.gov",
+            stateCode: "MD",
+            status: "revoked",
+            id: 3,
+        },
+        {
+            stateUser: "Philip Price",
+            email: "philip.price@state.state.gov",
+            stateCode: "NM",
+            status: "active",
+            id: 4,
+        },
     ];
 
 
     return (<>
-        <div id="title_bar" className="page-title-bar"><h1>Component Page</h1></div>
-        <section class="ds-l-container preview__grid">
-            <div class="ds-l-row">
-                <div className="ds-l-col--6">
-                    <div><h2>SPA Card Button</h2></div>
-                    <CardButton type='spa'></CardButton>
+            <div id="title_bar" className="page-title-bar"><h1>Component Page</h1></div>
+            <section class="ds-l-container preview__grid">
+                <div class="ds-l-row">
+                    <div className="ds-l-col--6">
+                        <div><h2>SPA Card Button</h2></div>
+                        <CardButton type='spa'></CardButton>
+                    </div>
+                    <div className="ds-l-col--6">
+                        <div><h2>Waiver Card Button</h2></div>
+                        <CardButton type='waiver'></CardButton>
+                    </div>
                 </div>
-                <div className="ds-l-col--6">
-                    <div><h2>Waiver Card Button</h2></div>
-                    <CardButton type='waiver'></CardButton>
+                <div className="ds-l-row">
+                    <div className="ds-l-col--12">
+                        <PortalTable className="portalTable" rows={rows} columns={columns}/>
+                    </div>
                 </div>
-            </div>
-            <div className="ds-l-row">
-                <div className="ds-l-lg-col--12">
-                <PortalTable rows={rows} columns={columns}/>
-                </div>
-            </div>
             </section>
-    </>
+        </>
     );
 }
