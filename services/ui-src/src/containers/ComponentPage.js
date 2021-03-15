23dc2f5b33a89d7d2cb532fca4075584bd022139
Kristin_Grue <k.grue@theta-llc.com>
2021-03-15 14:44:19 -0400

    cleaned up ComponentPage

--- services/ui-src/src/containers/ComponentPage.js
+++ services/ui-src/src/containers/ComponentPage.js
@@ -1,16 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
 import CardButton from "../components/cardButton";
-import {territoryList} from "../libs/territoryLib";
-import MultiSelectDropDown from "../components/MultiSelectDropDown";
+
 export default function ComponentPage() {
-    const options = territoryList;
-    const [value, setValue] = useState([])
 
-    const handleCancel = () => {
-        console.log("handledCancel Called ... Value: " + value)
-    }
-
-    return (<>
+return (<>
         <div id="title_bar" className="page-title-bar"><h1>Component Page</h1></div>
         <section class="ds-l-container preview__grid">
             <div class="ds-l-row">
