f598b3047b1851e2c2485667c8d07545b7df4c43
Amy DiPasquale <adipasquale@fearless.tech>
2021-03-16 11:44:08 -0400

    OY2-5196 add missing reference to test file

--- nightwatch/tests/cases/OY2_5196_Test_View_User_Profile.js
+++ nightwatch/tests/cases/OY2_5196_Test_View_User_Profile.js
@@ -1,13 +1,13 @@
 const login = require("./OY2-1494_Test_SPA_Login");
 
 const timeout = 1000;
-let spa;
+let pageObjects;
 
 module.exports = {
   before: function (browser) {
     login.before(browser);
     login["Login to SPA and Waiver Dashboard"](browser);
-    spa = browser.page.spaBasePage();
+    pageObjects = browser.page.spaBasePage();
     browser.pause(timeout * 3);
   },
 
@@ -25,8 +25,10 @@ module.exports = {
       pageTitle: "Account Management"
     }
   ) {
-    spa.click(testData.myAccountLink).waitForElementPresent(testData.manageAccountLink);
-    spa.click(testData.myAccountLink).waitForElementPresent("body");
+    pageObjects = browser.page.spaBasePage();
+
+    pageObjects.click(testData.myAccountLink).waitForElementPresent(testData.manageAccountLink);
+    pageObjects.click(testData.manageAccountLink).waitForElementPresent("body");
     browser.assert.urlContains(testData.subUrl);
     browser.verify.containsText('h1', testData.pageTitle);
   },
