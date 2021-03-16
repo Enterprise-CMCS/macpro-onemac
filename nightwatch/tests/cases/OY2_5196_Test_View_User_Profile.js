228fcbe7252ce0cf95dd93ca859c349a197b37f4
Amy DiPasquale <adipasquale@fearless.tech>
2021-03-16 09:58:51 -0400

    OY2-5296 add test to view profile page

---
+++ nightwatch/tests/cases/OY2_5196_Test_View_User_Profile.js
@@ -0,0 +1,33 @@
+const login = require("./OY2-1494_Test_SPA_Login");
+
+const timeout = 1000;
+let pageObjects;
+
+module.exports = {
+  before: function (browser) {
+    login.before(browser);
+    login["Login to SPA and Waiver Dashboard"](browser);
+    pageObjects = browser.page.spaBasePage();
+    browser.pause(timeout * 3);
+  },
+
+  after: function (browser) {
+    login["Logout of SPA and Waiver Dashboard"](browser);
+    login.after(browser);
+  },
+
+  "Navigate to the Manage Account page": function (
+    browser,
+    testData = {
+      myAccountLink: "@myAccountLink",
+      manageAccountLink: "@manageAccountLink",
+      subUrl: "/profile",
+      pageTitle: "Account Management"
+    }
+  ) {
+    pageObjects.click(testData.myAccountLink).waitForElementPresent(testData.manageAccountLink);
+    pageObjects.click(testData.myAccountLink).waitForElementPresent("body");
+    browser.assert.urlContains(testData.subUrl);
+    browser.verify.containsText('h1', testData.pageTitle);
+  },
+};
