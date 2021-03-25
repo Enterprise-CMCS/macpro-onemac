f96f42700041b5397e8825b728e3f5a66f402c66
zach lewis <zlewis@clarityinnovates.com>
2021-03-25 12:52:04 -0500

    refactor null checks added

--- services/ui-src/src/DynamicRoutes.js
+++ services/ui-src/src/DynamicRoutes.js
@@ -15,49 +15,53 @@ import {ROLES} from "cmscommonlib";
 export default function DynamicRoutes() {
     const {userProfile} = useAppContext();
 
-    const userType = userProfile.userData.type
-
-    if (userType === ROLES.STATE_USER) {
-        return (<>
-            <AuthenticatedRoute exact path={ROUTES.DASHBOARD}>
-                <Dashboard/>
-            </AuthenticatedRoute>
-            <AuthenticatedRoute path={`${ROUTES.SPA}/:id?`}>
-                <Spa/>
-            </AuthenticatedRoute>
-            <AuthenticatedRoute exact path={`${ROUTES.WAIVER}/:id?`}>
-                <Waiver/>
-            </AuthenticatedRoute>
-            <AuthenticatedRoute path={`${ROUTES.SPA_RAI}/:id?`}>
-                <SpaRai/>
-            </AuthenticatedRoute>
-            <AuthenticatedRoute path={`${ROUTES.WAIVER_RAI}/:id?`}>
-                <WaiverRai/>
-            </AuthenticatedRoute>
-            <AuthenticatedRoute path={`${ROUTES.WAIVER_EXTENSION}/:id?`}>
-                <WaiverExtension/>
-            </AuthenticatedRoute>
-            <AuthenticatedRoute exact path={`${ROUTES.WAIVER_APP_K}/:id?`}>
-                <WaiverAppK/>
-            </AuthenticatedRoute>
-            <AuthenticatedRoute path={`${ROUTES.METRICS}`}>
-                <Metrics/>
-            </AuthenticatedRoute>
-        </>)
-    } else if (userType === ROLES.STATE_ADMIN) {
-        return (<>
-                <AuthenticatedRoute exact path={ROUTES.DASHBOARD}>
-                    <Dashboard/>
-                </AuthenticatedRoute>
-                <AuthenticatedRoute path={`${ROUTES.METRICS}`}>
-                    <Metrics/>
-                </AuthenticatedRoute>
-            </>
-        )
-    } else if (userType === ROLES.CMS_APPROVER) {
-      return <></>
-    } else if (userType === ROLES.SYSTEM_ADMIN) {
-      return <></>
+    if ( userProfile ) {
+        if (userProfile.userData) {
+            const userType = userProfile.userData.type
+            console.log("userType: " + userType)
+            if (userType === ROLES.STATE_USER) {
+                return (<>
+                    <AuthenticatedRoute exact path={ROUTES.DASHBOARD}>
+                        <Dashboard/>
+                    </AuthenticatedRoute>
+                    <AuthenticatedRoute path={`${ROUTES.SPA}/:id?`}>
+                        <Spa/>
+                    </AuthenticatedRoute>
+                    <AuthenticatedRoute exact path={`${ROUTES.WAIVER}/:id?`}>
+                        <Waiver/>
+                    </AuthenticatedRoute>
+                    <AuthenticatedRoute path={`${ROUTES.SPA_RAI}/:id?`}>
+                        <SpaRai/>
+                    </AuthenticatedRoute>
+                    <AuthenticatedRoute path={`${ROUTES.WAIVER_RAI}/:id?`}>
+                        <WaiverRai/>
+                    </AuthenticatedRoute>
+                    <AuthenticatedRoute path={`${ROUTES.WAIVER_EXTENSION}/:id?`}>
+                        <WaiverExtension/>
+                    </AuthenticatedRoute>
+                    <AuthenticatedRoute exact path={`${ROUTES.WAIVER_APP_K}/:id?`}>
+                        <WaiverAppK/>
+                    </AuthenticatedRoute>
+                    <AuthenticatedRoute path={`${ROUTES.METRICS}`}>
+                        <Metrics/>
+                    </AuthenticatedRoute>
+                </>)
+            } else if (userType === ROLES.STATE_ADMIN) {
+                return (<>
+                        <AuthenticatedRoute exact path={ROUTES.DASHBOARD}>
+                            <Dashboard/>
+                        </AuthenticatedRoute>
+                        <AuthenticatedRoute path={`${ROUTES.METRICS}`}>
+                            <Metrics/>
+                        </AuthenticatedRoute>
+                    </>
+                )
+            } else if (userType === ROLES.CMS_APPROVER) {
+                return <></>
+            } else if (userType === ROLES.SYSTEM_ADMIN) {
+                return <></>
+            }
+        }
     }
-
+    return <></>
 }
