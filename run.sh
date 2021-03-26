f3bed7762cb48f333c367ee354f0a43b9a537975
zach lewis <zlewis@clarityinnovates.com>
2021-03-26 14:45:40 -0500

    disable test for this branch only

--- run.sh
+++ run.sh
@@ -4,10 +4,18 @@
 
 echo $branch_name
 
-if [ "$branch_name" ==  "develop" ]  || [ "$branch_name" = "master" ]
+#Non Nightwatch Branch Ignore:
+merge_branch=`git log --merges -n 1 | tail -3 | head -1 | cut -d"/" -f2`
+
+if [ "$branch_name" != "oy2-5385-t1" ] || [ "$branch_name" ==  "develop" ] && [ "$merge_branch" = "oy2-5385-t1" ]
 then
+
+ if [ "$branch_name" ==  "develop" ]  || [ "$branch_name" = "master" ]
+ then
   ./test.sh
-else
+ else
   echo "This branch is not develop or master, it is $branch_name branch"
   ./test.sh --dev
+ fi
+
 fi
--- test.sh
+++ test.sh
@@ -3,12 +3,6 @@
 
 set -e
 
-#Non Nightwatch Branch Ignore:
-branch=`git branch --show-current`
-
-if [ "$branch" != "oy2-5385-t1" ]
-then
-
 case $1 in
 '--dev')
   npm install && npm run regression-dev >&1 || exit 1
@@ -19,5 +13,3 @@ case $1 in
   npm install && npm run regression >&1 || exit 1
   ;;
 esac
-
-fi
