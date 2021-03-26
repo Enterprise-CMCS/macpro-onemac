4c1b5763694ce985573baecc7a15f35aeb65803c
zach lewis <zlewis@clarityinnovates.com>
2021-03-26 14:27:04 -0500

    do not run test for this branch

--- test.sh
+++ test.sh
@@ -3,6 +3,12 @@
 
 set -e
 
+#Non Nightwatch Branch Ignore:
+branch=`git branch --show-current`
+
+if [ "$branch" != "oy2-5385-t1" ]
+then
+
 case $1 in
 '--dev')
   npm install && npm run regression-dev >&1 || exit 1
@@ -13,3 +19,5 @@ case $1 in
   npm install && npm run regression >&1 || exit 1
   ;;
 esac
+
+fi
