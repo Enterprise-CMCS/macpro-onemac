a71e87a23618087f7eaa10d4e78637fcf7b1801d
Zach Lewis <72087844+zachclarity@users.noreply.github.com>
2021-04-01 14:52:50 -0500

    restore nw tests (#186)

--- run.sh
+++ run.sh
@@ -4,18 +4,10 @@
 
 echo $branch_name
 
-#Non Nightwatch Branch Ignore:
-merge_branch=`git log --merges -n 1 | tail -3 | head -1 | cut -d"/" -f2`
-
-if [ "$branch_name" != "oy2-5385-t1" ] || [ "$branch_name" ==  "develop" ] && [ "$merge_branch" = "oy2-5385-t1" ]
+if [ "$branch_name" ==  "develop" ]  || [ "$branch_name" = "master" ]
 then
-
- if [ "$branch_name" ==  "develop" ]  || [ "$branch_name" = "master" ]
- then
   ./test.sh
- else
+else
   echo "This branch is not develop or master, it is $branch_name branch"
   ./test.sh --dev
- fi
-
 fi
