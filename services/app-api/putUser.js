f1e3706f852f599de563d8a764fb77546666b106
Hasan Farook <hasanfar@gmail.com>
2021-04-01 15:46:42 -0400

    removed trailing space

--- services/app-api/putUser.js
+++ services/app-api/putUser.js
@@ -214,7 +214,7 @@ const ensureLegalStatusChange = (userAttribs = [], inputAttrib, isPutUser) => {
         || (currentStatus === 'active') && (targetStatus === 'denied') // active status cannot be changed to denied
         || (currentStatus === 'pending') && (targetStatus === 'revoked') // pending cannot be changed to revoved
         || (targetStatus === 'pending')  // Existing user attribute cannot be changed to pending
-        || (currentStatus === targetStatus) 
+        || (currentStatus === targetStatus)
     ) {
         console.log(`Warning: Illegal status change request from ${currentStatus}, to ${targetStatus}`);
         throw new Error(RESPONSE_CODE.VALIDATION_ERROR);
