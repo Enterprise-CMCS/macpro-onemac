d4508da4992cc0774bce442d77b2161d0d462010
zach lewis <zlewis@clarityinnovates.com>
2021-04-02 09:39:23 -0500

    add form text.

--- services/ui-src/src/changeRequest/ChipSpa.js
+++ services/ui-src/src/changeRequest/ChipSpa.js
@@ -16,6 +16,7 @@ const ChipSpa = () => {
     pageTitle: "Submit New CHIP SPA",
     readOnlyPageTitle: "CHIP SPA Submission Details",
     detailsHeader: "CHIP SPA",
+    subheaderMessage: "Once you submit this form, a confirmation email is sent to you and to CMS. CMS will use this content to review your package and you will not be able to edit this form. If CMS needs any additional information, they will follow up by email.",
     requiredUploads: ["Current State Plan", "Revised Amended State Plan Language"],
     optionalUploads: [
       "Cover Letter",
