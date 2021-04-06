3a7c60d8fd5b699ed637d7ec86b42a8ca669459e
Zach Lewis <72087844+zachclarity@users.noreply.github.com>
2021-04-06 15:06:59 -0500

    updated restored nw test (#197)
    * updated restored nw
    
    * removed extra var.

--- run.sh
+++ run.sh
@@ -5,10 +5,6 @@
 echo $branch_name
 
 #Non Nightwatch Branch Ignore:
-merge_branch=`git log --merges -n 1 | tail -3 | head -1 | cut -d"/" -f2`
-
-if [ "$branch_name" != "oy2-5385-t1" ] || [ "$branch_name" ==  "develop" ] && [ "$merge_branch" = "oy2-5385-t1" ]
-then
 
  if [ "$branch_name" ==  "develop" ]  || [ "$branch_name" = "master" ]
  then
@@ -18,4 +14,3 @@ then
   ./test.sh --dev
  fi
 
-fi
