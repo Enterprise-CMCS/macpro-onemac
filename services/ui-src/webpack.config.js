de17003c4f691bf34ebd7050aa7cacd462917698
mblevins-collabralink <jerry.blevins@cms.hhs.gov>
2021-04-06 18:45:56 -0400

    back to basics

--- services/ui-src/webpack.config.js
+++ services/ui-src/webpack.config.js
@@ -15,10 +15,6 @@ const ie11BabeLoader = {
     }
 };
 
-const nodeExternals = require('webpack-node-externals');
-const path = require('path');
-const slsw = require('serverless-webpack');
-
 module.exports = [
     {
         mode: 'development',
@@ -86,27 +82,5 @@ module.exports = [
             ],
         ]
     },
-    {
-      entry: slsw.lib.entries,
-      target: 'node',
-      externals: [nodeExternals()],
-      output: {
-        libraryTarget: 'commonjs',
-        path: path.resolve(__dirname, '.webpack'),
-        filename: '[name].js' // this should match the first part of function handler in serverless.yml
-      },
-  module: {
-    loaders: [
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        loaders: ["babel-loader"]
-      },
-      {
-        test: /\.json$/,
-        loader: 'json-loader'
-      }
     ]
-  }
-  }]
 }
