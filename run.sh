4a44e27534f2a6fb7dd78e098aafb14d28486b99
zach lewis <zlewis@clarityinnovates.com>
2021-04-06 14:34:22 -0500

    updated restored nw

--- run.sh
+++ run.sh
@@ -7,9 +7,6 @@ echo $branch_name
 #Non Nightwatch Branch Ignore:
 merge_branch=`git log --merges -n 1 | tail -3 | head -1 | cut -d"/" -f2`
 
-if [ "$branch_name" != "oy2-5385-t1" ] || [ "$branch_name" ==  "develop" ] && [ "$merge_branch" = "oy2-5385-t1" ]
-then
-
  if [ "$branch_name" ==  "develop" ]  || [ "$branch_name" = "master" ]
  then
   ./test.sh
@@ -18,4 +15,3 @@ then
   ./test.sh --dev
  fi
 
-fi
