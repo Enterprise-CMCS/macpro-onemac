6b5c3bd513a14a7fd60fac1c2b546c926c2746e0
Hasan Farook <hasanfar@gmail.com>
2021-04-12 15:30:29 -0400

    created homeHeader component

---
+++ services/ui-src/src/components/HomeHeader.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Home from "../containers/Home";
+
+function HomeHeader() {
+    return (
+        <div className="nav-bar">
+            <div className="nav-left">
+                <button className="dropdown" id="myAccountLink">Test</button>
+            </div>
+        </div>
+    );
+}
+export default HomeHeader;
