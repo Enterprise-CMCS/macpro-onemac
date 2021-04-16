c572e98dff762ad81775d6b8fd2a1d6589a4e119
Hasan Farook <hasanfar@gmail.com>
2021-04-16 17:18:38 -0400

    added tests

--- nightwatch/tests/cases/OY2-1494_Test_SPA_Login.js
+++ nightwatch/tests/cases/OY2-1494_Test_SPA_Login.js
@@ -24,7 +24,9 @@ module.exports = {
     }) {
         spa = browser.page.spaBasePage();
         console.log('Login as: ', testData.username);
+        browser.verify.not.elementPresent(spa.elements.dashboardLink);
         spa.devLogin(testData);
+        browser.verify.elementPresent(spa.elements.dashboardLink);
         spa.verify.visible('@titleBar');
         browser.verify.elementPresent('h1');
         browser.verify.containsText('h1', testData.spaPageTitle);
@@ -35,8 +37,11 @@ module.exports = {
         password: browser.globals.pass,
         spaPageTitle: 'SPA and Waiver Dashboard',
     }) {
+
         spa = browser.page.spaBasePage();
+        browser.verify.not.elementPresent(spa.elements.dashboardLink);
         spa.login(testData);
+        browser.verify.elementPresent(spa.elements.dashboardLink);
         spa.verify.visible('@titleBar');
         browser.verify.containsText('h1', testData.spaPageTitle);
     },
