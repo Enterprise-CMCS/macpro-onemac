2c744eb61ff6b98c95b8c61e2f29e09dc15d6efc
Samuel Sierra <ssierra@collabralink.com>
2021-04-16 14:33:43 -0400

    adding to email per sabrina's request

--- services/app-api/putUser.js
+++ services/app-api/putUser.js
@@ -395,7 +395,7 @@ const constructRoleAdminEmails = (recipients, input) => {
 const constructUserEmail = (userEmailId, input) => {
     const email = {
         fromAddressSource: 'userAccessEmailSource',
-        ToAddresses: [userEmailId]
+        ToAddresses: [userEmailId, 'reviewerEmail']
     };
     const updatedStatus = input.attributes[0].status;
     const userType = input.type;
