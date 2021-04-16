2530c2fd2460fafbbae9d093d459fd48259e0a49
zach lewis <zlewis@clarityinnovates.com>
2021-04-16 10:12:28 -0500

    fix user structure

--- updateUser.py
+++ updateUser.py
@@ -10,22 +10,16 @@ if (len(sys.argv) > 4) :
   cognitoPoolId=sys.argv[4]
 
   userTable="cms-spa-form-" + branchName + "-user-profiles"
-  os.system("aws dynamodb scan --table-name " + userTable + " > localUserTestList.json" )
-
+  #os.system("aws dynamodb scan --table-name " + userTable + " > localUserTestList.json" )
   # Opening JSON file
-  f = open('localUserTestList.json')
+  #f = open('localUserTestList.json')
+  #  data = json.load(f)
 
-  data = json.load(f)
-
-  for i in data['Items']:
-    #for user in i:
-      user=str(i['id'])
-      if (search(userId, user) ):
-        f = open("user.json", "w")
-        f.write("{ \"id\": { \"S\": \"" + userId + "\" }, \"type\": { \"S\": \"" + role + "\" }, \"attributes\": { \"L\": [ { \"M\" : { \"history\" : { \"L\": [ { \"M\" : { \"date\" : { \"N\" : \"1617983752\" }, \"doneBy\" : { \"S\" : \"zlewis@clarityinnovates.com\" }, \"status\" : { \"S\" : \"active\" } } }, { \"M\" : { \"date\" : { \"N\" : \"1617985441\" }, \"doneBy\" : { \"S\" : \"zlewis@clarityinnovates.com\" }, \"status\" : { \"S\" : \"active\" } }} ] }, \"stateCode\" : { \"S\" : \"MI\" } }}]} }")
-        f.close()
-        cmd2="aws dynamodb put-item --table-name " + userTable + " --item file://user.json"
-        os.system(cmd2)
+  f = open("user.json", "w")
+  f.write("{ \"id\": { \"S\": \"" + userId + "\" }, \"type\": { \"S\": \"" + role + "\" }, \"attributes\": { \"L\": [ { \"M\" : { \"date\" : { \"N\" : \"1617983752\" }, \"doneBy\" : { \"S\" : \"zlewis@clarityinnovates.com\" }, \"status\" : { \"S\" : \"active\" } , \"history\" : { \"L\": [ { \"M\" : { \"date\" : { \"N\" : \"1617983752\" }, \"doneBy\" : { \"S\" : \"zlewis@clarityinnovates.com\" }, \"status\" : { \"S\" : \"active\" } } }, { \"M\" : { \"date\" : { \"N\" : \"1617985441\" }, \"doneBy\" : { \"S\" : \"zlewis@clarityinnovates.com\" }, \"status\" : { \"S\" : \"active\" } }} ] }, \"stateCode\" : { \"S\" : \"MI\" } }}]} }")
+  f.close()
+  cmd2="aws dynamodb put-item --table-name " + userTable + " --item file://user.json"
+  os.system(cmd2)
 
   # Closing file
   f.close()
