1e87fbaa1d5cfd6ea52a3ce73ef9913028a61b6f
zach lewis <zlewis@clarityinnovates.com>
2021-05-21 14:50:11 -0500

    add GSK severless test method works #11 closer

--- runUnitTest.sh
+++ runUnitTest.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 #set -x
 cd services/ui-src
-cp src/utils/config.js src/mockjson
 cp src/mockjson/mockconfig.js src/utils/config.js
 npm test
-cp src/mockjson/config.js src/utils/config.js
+cp src/mockjson/origconfig.js src/utils/config.js
