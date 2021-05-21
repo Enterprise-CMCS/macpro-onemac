12ea808000122c8b216e64e445aefce91e2dae78
Guli <gabudusalamu@collabralink.com>
2021-05-21 14:58:06 -0400

    updates on tests

--- nightwatch/tests/suites/OY2_7531_State_User_View_Dashboard.js
+++ nightwatch/tests/suites/OY2_7531_State_User_View_Dashboard.js
@@ -42,7 +42,75 @@ module.exports = {
         browser.useXpath().expect.element(michigan).to.be.visible;
         browser.useXpath().expect.element(michigan_access_granted).to.be.visible;
         browser.useCss();
+        let dashboard = "//a[@class ='activeLink']";
+        browser.pause(2000);
+        browser.useXpath().click(dashboard);
+        browser.useCss();
+        browser.pause(2000);
     },
+
+    /*
+    'Submission List Verification > Submit new SPA': function (browser) {
+        // Submit a SPA Report 
+        const newSPA = require('../suites/OY2-3636_Suite_Smoke.js');
+        generatedSPAID = newSPA['Verify user can submit new SPA'](browser);
+        // Verify the submitted Content 
+        let submittedIDNumber = "//table[@class='submissions-table']//tr[1]/td[1]/a";
+        let submittedType = "//table[@class='submissions-table']//tr[1]/td[2]/span";
+        let submittedDate = "//table[@class='submissions-table']//tr[1]/td[3]";
+        // SPA ID Verification 
+        browser.useXpath().expect.element(submittedIDNumber).to.be.visible;
+        browser.useXpath().assert.containsText(submittedIDNumber, generatedSPAID);
+        // Submitted Type Verification 
+        browser.useXpath().expect.element(submittedType).to.be.visible;
+        browser.pause(1000);
+        browser.useXpath().assert.containsText(submittedType, "SPA").pause(1000);
+        // Data Submitted Verification 
+        browser.useXpath().expect.element(submittedDate).to.be.visible;
+        browser.useCss();
+    },
+
+    'Submission List Verification > Submit 1915(b) Waiver Action': function (browser) {
+        browser.useCss().click("button#waiverBtn");
+        browser.useCss().click("select#actionType");
+        browser.useCss().click("select#actionType > option[value='new']");
+        browser.useCss().click("select#waiverAuthority");
+        browser.useCss().click("select#waiverAuthority > option[value='1915(b)(4)']");
+        
+        // Enter Waiver number
+        let num1 = Math.floor(Math.random() * Math.floor(80)) + 10;
+        let num2 = Math.floor(Math.random() * Math.floor(80)) + 10;
+        // SS-YY-NNNN
+        generatedWaiverID = 'VA.' + num1 + '' + num2;
+        // input the SPA ID number 
+        browser.useCss().setValue("input#transmittalNumber", generatedWaiverID);
+
+        
+        // upload a document and make a comment 
+        let fileUploadElem = "[name='uploader-input-0']";
+        let filePath = require('path').resolve(__dirname + '/files/file.docx')
+        browser.useCss().setValue(fileUploadElem, filePath).pause(5000);
+        browser.useCss().setValue("textarea", "This is a test, just a test");
+
+        // click ["Submit"] button 
+        browser.useCss().click("[value='Submit']").pause(1000);
+
+        // Verify the submitted SPA Report Content 
+        let submittedIDNumber = "//table[@class='submissions-table']//tr[1]/td[1]/a";
+        let submittedType = "//table[@class='submissions-table']//tr[1]/td[2]/span";
+        let submittedDate = "//table[@class='submissions-table']//tr[1]/td[3]";
+        // Waiver ID Verification 
+        browser.useXpath().expect.element(submittedIDNumber).to.be.visible;
+        browser.useXpath().assert.containsText(submittedIDNumber, generatedWaiverID);
+        // Submitted Type Verification 
+        browser.useXpath().expect.element(submittedType).to.be.visible;
+        browser.useXpath().assert.containsText(submittedType, "Waiver");
+        // Data Submitted Verification 
+        browser.useXpath().expect.element(submittedDate).to.be.visible;
+        browser.useCss();
+    },
+    */
+
 
 
 
