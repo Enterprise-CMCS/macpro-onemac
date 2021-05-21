dc6f157dedfa86d3ecca7af11dcb14a8d8d1572a
zach lewis <zlewis@clarityinnovates.com>
2021-05-21 12:09:39 -0500

    add config mock for serverless run

--- runUnitTest.sh
+++ runUnitTest.sh
@@ -1,2 +1,7 @@
+#!/bin/sh
+#set -x
 cd services/ui-src
+cp src/utils/config.js src/mockjson
+cp src/mockjson/mockconfig.js src/utils/config.js
 npm test
+cp src/mockjson/config.js src/utils/config.js
