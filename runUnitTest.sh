b446edffd0fa4fd37c6881d6a29dc2b57c82df39
zach lewis <zlewis@clarityinnovates.com>
2021-05-24 08:30:29 -0500

    fix orig config restore

--- runUnitTest.sh
+++ runUnitTest.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
-#set -x
+set -x
 cd services/ui-src
 cp src/mockjson/mockconfig.js src/utils/config.js
 npm test
 cp src/mockjson/origconfig.js src/utils/config.js
+cat src/utils/config.js
+
