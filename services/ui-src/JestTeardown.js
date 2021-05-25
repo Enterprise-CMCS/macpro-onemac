012891376a33ff07b1ef2487c9169ed6ec2c483c
zach lewis <zlewis@clarityinnovates.com>
2021-05-25 08:59:05 -0500

    TBD workaround

--- services/ui-src/JestTeardown.js
+++ services/ui-src/JestTeardown.js
@@ -6,10 +6,10 @@ module.exports = async () => {
         if (err) throw err;
         console.log('Original Config File was copied to destination');
     });
-    fs.unlink('src/utils/getUser.json', (err) => {
+   /* fs.unlink('src/utils/getUser.json', (err) => {
         if (err) throw err;
         console.log('Removed getUser.Json testing Mock File');
-    });
+    });*/
     console.log("Jest -> Tear Down Complete")
 }
 
