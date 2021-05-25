40e226576646e35fe05c8c1bf6ffa92d7320b234
Samuel Sierra <ssierra@collabralink.com>
2021-05-25 13:35:55 -0400

    fix warmup log

--- services/app-api/getMyUserList.js
+++ services/app-api/getMyUserList.js
@@ -8,7 +8,7 @@ import getUser from "./utils/getUser";
 export const main = handler(async (event, context) => {
   // If this invokation is a prewarm, do nothing and return.
   if (event.source == "serverless-plugin-warmup") {
-    console.log()("Warmed up!");
+    console.log("Warmed up!");
     return null;
   }
 
