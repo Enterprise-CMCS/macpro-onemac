f5066bd2fabde804781f061b7133f852cb0d9f83
Guli <gabudusalamu@collabralink.com>
2021-05-27 11:55:11 -0400

    updates on oy2-8595 and oy2-7532

--- nightwatch/tests/suites/OY2_7532_State_User_CHIP_SPA_Form.js
+++ nightwatch/tests/suites/OY2_7532_State_User_CHIP_SPA_Form.js
@@ -1,7 +1,7 @@
 const login = require('../cases/OY2-1494_Test_SPA_Login');
 
 module.exports = {
-    "@tags": ["profileOne", "smoke", "regression-soon"],
+    "@tags": ["smoke", "regression-soon"],
 
     before: function (browser) {
         const testData = {
@@ -21,8 +21,8 @@ module.exports = {
         browser.end();
     },
 
-    'User can see their profile page information Full Name and Email': function (browser) {
-        let my_account_button = 'button#myAccountLink';
+    'State user check the Submit New CHIP SPA form': function (browser) {
+        let submit_new_chip_spa_button = "(//button[@class='ds-c-button ds-c-button--transparent'])[3]";
         let manage_account_link = 'a#manageAccountLink';
 
         // Click on button My Account and get access to account management 
@@ -30,14 +30,13 @@ module.exports = {
         browser.click(manage_account_link); 
         browser.pause(2000);
         //browser.expect.element(".ds-l-col--6>h3").to.be.present;
-        let profile_information = "(//div[@class='ds-l-col--6']/h3)[1]";
+        let expected = "Profile Information";
         let full_name = "(//h3)[2]";
-        let state_user_name ="//div[text()='OneMACState STATEUser']";
         let email = "(//h3)[3]";
-        let email_address = "//div[text()='nkrishna@collabralink.com']";
-        browser.useXpath().expect.element(profile_information).to.be.visible;
-        browser.useXpath().expect.element(full_name).to.be.visible;
-        browser.useXpath().expect.element(state_user_name).to.be.visible;
+        let email_address = "//div[text()='zzsespa03@gmail.com']";
+        browser.useXpath().expect.element("(//div[@class='ds-l-col--6']/h3)[1]").to.be.visible;
+        browser.useXpath().expect.element("(//h3)[2]").to.be.visible;
+        browser.useXpath().expect.element("//div[text()='OneMacState STATEUSER']").to.be.visible;
         browser.useXpath().expect.element(email).to.be.visible;
         browser.useXpath().expect.element(email_address).to.be.visible;
         browser.useCss();
