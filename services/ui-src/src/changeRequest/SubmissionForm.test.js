d407eef93c4c6b7e6bbbfdcaa5136861ed94aafd
Samuel Sierra <ssierra@collabralink.com>
2021-06-04 13:25:42 -0400

    fix form tests

--- services/ui-src/src/changeRequest/SubmissionForm.test.js
+++ services/ui-src/src/changeRequest/SubmissionForm.test.js
@@ -128,7 +128,7 @@ it("does not clear Transmittal Number if submit fails.", async () => {
   const transmittalNumberEl = screen.getByLabelText(
     testFormInfo.transmittalNumber.idLabel
   );
-  const summaryEl = screen.getByLabelText("Additional Information");
+  const summaryEl = screen.getByLabelText("Additional Information",{exact:false});
 
   expect(transmittalNumberEl.value).toBe("");
   expect(summaryEl.value).toBe("");
@@ -196,7 +196,7 @@ it("does not exceed additional information character limit", async () => {
     </AppContext.Provider>
   );
 
-  const summaryEl = screen.getByLabelText("Additional Information");
+  const summaryEl = screen.getByLabelText("Additional Information",{exact:false});
 
   expect(summaryEl.value).toBe("");
 
