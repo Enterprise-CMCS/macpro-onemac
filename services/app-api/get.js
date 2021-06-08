77afee561de6d244347ebef7326efdcb886bf1d7
Samuel Sierra <ssierra@collabralink.com>
2021-06-08 17:39:35 -0400

    trailing spaces

--- services/app-api/get.js
+++ services/app-api/get.js
@@ -22,7 +22,7 @@ export const main = handler(async (event, context) => {
   const result = await dynamoDb.get(params);
   if ( ! result.Item) {
     throw new Error("Item not found.");
-  } 
+  }
   console.log('Sending back result:', JSON.stringify(result,null,2));
   // Return the retrieved item
   return result.Item;
