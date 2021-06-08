6f1f9e51f25272d7961e6158b288c5b498d3b2b2
Amy DiPasquale <adipasquale@fearless.tech>
2021-06-07 22:58:36 -0400

    OY2-8959 minor css updates

--- services/ui-src/src/containers/Dashboard.test.js
+++ services/ui-src/src/containers/Dashboard.test.js
@@ -3,6 +3,7 @@ import { render, screen } from "@testing-library/react";
 import { act } from "react-dom/test-utils";
 import { MemoryRouter } from "react-router-dom";
 
+import { ROUTES } from "cmscommonlib";
 import { AppContext } from "../libs/contextLib";
 import { stateUserInitialAuthState } from "../libs/testDataAppContext";
 import ChangeRequestDataApi from "../utils/ChangeRequestDataApi";
@@ -26,6 +27,6 @@ it("renders with a New Submission button", async () => {
   );
 
   const newSubmissionButton = screen.getByText("New Submission");
-  expect(newSubmissionButton.getAttribute("href")).toBe("/new");
+  expect(newSubmissionButton.getAttribute("href")).toBe(ROUTES.NEW_SUBMISSION_SELECTION);
   await act(() => promise);
 });
