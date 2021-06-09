cc1e91d42bdc4a931b55cb940e3deb78a117e6fc
zach lewis <zlewis@clarityinnovates.com>
2021-06-09 12:20:41 -0500

    add cms approver test and verbose settings on testing

--- services/ui-src/src/libs/testDataAppContext.js
+++ services/ui-src/src/libs/testDataAppContext.js
@@ -134,3 +134,148 @@ export const stateUserDeniedInitialAuthState = {
     },
   },
 };
+
+
+export const stateAdminRevokedInitialAuthState = {
+  isAuthenticating: false,
+  isAuthenticated: true,
+  isLoggedInAsDeveloper: false,
+  isValidRoute: true,
+  userProfile: {
+    cmsRoles: "onemac-state-user",
+    email: "stateuseractive@cms.hhs.local",
+    firstName: "Unit",
+    lastName: "Tester",
+    userData: {
+      firstName: "Unita",
+      lastName: "Goodcode",
+      attributes: [
+        {
+          stateCode: "VA",
+          history: [
+            {
+              date: 1617149287,
+              doneBy: "systemsadmin@cms.hhs.local",
+              status: "revoked",
+            },
+          ],
+        },
+      ],
+      id: "stateuseractive@cms.hhs.local",
+      type: "stateadmin",
+      validRoutes: [
+        "/",
+        "/componentpage",
+        "/profile",
+        "/devlogin",
+        "/FAQ",
+        "/dashboard"
+      ],
+    },
+  },
+};
+
+export const stateAdminDeniedInitialAuthState = {
+  isAuthenticating: false,
+  isAuthenticated: true,
+  isLoggedInAsDeveloper: false,
+  isValidRoute: true,
+  userProfile: {
+    cmsRoles: "onemac-state-user",
+    email: "stateuseractive@cms.hhs.local",
+    firstName: "Unit",
+    lastName: "Tester",
+    userData: {
+      firstName: "Unita",
+      lastName: "Goodcode",
+      attributes: [
+        {
+          stateCode: "VA",
+          history: [
+            {
+              date: 1617149287,
+              doneBy: "systemsadmin@cms.hhs.local",
+              status: "denied",
+            },
+          ],
+        },
+      ],
+      id: "stateuseractive@cms.hhs.local",
+      type: "stateadmin",
+      validRoutes: [
+        "/",
+        "/componentpage",
+        "/profile",
+        "/devlogin",
+        "/FAQ",
+        "/dashboard"
+      ],
+    },
+  },
+};
+
+export const cmsApproverDeniedInitialAuthState = {
+  isAuthenticating: false,
+  isAuthenticated: true,
+  isLoggedInAsDeveloper: false,
+  isValidRoute: true,
+  userProfile: {
+    cmsRoles: "onemac-state-user",
+    email: "stateuseractive@cms.hhs.local",
+    firstName: "Unit",
+    lastName: "Tester",
+    userData: {
+      firstName: "Unita",
+      lastName: "Goodcode",
+      "attributes":[
+          {
+              "date":1617149287,
+              "doneBy":"systemadmintest@cms.hhs.local",
+              "status":"denied"}],
+        "id":"cmsapproverrevoked@cms.hhs.local",
+        "firstName":"Rhonda",
+        "lastName":"Denied",
+        "type":"cmsapprover",
+        "validRoutes":["/",
+            "/componentpage",
+            "/profile",
+            "/devlogin",
+            "/FAQ",
+            "/usermanagement",
+            "/metrics"]
+        },
+  },
+};
+
+export const cmsApproverRevokedInitialAuthState = {
+    isAuthenticating: false,
+    isAuthenticated: true,
+    isLoggedInAsDeveloper: false,
+    isValidRoute: true,
+    userProfile: {
+        cmsRoles: "onemac-state-user",
+        email: "stateuseractive@cms.hhs.local",
+        firstName: "Unit",
+        lastName: "Tester",
+        userData: {
+            firstName: "Unita",
+            lastName: "Goodcode",
+            "attributes":[
+                {
+                    "date":1617149287,
+                    "doneBy":"systemadmintest@cms.hhs.local",
+                    "status":"revoked"}],
+            "id":"cmsapproverrevoked@cms.hhs.local",
+            "firstName":"Rhonda",
+            "lastName":"Revoked",
+            "type":"cmsapprover",
+            "validRoutes":["/",
+                "/componentpage",
+                "/profile",
+                "/devlogin",
+                "/FAQ",
+                "/usermanagement",
+                "/metrics"]
+        },
+    },
+};
--- services/ui-src/src/unit-test/oy2-8060-Dashboard.test.js
+++ services/ui-src/src/unit-test/oy2-8060-Dashboard.test.js
@@ -1,32 +1,43 @@
 import React, {useState} from "react";
-import { render, screen } from "@testing-library/react";
-import { act } from "react-dom/test-utils";
-import { MemoryRouter } from "react-router-dom";
+import {render, screen} from "@testing-library/react";
+import {act} from "react-dom/test-utils";
+import {MemoryRouter} from "react-router-dom";
 
-import { AppContext } from "../libs/contextLib";
-import { stateUserDeniedInitialAuthState, stateUserRevokedInitialAuthState } from "../libs/testDataAppContext";
+import {AppContext} from "../libs/contextLib";
+import {
+    stateUserDeniedInitialAuthState,
+    stateUserRevokedInitialAuthState,
+    stateAdminRevokedInitialAuthState,
+    stateAdminDeniedInitialAuthState,
+    cmsApproverDeniedInitialAuthState,
+    cmsApproverRevokedInitialAuthState
+} from "../libs/testDataAppContext";
 import Dashboard from "../containers/Dashboard";
 
 jest.mock("../utils/ChangeRequestDataApi");
 
+const stateUserRevokedDeniedMsg = "Sorry, you don't have access. Please contact the State System Admin with any questions or visit your user profile for more information.";
+const stateAdminRevokedDeniedMsg = "Sorry, you don't have access. Please contact the CMS Role Approver with any questions, or visit your user profile for more information.";
+const cmsApproverRevokedDeniedMsg = "Sorry, you don't have access. Please contact the CMS System Admin with any questions, or visit your user profile for more information.";
 it("renders state user Denied Message", async () => {
-  const promise = Promise.resolve([{isLoading: false}]);
+    const promise = Promise.resolve([{isLoading: false}]);
 
-  render(
-      <AppContext.Provider
-          value={{
-            ...stateUserDeniedInitialAuthState,
-          }}
-      >
-        <Dashboard />
-      </AppContext.Provider>,
-      { wrapper: MemoryRouter }
-  );
+    render(
+        <AppContext.Provider
+            value={{
+                ...stateUserDeniedInitialAuthState,
+            }}
+        >
+            <Dashboard/>
+        </AppContext.Provider>,
+        {wrapper: MemoryRouter}
+    );
 
-  screen.getByText("Sorry, you don't have access. Please contact the State System Admin with any questions or visit your user profile for more information.");
-  await act(() => promise);
+    screen.getByText(stateUserRevokedDeniedMsg);
+    await act(() => promise);
 
 });
+
 
 it("renders state user Revoked Message", async () => {
     const promise = Promise.resolve([{isLoading: false}]);
@@ -37,12 +48,91 @@ it("renders state user Revoked Message", async () => {
                 ...stateUserRevokedInitialAuthState,
             }}
         >
-            <Dashboard />
+            <Dashboard/>
         </AppContext.Provider>,
-        { wrapper: MemoryRouter }
+        {wrapper: MemoryRouter}
     );
 
-    screen.getByText("Sorry, you don't have access. Please contact the State System Admin with any questions or visit your user profile for more information.");
+    screen.getByText(stateUserRevokedDeniedMsg);
     await act(() => promise);
 
 });
+
+it("renders state Admin Revoked Message", async () => {
+    const promise = Promise.resolve([{isLoading: false}]);
+
+    render(
+        <AppContext.Provider
+            value={{
+                ...stateAdminRevokedInitialAuthState,
+            }}
+        >
+            <Dashboard/>
+        </AppContext.Provider>,
+        {wrapper: MemoryRouter}
+    );
+
+    screen.getByText(stateAdminRevokedDeniedMsg);
+    await act(() => promise);
+
+});
+
+
+it("renders state Admin Denied Message", async () => {
+    const promise = Promise.resolve([{isLoading: false}]);
+
+    render(
+        <AppContext.Provider
+            value={{
+                ...stateAdminDeniedInitialAuthState,
+            }}
+        >
+            <Dashboard/>
+        </AppContext.Provider>,
+        {wrapper: MemoryRouter}
+    );
+
+    screen.getByText(stateAdminRevokedDeniedMsg);
+    await act(() => promise);
+
+});
+
+
+it("renders CMS Approver Denied Message", async () => {
+    const promise = Promise.resolve([{isLoading: false}]);
+
+    render(
+        <AppContext.Provider
+            value={{
+                ...cmsApproverDeniedInitialAuthState,
+            }}
+        >
+            <Dashboard/>
+        </AppContext.Provider>,
+        {wrapper: MemoryRouter}
+    );
+
+    screen.getByText(cmsApproverRevokedDeniedMsg);
+    await act(() => promise);
+
+});
+
+it("renders CMS Approver Denied Message", async () => {
+    const promise = Promise.resolve([{isLoading: false}]);
+
+    render(
+        <AppContext.Provider
+            value={{
+                ...cmsApproverRevokedInitialAuthState,
+            }}
+        >
+            <Dashboard/>
+        </AppContext.Provider>,
+        {wrapper: MemoryRouter}
+    );
+
+    screen.getByText(cmsApproverRevokedDeniedMsg);
+    await act(() => promise);
+
+});
+
