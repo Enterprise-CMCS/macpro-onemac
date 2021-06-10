2552e2c0a52b2e97ab3ea2435302bb3cba24acf6
Guli <gabudusalamu@collabralink.com>
2021-06-10 15:20:22 -0400

    updated on nightwatch.js file

--- nightwatch.js
+++ nightwatch.js
@@ -1,2 +1,16 @@
 require('dotenv').config();
-require('nightwatch/bin/nightwatch');
+require('nightwatch/bin/runner');
+
+
+if(`${process.env.NODE_ENV}` === 'dev') {
+    require('dotenv').config({path: '.env.dev'});
+}
+
+if(`${process.env.NODE_ENV}` === 'val') {
+    require('dotenv').config({path: '.env.val'});
+}
+
+console.log('Setting up the browser instance...');
+console.log('Opening the browser...')
+console.log('Maximizing the browser window size...');
+console.log(`${process.env.APPLICATION_ENDPOINT}`);
