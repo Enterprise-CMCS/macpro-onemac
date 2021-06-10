3b5eb4af93dd3d978417a5133ad7b9a665612354
Kristin_Grue <k.grue@theta-llc.com>
2021-06-10 10:38:16 -0400

    Unit tests should now pass

--- services/ui-src/src/changeRequest/SubmissionForm.test.js
+++ services/ui-src/src/changeRequest/SubmissionForm.test.js
@@ -112,6 +112,7 @@ it("does not clear Transmittal Number if submit fails.", async () => {
 
   window.HTMLElement.prototype.scrollIntoView = function () {};
   window.HTMLElement.prototype.scrollTo = function () {};
+  window.scrollTo = function () {};
 
   render(
     <AppContext.Provider
