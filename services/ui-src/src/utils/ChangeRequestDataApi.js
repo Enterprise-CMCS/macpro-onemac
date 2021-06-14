4f8106c86c1e403d7a9bf80a8fe0e1a2eca40a64
gitsierra <35947499+gitsierra@users.noreply.github.com>
2021-06-14 15:58:20 -0400

    Oy2 9015/9294 attachments accessibility (#296)
    * add public urls
    
    * delete commented code
    
    * trailing spaces

--- services/ui-src/src/utils/ChangeRequestDataApi.js
+++ services/ui-src/src/utils/ChangeRequestDataApi.js
@@ -1,6 +1,5 @@
 import { API } from "aws-amplify";
 import { Auth } from "aws-amplify";
-import { Storage } from "aws-amplify";
 
 /**
  * Singleton class to perform operations with the change request backend.
@@ -48,17 +47,6 @@ class ChangeRequestDataApi {
 
     try {
       let changeRequest = await API.get("changeRequestAPI", `/get/${id}/${userId}`);
-      // Get temporary URLs to the S3 bucket
-      if (changeRequest.uploads) {
-        let i;
-        // Use a for loop instead of forEach to stay in the context of this async function.
-        for (i = 0; i < changeRequest.uploads.length; i++) {
-          changeRequest.uploads[i].url = await Storage.vault.get(
-            changeRequest.uploads[i].s3Key,
-            { level: "protected" }
-          );
-        }
-      }
       return changeRequest;
     } catch (error) {
       console.log(`There was an error fetching ID ${id}.`, error);
