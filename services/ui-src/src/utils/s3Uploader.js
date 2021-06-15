7c622e8294ffc321fabcb71acbe7210fbedf1b21
amyd11 <adipasquale@fearless.tech>
2021-06-15 11:16:53 -0400

    OY2-9057 file extension (#302)
    * oy2-9057 convert file extensions to lower case if needed
    
    * OY2-9057 add tests
    
    * OY2-9057 fix incorrect test label

--- services/ui-src/src/utils/s3Uploader.js
+++ services/ui-src/src/utils/s3Uploader.js
@@ -1,6 +1,27 @@
 import { Storage } from "aws-amplify";
 
 /**
+ * Checks if file extension is lowercase and if not converts it to lowercase.
+ * @param {File} file a file
+ * @return {File} original file if extension is lowercase or a copy of the file with a lowercased file extension
+ */
+export function ensureLowerCaseFileExtension(file) {
+  const extensionStartIndex = file.name.lastIndexOf(".")
+  const fileNameText = file.name.slice(0, extensionStartIndex)
+  const fileNameExtension = file.name.substring(extensionStartIndex)
+  const lowerCaseFileNameExtension = fileNameExtension.toLowerCase()
+
+  if (fileNameExtension === lowerCaseFileNameExtension) {
+    return file
+  } else {
+    const updatedFileName = fileNameText.concat(lowerCaseFileNameExtension)
+    const updatedFile = new File([file], updatedFileName, {type:file.type});
+    updatedFile.title = file.title;
+    return updatedFile;
+  }
+}
+
+/**
  * Upload a list of files to an S3 bucket.
  * @param {Array.Object} fileArray a list of files to upload
  * @return {Promise} promise that returns an array of files uploaded
@@ -32,29 +53,32 @@ export async function uploadFiles(fileArray) {
 
   return resultPromise;
 }
+
 /**
  * Upload a file to the S3 bucket.
  * @param {Object} file file object from the form
  * @returns metadata of the uploaded object which includes s3 key, filename, content type and url
  */
 export async function uploadFile(file) {
+  const fileToUpload = ensureLowerCaseFileExtension(file)
+
   let retPromise;
-  const targetPathname = `${Date.now()}/${file.name}`;
+  const targetPathname = `${Date.now()}/${fileToUpload.name}`;
 
   try {
-    const stored = await Storage.vault.put(targetPathname, file, {
+    const stored = await Storage.vault.put(targetPathname, fileToUpload, {
       level: "protected",
-      contentType: file.type,
+      contentType: fileToUpload.type,
     });
 
     const url = await Storage.vault.get(stored.key, { level: "protected" });
 
     let result = {
       s3Key: stored.key,
-      filename: file.name,
-      contentType: file.type,
+      filename: fileToUpload.name,
+      contentType: fileToUpload.type,
       url: url.split("?", 1)[0], //We only need the permalink part of the URL since the S3 bucket policy allows for public read
-      title: file.title,
+      title: fileToUpload.title,
     };
 
     // If the upload to S3 fails (handled in AWS Amplify Storage) a message gets added to the console, but the
