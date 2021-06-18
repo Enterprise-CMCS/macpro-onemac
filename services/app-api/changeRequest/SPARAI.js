71c09054e3e8864e9beccff8ea0f16dbf45eb04f
Haywood Wells <hwells@collabralink.com>
2021-06-18 07:29:29 -0700

    Comment added to kick off commit

--- services/app-api/changeRequest/SPARAI.js
+++ services/app-api/changeRequest/SPARAI.js
@@ -1,6 +1,6 @@
 import { getLinksHtml } from "./changeRequest-util";
 import dynamoDb from "../libs/dynamodb-lib";
-import { RESPONSE_CODE } from "../libs/response-codes";
+import { RESPONSE_CODE } from "../libs/response-codes";  //comment to kick off change
 
 /**
  * SPA RAI submission specific email generation functions.
