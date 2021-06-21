f0bee5fa485d285550b63ef7fe6b98da1bf9cbf9
Samuel Sierra <ssierra@collabralink.com>
2021-06-21 10:16:40 -0400

    fix dynamo search for helpdesk and state users

--- services/app-api/getAllByAuthorizedTerritories.js
+++ services/app-api/getAllByAuthorizedTerritories.js
@@ -9,7 +9,10 @@ import { getAuthorizedStateList } from "./user/user-util";
  * Gets all change requests from the DynamoDB change requests table
  * that correspond to the user's active access to states/territories
  */
+
 export const main = handler(async (event, context) => {
+  var allResults = [];
+  var newKey;
   // If this invokation is a prewarm, do nothing and return.
   if (event.source == "serverless-plugin-warmup") {
     console.log("Warmed up!");
@@ -20,59 +23,97 @@ export const main = handler(async (event, context) => {
   if (!user) {
     return RESPONSE_CODE.USER_NOT_FOUND;
   }
-  var allResults = [];
-  return await getDataFromDB(null).then(function () {
-    // extracts items from each of the results
-    let items = [];
-    allResults.forEach((result) => {
-      items = items.concat(result[0].Items);
-    });
-
-    if (items.length === 0) {
-      console.log(`No change requests found matching that query.`);
-    }
-
-    console.log(`Sending back ${items.length} change request(s).`);
-    return items;
+  allResults = await getDataFromDB(newKey, user);
+  // extracts items from each of the results
+  let items = [];
+  allResults.forEach((result) => {
+    items = items.concat(result.Items);
   });
+  if (items.length === 0) {
+    console.log(`No change requests found matching that query.`);
+  }
 
-  async function getDataFromDB(startingKey) {
-    let promises;
+  console.log(`Sending back ${items.length} change request(s).`);
+  return items;
+});
+
+async function getDataFromDB(startingKey, user) {
+  let promises;
+  let tempResults = [];
+  let allResults = [];
+  if (!startingKey) {
+    startingKey = null;
+  }
+  try {
+    var keepSearching;
     if (user.type === USER_TYPE.HELPDESK) {
-      promises = [dynamoDb.scan({
-        TableName: process.env.tableName,
-        ExclusiveStartKey: startingKey
-      })];
+      keepSearching = true;
+      while (keepSearching == true) {
+        [startingKey, keepSearching, tempResults] = await helpdeskOrReviewerDynamoDbQuery(startingKey, keepSearching, tempResults);
+      }
+      return tempResults;
     } else {
       // query dynamodb for each territory in the territiories array
-      promises = getAuthorizedStateList(user).map((territory) =>
-        dynamoDb.query({
-          TableName: process.env.tableName,
-          ExclusiveStartKey: startingKey,
-          IndexName: "territory-submittedAt-index",
-          KeyConditionExpression:
-            "territory = :v_territory and submittedAt > :v_submittedAt",
-          ExpressionAttributeValues: {
-            ":v_territory": territory,
-            ":v_submittedAt": 0,
-          },
-          ScanIndexForward: false, // sorts the results by submittedAt in descending order (most recent first)
-        })
-      );
-    }
-    // resolve promises from all queries
-    let results;
-    try {
-      results = await Promise.all(promises);
-      allResults.push(results);
-      if (results[0].LastEvaluatedKey) {
-        await getDataFromDB(results[0].LastEvaluatedKey);
-      } else {
-        return;
+      promises = getAuthorizedStateList(user).map(async (territory) => {
+        tempResults = [];
+        keepSearching = true;
+        while (keepSearching == true) {
+          [startingKey, keepSearching, tempResults] = await stateUserDynamoDbQuery(startingKey, territory, keepSearching, tempResults);
+        };
+        return tempResults;
+      });
+        // resolve promises from all queries
+      allResults = await Promise.all(promises);
+      let concatResults = [];
+      for (var i = 0; i < allResults.length; i++) {
+        allResults[i].forEach((stateInfo) => {
+          concatResults.push(stateInfo);
+        });
       }
-    } catch (error) {
-      console.error("Could not fetch results from Dynamo:", error);
-      return RESPONSE_CODE.DATA_RETRIEVAL_ERROR;
+      return concatResults;
     }
   }
-});
+  catch (error) {
+    console.error("Could not fetch results from Dynamo:", error);
+    return RESPONSE_CODE.DATA_RETRIEVAL_ERROR;
+  }
+}
+async function helpdeskOrReviewerDynamoDbQuery(startingKey, keepSearching, allResults) {
+  let results = await dynamoDb.scan({
+    TableName: process.env.tableName,
+    ExclusiveStartKey: startingKey
+  });
+  allResults.push(results);
+  if (results.LastEvaluatedKey) {
+    startingKey = results.LastEvaluatedKey;
+    return [startingKey, keepSearching, allResults];
+  } else {
+    keepSearching = false;
+    return [null, keepSearching, allResults];
+  }
+}
+
+
+async function stateUserDynamoDbQuery(startingKey, territory, keepSearching, allResults) {
+  let results = await dynamoDb.query({
+    TableName: process.env.tableName,
+    ExclusiveStartKey: startingKey,
+    IndexName: "territory-submittedAt-index",
+    KeyConditionExpression:
+      "territory = :v_territory and submittedAt > :v_submittedAt",
+    ExpressionAttributeValues: {
+      ":v_territory": territory,
+      ":v_submittedAt": 0,
+    },
+    ScanIndexForward: false, // sorts the results by submittedAt in descending order (most recent first)
+  });
+  allResults.push(results);
+  if (results.LastEvaluatedKey) {
+    startingKey = results.LastEvaluatedKey;
+    return [startingKey, keepSearching, allResults];
+  } else {
+    keepSearching = false;
+    return [null, keepSearching, allResults];
+  }
+}
+
