149129df68eeedd8693277e91a5ebbd9e8703e2d
Hasan Farook <hasanfar@gmail.com>
2021-06-24 18:09:27 -0400

    added CMSreviewer user type

---
+++ services/app-api/user/CMSReviewer.js
@@ -0,0 +1,118 @@
+import { USER_TYPE } from "cmscommonlib";
+import { getCurrentStatus } from "./user-util";
+
+/**
+ * CMSReiewer User specific functions.
+ *
+ * all CMSReiewer User can view all Users except CMS System Admins
+ *
+ * @class
+ */
+class CMSReviewer {
+  /**
+   * CMS Approvers manage the State Admins
+   * @returns {String} the User Role
+   */
+  getScanParams() {
+    let scanParams = {
+      TableName: process.env.userTableName,
+      FilterExpression: "#ty <> :userType",
+      ExpressionAttributeNames: { "#ty": "type" },
+      ExpressionAttributeValues: { ":userType": USER_TYPE.SYSTEM_ADMIN },
+    };
+    return scanParams;
+  }
+
+  /**
+   * CMSReiewer User do NOT have a state
+   * @returns {Boolean} false because we do not check if the states match
+   */
+  shouldICheckState() {
+    return false;
+  }
+
+  /**
+   * CMSReiewer User have to be active to see user lists
+   * @returns {String} null always as CMSReiewer User can have a read-only view of all users with any statuses
+   */
+  canIRequestThis(doneBy) {
+    return undefined;
+  }
+
+  /**
+   * takes the raw user data and transforms into
+   * what to send to front end.
+   *
+   * CMSReiewer User gets all users except CMS System Admins
+   *
+   * @param {userResult} Array of User Objects from database
+   * @returns {userRows} the list of users
+   */
+  transformUserList(userResult) {
+    let userRows = [];
+    let errorList = [];
+    let i = 1;
+
+    console.log("results:", JSON.stringify(userResult));
+
+    // if there are no items, return an empty user list
+    if (!userResult.Items) return userRows;
+
+    userResult.Items.forEach((oneUser) => {
+      // All users must have the attribute section
+      if (!oneUser.attributes) {
+        errorList.push(
+          "Attributes data required for this role, but not found ",
+          oneUser
+        );
+        return;
+      }
+      if (oneUser.type !== "cmsapprover" && oneUser.type !== "helpdesk" && oneUser.type !== "cmsreviewer") {
+        oneUser.attributes.forEach((oneAttribute) => {
+          // State Admins and State Users must have the history section
+          if (!oneAttribute.history) {
+            errorList.push(
+              "History data required for this role, but not found ",
+              oneUser
+            );
+            return;
+          }
+
+          userRows.push({
+            id: i,
+            email: oneUser.id,
+            firstName: oneUser.firstName,
+            lastName: oneUser.lastName,
+            stateCode: oneAttribute.stateCode,
+            role: oneUser.type,
+            latest: getCurrentStatus(oneAttribute.history),
+          });
+          i++;
+        });
+      }
+      // Helpdesk users, CMSReviewers and CMS Approvers must not have the history section
+      else {
+        userRows.push({
+          id: i,
+          email: oneUser.id,
+          firstName: oneUser.firstName,
+          lastName: oneUser.lastName,
+          stateCode: "N/A",
+          role: oneUser.type,
+          latest: getCurrentStatus(oneUser.attributes),
+        });
+        i++;
+      }
+    });
+
+    console.log("error List is ", errorList);
+
+    console.log("Response:", userRows);
+
+    return userRows;
+  }
+}
+
+const instance = new CMSReviewer();
+Object.freeze(instance);
+export default instance;
