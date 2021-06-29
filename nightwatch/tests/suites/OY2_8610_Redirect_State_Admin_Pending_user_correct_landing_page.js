2df9c73b51ef728227db4933897a6f1c6b11c573
Kristin_Grue <k.grue@theta-llc.com>
2021-06-29 14:28:07 -0400

    wrong State User - this was an admin

--- nightwatch/tests/suites/OY2_8610_Redirect_State_Admin_Pending_user_correct_landing_page.js
+++ nightwatch/tests/suites/OY2_8610_Redirect_State_Admin_Pending_user_correct_landing_page.js
@@ -18,7 +18,7 @@ module.exports = {
         //check on user management page 
         let home_link = "(//a[@class='ds-u-text-decoration--none'])[1]";
         let faq_link = "(//a[@class='ds-u-text-decoration--none'])[2]";
-        let dashboard_link = "//a[@id='dashboardLink']";
+        // State Admin User does not see dashboard let dashboard_link = "//a[@id='dashboardLink']";
         let component_page_link = "(//a[@class='ds-u-text-decoration--none'])[3]";
         let submission_list_title = "//h1";
         let puzzle_piece_image = "//div[@class='empty-list']/img";
@@ -33,7 +33,7 @@ module.exports = {
         browser.useXpath().expect.element(submission_list_title).to.be.visible;
         browser.useXpath().expect.element(puzzle_piece_image).to.be.visible;
         browser.useXpath().expect.element(pending_message).to.be.visible;
-        browser.useXpath().verify.containsText(pending_message, "Your system access is pending approval. Contact your State System Admin with any questions..");
+        browser.useXpath().verify.containsText(pending_message, "Your system access is pending approval.");
         browser.useXpath().expect.element(my_account_button).to.be.visible;
         browser.click(my_account_button);
         browser.useXpath().expect.element(management_profile_link).to.be.visible;
