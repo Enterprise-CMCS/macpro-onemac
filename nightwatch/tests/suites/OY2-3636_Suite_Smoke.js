a8d3cfc604e5ec4aed73ae56da8042fad05eb954
Guli <72164092+GL2020-QM@users.noreply.github.com>
2021-07-02 12:03:07 -0400

    fixed oy2-3636 and oy2-5867 (#345)

--- nightwatch/tests/suites/OY2-3636_Suite_Smoke.js
+++ nightwatch/tests/suites/OY2-3636_Suite_Smoke.js
@@ -64,7 +64,7 @@ module.exports = {
 
         // Verify the SPA on Submission List 
         // browser.useXpath().click("//a[@id='new-submission-button']");
-        browser.useXpath().waitForElementVisible("(//table[@class='submissions-table']/tbody/tr/td/a)[1]", 1000);
+        //browser.useXpath().waitForElementVisible("(//table[@class='submissions-table']/tbody/tr/td/a)[1]", 1000);
         // browser.refresh();
         browser.useXpath().verify.containsText('(//table[@class="submissions-table"]/tbody/tr/td/a)[1]', spaID);
         browser.useCss();
