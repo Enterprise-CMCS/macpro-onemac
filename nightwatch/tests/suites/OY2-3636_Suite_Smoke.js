ce271021c7890a1e08abafb4bc8a0494429cd73f
pboggarapu <77852304+pboggarapu@users.noreply.github.com>
2021-07-08 13:46:40 -0400

    3636 code update

--- nightwatch/tests/suites/OY2-3636_Suite_Smoke.js
+++ nightwatch/tests/suites/OY2-3636_Suite_Smoke.js
@@ -58,10 +58,11 @@ module.exports = {
         browser
         .useCss()
         .waitForElementPresent("[value='Submit']", 1000)
-        .click("[value='Submit']").pause(5000);
+        .click("[value='Submit']").pause(8000);
         
         browser.refresh();
-
+        browser.pause(5000);
+        browser.refresh();
         // Verify the SPA on Submission List 
         // browser.useXpath().click("//a[@id='new-submission-button']");
         //browser.useXpath().waitForElementVisible("(//table[@class='submissions-table']/tbody/tr/td/a)[1]", 1000);
