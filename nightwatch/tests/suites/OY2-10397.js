b73c68449d550e365fed581e711f9d57c960fc76
nkrishna3 <74625902+nkrishna3@users.noreply.github.com>
2021-07-12 16:26:34 -0400

    Oy2 10397 (#356)
    * added test OY2-9990 State Submitter and State System Admin
    
    * added OY2-9990 CMS user
    
    * Nightwatch script for sorting
    
    * Updates to Oy2-10397
    
    * modified config file
    
    * changes to screen enhancements
    
    * changes to oy2-7531
    
    Co-authored-by: Guli <gabudusalamu@collabralink.com>
    Co-authored-by: gitsierra <35947499+gitsierra@users.noreply.github.com>

---
+++ nightwatch/tests/suites/OY2-10397.js
@@ -0,0 +1,38 @@
+
+const login = require('../suites/OY2_9999_Login');
+module.exports = {
+
+    "@tags": ["smoke", "regression-soon"],
+
+    before: function (browser) {
+        login.beforeEach(browser);
+        login['Login with cms_helpdesk_users'](browser); 
+    },
+
+    after: function (browser) {
+        login.afterEach(browser);
+    },
+    'Verify the sort funtionality on Dashboard': function (browser) {
+       browser.useXpath().assert.containsText( "/html/body/reference/div/div/div[2]/div[1]/div/h1", "Submission List");
+       browser.useXpath().assert.containsText( "/html/body/reference/div/div/div[2]/div[2]/table/thead/tr/th[2]", "Type");
+       browser.click("xpath", "/html/body/reference/div/div/div[2]/div[2]/table/thead/tr/th[2]");
+       browser.useXpath().assert.containsText( "/html/body/reference/div/div/div[2]/div[2]/table/thead/tr/th[3]", "State");
+       browser.click("xpath", "/html/body/reference/div/div/div[2]/div[2]/table/thead/tr/th[3]");
+       browser.useXpath().assert.containsText( "/html/body/reference/div/div/div[2]/div[2]/table/thead/tr/th[4]", "Date Submitted");
+       browser.click("xpath", "/html/body/reference/div/div/div[2]/div[2]/table/thead/tr/th[4]");
+       browser.useXpath().assert.containsText( "/html/body/reference/div/div/div[2]/div[2]/table/thead/tr/th[5]", "State Submitter");
+       browser.click("xpath", "/html/body/reference/div/div/div[2]/div[2]/table/thead/tr/th[5]");
+       browser.waitForElementPresent('link text', 'User Management').click('link text', 'User Management');
+       browser.useXpath().assert.containsText( "/html/body/reference/div/div/div[2]/div[2]/table/thead/tr/th[1]", "Name");
+       browser.click("xpath", "/html/body/reference/div/div/div[2]/div[2]/table/thead/tr/th[1]");
+       browser.useXpath().assert.containsText( "/html/body/reference/div/div/div[2]/div[2]/table/thead/tr/th[2]", "Email");
+       browser.click("xpath", "/html/body/reference/div/div/div[2]/div[2]/table/thead/tr/th[2]");
+       browser.useXpath().assert.containsText( "/html/body/reference/div/div/div[2]/div[2]/table/thead/tr/th[3]", "State");
+       browser.click("xpath", "/html/body/reference/div/div/div[2]/div[2]/table/thead/tr/th[3]");
+       browser.useXpath().assert.containsText( "/html/body/reference/div/div/div[2]/div[2]/table/thead/tr/th[4]", "Status");
+       browser.click("xpath", "/html/body/reference/div/div/div[2]/div[2]/table/thead/tr/th[4]");
+       browser.useXpath().assert.containsText( "/html/body/reference/div/div/div[2]/div[2]/table/thead/tr/th[5]", "Role");
+       browser.click("xpath", "/html/body/reference/div/div/div[2]/div[2]/table/thead/tr/th[5]");
+       
+    },
+}
