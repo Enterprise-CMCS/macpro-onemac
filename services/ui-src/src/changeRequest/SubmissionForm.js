9d16f2bcf229ad4bfb721ce27c516f972f33b9e6
zach lewis <zlewis@clarityinnovates.com>
2021-07-29 08:52:24 -0500

    fix warning message code

--- services/ui-src/src/changeRequest/SubmissionForm.js
+++ services/ui-src/src/changeRequest/SubmissionForm.js
@@ -220,6 +220,7 @@ export const SubmissionForm = ({ changeRequestType }) => {
     let formatMessage = {
       statusLevel: "error",
       statusMessage: validateTransmittalNumber(changeRequest.transmittalNumber),
+      warningMessageCode: "",
     };
 
     let existMessages = [];
@@ -261,11 +262,13 @@ export const SubmissionForm = ({ changeRequestType }) => {
                   tempMessage = `According to our records, this ${transmittalNumberDetails.idLabel} does not exist. Please check the ${transmittalNumberDetails.idLabel} and try entering it again.`;
                 } else {
                   tempMessage = `${transmittalNumberDetails.idLabel} not found. Please ensure you have the correct ${transmittalNumberDetails.idLabel} before submitting. Contact the MACPro Help Desk (code: ${RESPONSE_CODE.SUBMISSION_ID_NOT_FOUND_WARNING}) if you need support.`;
+                  tempCode = RESPONSE_CODE.SUBMISSION_ID_NOT_FOUND_WARNING
                 }
                 // ID exists but it should NOT exist
               } else if (dupID && !correspondingValidation.idMustExist) {
                 if (correspondingValidation.errorLevel === "error") {
                   tempMessage = `According to our records, this ${transmittalNumberDetails.idLabel} already exists. Please check the ${transmittalNumberDetails.idLabel} and try entering it again.`;
+                  tempCode = RESPONSE_CODE.SUBMISSION_ID_EXIST_WARNING
                 } else {
                   tempMessage = `According to our records, this ${transmittalNumberDetails.idLabel} already exists. Please ensure you have the correct ${transmittalNumberDetails.idLabel} before submitting. Contact the MACPro Help Desk (code: ${RESPONSE_CODE.SUBMISSION_ID_EXIST_WARNING}) if you need support.`;
                   tempCode = RESPONSE_CODE.SUBMISSION_ID_EXIST_WARNING
@@ -334,7 +337,7 @@ export const SubmissionForm = ({ changeRequestType }) => {
         transmittalNumberStatusMessage.statusLevel === "warn" &&
         transmittalNumberStatusMessage.statusMessage
       ) {
-        transmittalNumberWarningMessage = transmittalNumberWarningMessage.warningMessageCode;
+        transmittalNumberWarningMessage = transmittalNumberStatusMessage.warningMessageCode;
       }
 
       uploadRef
