a6f66183ab00d336ee44e13ee6c42e70f0903d3e
Kristin_Grue <k.grue@theta-llc.com>
2021-08-02 16:41:17 -0400

    broke out transactions for debugging

--- services/app-api/utils/updatePackage.js
+++ services/app-api/utils/updatePackage.js
@@ -4,6 +4,9 @@ export default async function updatePackage(updateData) {
   // get the latest version from current v0 Item
   var v0sk = "v0#" + updateData.packageID;
   var v0pk = updateData.packageID.toString().substring(0, 2);
+  var oldVersion;
+  var newVersion;
+  var nextItem = {};
 
   var v0params = {
     TableName: process.env.oneMacTableName,
@@ -13,101 +16,79 @@ export default async function updatePackage(updateData) {
     },
   };
 
-  let currentPackage;
-  try {
-    currentPackage = await dynamoDb.get(v0params);
-  } catch (dbError) {
-    console.log(`Error happened while reading from DB:  ${dbError.message}`);
-    throw dbError;
-  }
+  dynamoDb
+    .get(v0params)
+    .then((currentPackage) => {
+      if (!currentPackage.Item) {
+        throw new Error(
+          `v0 Item does not exist for params: ${JSON.stringify(v0params)}`
+        );
+      }
+      // copy the v0 into the v(currentVersion+1)
+      console.log("Result.Item is : ", currentPackage.Item);
 
-  if (!currentPackage.Item) {
-    throw new Error(
-      `v0 Item does not exist for params: ${JSON.stringify(v0params)}`
-    );
-  }
+      oldVersion = parseInt(currentPackage.Item.currentVersion);
+      newVersion = oldVersion + 1;
 
-  // copy the v0 into the v(currentVersion+1)
-  console.log("Result.Item is : ", currentPackage.Item);
+      nextItem = { ...currentPackage.Item };
 
-  let oldVersion = parseInt(currentPackage.Item.currentVersion);
-  let newVersion = oldVersion + 1;
+      nextItem.sk = "v" + newVersion + "#" + currentPackage.Item.packageID;
+      nextItem.GSI1pk = "PACKAGE#v" + newVersion;
+      nextItem.GSI1sk =
+        currentPackage.Item.timestamp + "#" + currentPackage.Item.packageID;
 
-  let nextItem = { ...currentPackage.Item };
-  let updateExp = "SET #currentVersion = :newVersion";
-  let expressionAttributeNames = {
-    "#currentVersion": "currentVersion",
-  };
-  let expressionAttributeValues = {
-    ":newVersion": newVersion.toString(),
-    ":currentVersion": oldVersion.toString(),
-  };
+      var vnextparams = {
+        TableName: process.env.oneMacTableName,
+        Item: nextItem,
+      };
+      console.log("vnextParams is: ", vnextparams);
 
-  nextItem.currentVersion = newVersion;
-  nextItem.sk = "v" + newVersion + "#" + currentPackage.Item.packageID;
+      return dynamoDb.put(vnextparams);
+    })
+    .then((vnextresult) => {
+      console.log("Put result is: ", vnextresult);
 
-  // go through and make our updates... if the attribute already exists, overwrite
-  // if it does not, then add
-  for (const [key, value] of Object.entries(updateData)) {
-    if (key === "packageID") continue;
-    nextItem[key] = value;
-    updateExp += ",#" + key + " = :" + key;
-    expressionAttributeNames["#" + key] = key.toString();
-    expressionAttributeValues[":" + key] = value.toString();
-  }
+      let updateExp = "SET #currentVersion = :newVersion";
+      let expressionAttributeNames = {
+        "#currentVersion": "currentVersion",
+      };
+      let expressionAttributeValues = {
+        ":newVersion": newVersion.toString(),
+        ":currentVersion": oldVersion.toString(),
+      };
 
-  console.log("update Expression: ", updateExp);
-  var transParams = {
-    TransactItems: [
-      {
-        Put: {
-          TableName: process.env.oneMacTableName,
-          Item: nextItem,
+      // go through and make our updates... if the attribute already exists, overwrite
+      // if it does not, then add
+      for (const [key, value] of Object.entries(updateData)) {
+        if (key === "packageID") continue;
+        updateExp += ",#" + key + " = :" + key;
+        expressionAttributeNames["#" + key] = key.toString();
+        expressionAttributeValues[":" + key] = value.toString();
+      }
+
+      console.log("update Expression: ", updateExp);
+
+      let updateParams = {
+        TableName: process.env.oneMacTableName,
+        Key: {
+          pk: v0pk,
+          sk: v0sk,
         },
-      },
-      {
-        Update: {
-          TableName: process.env.oneMacTableName,
-          Key: {
-            pk: v0pk,
-            sk: v0sk,
-          },
-          ConditionExpression: "#currentVersion = :currentVersion",
-          UpdateExpression: updateExp,
-          ExpressionAttributeNames: expressionAttributeNames,
-          ExpressionAttributeValues: expressionAttributeValues,
-        },
-      },
-    ],
-  };
+        ConditionExpression: "#currentVersion = :currentVersion",
+        UpdateExpression: updateExp,
+        ExpressionAttributeNames: expressionAttributeNames,
+        ExpressionAttributeValues: expressionAttributeValues,
+      };
 
-  console.log("trasaction params: ", JSON.stringify(transParams));
-
-  try {
-    let transResult = await dynamoDb.transactWriteItems(transParams);
-    console.log(`transaction write result: `, transResult);
-  } catch (dbError) {
-    console.log(
-      `Error happened while transact writing to DB:  ${dbError.message}`
-    );
-    throw dbError;
-  }
-
-  try {
-    currentPackage = await dynamoDb.get(v0params);
-  } catch (dbError) {
-    console.log(`Error happened while reading from DB:  ${dbError.message}`);
-    throw dbError;
-  }
-
-  if (!currentPackage.Item) {
-    throw new Error(
-      `v0 Item does not exist for params: ${JSON.stringify(v0params)}`
-    );
-  }
-
-  // copy the v0 into the v(currentVersion+1)
-  console.log("Updated v0 Result.Item is : ", currentPackage.Item);
+      return dynamoDb.update(updateParams);
+    })
+    .then((updateResult) => {
+      console.log("updateResult is: ", updateResult);
+    })
+    .catch((dbError) => {
+      console.log(`Error happened while reading from DB:  ${dbError.message}`);
+      throw dbError;
+    });
 
   return;
 }
