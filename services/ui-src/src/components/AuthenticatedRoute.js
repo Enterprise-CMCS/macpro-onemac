9e22cf63094cc80868f4b0237734325c488ae827
Kristin_Grue <k.grue@theta-llc.com>
2021-08-09 09:24:59 -0400

    more cleanup of Routes

--- services/ui-src/src/components/AuthenticatedRoute.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Route } from "react-router-dom";
-import { useAppContext } from "../libs/contextLib";
-
-export default function AuthenticatedRoute({ children, ...rest }) {
-  const { isAuthenticated } = useAppContext();
-
-  return <Route {...rest}>{isAuthenticated ? children : <div></div>}</Route>;
-}
