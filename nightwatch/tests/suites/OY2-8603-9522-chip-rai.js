ab6419141eb3213afb9840793872423f86882487
Krishna <nkrishna@collabralink.com>
2021-08-09 19:02:48 -0400

    fixes

--- nightwatch/tests/suites/OY2-8603-9522-chip-rai.js
+++ nightwatch/tests/suites/OY2-8603-9522-chip-rai.js
@@ -139,7 +139,7 @@ module.exports = {
         
         // SPA ID Verification 
         browser.useCss().click(submittedIDNumber);
-        browser.pause(2000);
+        browser.pause(3000);
         browser.useXpath().assert.not.elementPresent("/html/body/reference/div/div/div[3]/form/div[1]/div/div/div[2]/a");
         browser.useXpath().assert.not.elementPresent("/html/body/reference/div/div/div[3]/form/div[2]/p[2]");
        // browser.useCss().click( "#back-button > svg").waitForElementPresent("body");
