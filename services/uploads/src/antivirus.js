af3855099095d886748124d053b1c2c204aad29a
Haywood Wells <hwells@collabralink.com>
2021-08-12 14:47:41 -0700

    Trying the ACL change again

--- services/uploads/src/antivirus.js
+++ services/uploads/src/antivirus.js
@@ -140,13 +140,13 @@ async function scanS3Object(s3ObjectKey, s3ObjectBucket) {
   var aclParams = {
     Bucket: s3ObjectBucket,
     Key: s3ObjectKey,
-    GrantRead: '*'
+    GrantRead: "uri=http://acs.amazonaws.com/groups/global/AllUsers",
   };
 
   try {
     await s3.putObjectTagging(taggingParams).promise();
     utils.generateSystemMessage("Tagging successful");
-    s3.putObjectAcl(params, function(err, data) {
+    s3.putObjectAcl(aclParams, function(err, data) {
       if (err) console.log(err, err.stack); // an error occurred
       else     console.log(data);           // successful response
     });
