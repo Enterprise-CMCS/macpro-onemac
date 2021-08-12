02643c9116135bf720e7a7ae5bf4238211c1194a
Haywood Wells <hwells@collabralink.com>
2021-08-12 15:14:19 -0700

    Commenting out ACL to see if it writes correctly

--- services/uploads/src/antivirus.js
+++ services/uploads/src/antivirus.js
@@ -146,10 +146,10 @@ async function scanS3Object(s3ObjectKey, s3ObjectBucket) {
   try {
     await s3.putObjectTagging(taggingParams).promise();
     utils.generateSystemMessage("Tagging successful");
-    s3.putObjectAcl(aclParams, function(err, data) {
-      if (err) console.log(err, err.stack); // an error occurred
-      else     console.log(data);           // successful response
-    });
+    // s3.putObjectAcl(aclParams, function(err, data) {
+      // if (err) console.log(err, err.stack); // an error occurred
+      // else     console.log(data);           // successful response
+    // });
   } catch (err) {
     console.log(err);
   } finally {
