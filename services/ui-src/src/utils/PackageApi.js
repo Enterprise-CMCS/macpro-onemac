7bf264425db57c5f44744bbf3d6d4801335ecfa5
Kristin_Grue <k.grue@theta-llc.com>
2021-08-16 15:24:37 -0400

    now can view the packages

---
+++ services/ui-src/src/utils/PackageApi.js
@@ -0,0 +1,33 @@
+import { API, Storage } from "aws-amplify";
+import handleApiError from "../libs/apiErrorHandler";
+/**
+ * Singleton class to perform operations with the change request backend.
+ */
+class PackageApi {
+  /**
+   * Fetch all packages that correspond to the user's active access to states/territories
+   * @param {string} email the user's email
+   * @return {Promise<Array>} a list of change requests
+   */
+  async getMyPackages(userEmail) {
+    if (!userEmail) return [];
+
+    try {
+      return await API.get(
+        "changeRequestAPI",
+        `/getMyPackages?email=${userEmail}`
+      );
+    } catch (error) {
+      handleApiError(
+        error,
+        "FETCH_ERROR",
+        `There was an error fetching the states/territories for ${userEmail}.`
+      );
+    }
+  }
+}
+
+const instance = new PackageApi();
+Object.freeze(instance);
+
+export default instance;
