0701f438baf6dd2d5173fbb2f6fdae620a790c93
Haywood Wells <hwells@collabralink.com>
2021-08-18 15:22:28 -0700

    Fixed the code review issues in PR

--- services/uploads/src/antivirus.js
+++ services/uploads/src/antivirus.js
@@ -117,7 +117,7 @@ async function lambdaHandleEvent(event) {
 
   try {
     await s3.putObjectTagging(taggingParams).promise();
-    utils.generateSystemMessage("Tagging successful");
+    utils.generateSystemMessage("AV Scan Tagging successful");
     s3.putObjectAcl(aclParams, function(err, data) {
       if (err) console.log(err, err.stack); // an error occurred
       else     console.log(data);           // successful response
@@ -148,7 +148,7 @@ async function scanS3Object(s3ObjectKey, s3ObjectBucket) {
 
   try {
     await s3.putObjectTagging(taggingParams).promise();
-    utils.generateSystemMessage("Tagging successful");
+    utils.generateSystemMessage("AV Scan Tagging successful");
      //s3.putObjectAcl(aclParams, function(err, data) {
        //if (err) console.log(err, err.stack); // an error occurred
        //else     console.log(data);           // successful response
