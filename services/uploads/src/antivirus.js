07858ed9f5f3fd5446b4992b25b33c1ad4068efc
Haywood Wells <hwells@collabralink.com>
2021-08-18 19:38:11 -0700

    Reverting back code

--- services/uploads/src/antivirus.js
+++ services/uploads/src/antivirus.js
@@ -144,21 +144,16 @@ async function scanS3Object(s3ObjectKey, s3ObjectBucket) {
     Key: s3ObjectKey,
     Tagging: utils.generateTagSet(virusScanStatus),
   };
-
-  const aclParams = {
-    Bucket: s3ObjectBucket,
-    Key: s3ObjectKey,
-    ACL: 'public-read'
-  };
+  
 
 
   try {
     await s3.putObjectTagging(taggingParams).promise();
     utils.generateSystemMessage("AV Scan Tagging successful");
-     s3.putObjectAcl(aclParams, function(err, data) {
-       if (err) console.log(err, err.stack); // an error occurred
-       else     console.log(data);           // successful response
-    });
+     //s3.putObjectAcl(aclParams, function(err, data) {
+       //if (err) console.log(err, err.stack); // an error occurred
+       //else     console.log(data);           // successful response
+    //});
   } catch (err) {
     console.log(err);
   }
