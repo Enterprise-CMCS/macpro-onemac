6abc427d79eab84cc2d9369521172cac010a6205
Amy DiPasquale <adipasquale@fearless.tech>
2021-08-31 13:52:34 -0400

    OY2-10810 dashboard ests

---
+++ services/ui-src/src/libs/testDataSubmissions.js
@@ -0,0 +1,98 @@
+export const waiverSubmission = {
+  summary: "",
+  waiverAuthority: "1915(b)",
+  ninetyDayClockEnd: 1631626754502,
+  type: "waiver",
+  userId: "us-east-1:4267bada-207d-47f2-8cf9-e80150f3e670",
+  uploads: [
+    {
+      s3Key: "1623850751563/spacex2.jpeg",
+      filename: "spacex2.jpeg",
+      title:
+        "1915(b)(4) FFS Selective Contracting (Streamlined) waiver application pre-print (Initial, Renewal, Amendment)",
+      contentType: "image/jpeg",
+      url: "https://uploads-develop-attachments-116229642442.s3.us-east-1.amazonaws.com/protected/us-east-1%3A4267bada-207d-47f2-8cf9-e80150f3e670/1623850751563/spacex2.jpeg",
+    },
+  ],
+  createdAt: 1623850754118,
+  actionType: "new",
+  state: "submitted",
+  id: "3d2dee60-cea8-11eb-b2ab-f9a5ac19ffad",
+  transmittalNumber: "MI.77777",
+  submittedAt: 1623850754502,
+  user: {
+    authProvider:
+      "cognito-idp.us-east-1.amazonaws.com/us-east-1_KDU39SsRi,cognito-idp.us-east-1.amazonaws.com/us-east-1_KDU39SsRi:CognitoSignIn:3554f7a8-b563-4f7a-bc90-447d44ee3d82",
+    firstName: "Angie",
+    lastName: "Active",
+    id: "us-east-1:4267bada-207d-47f2-8cf9-e80150f3e670",
+    email: "statesubmitteractive@cms.hhs.local",
+  },
+  territory: "MI",
+};
+
+export const spaSubmission = {
+  summary: "zx",
+  waiverAuthority: "",
+  ninetyDayClockEnd: 1631545241170,
+  type: "spa",
+  userId: "us-east-1:4267bada-207d-47f2-8cf9-e80150f3e670",
+  uploads: [
+    {
+      s3Key:
+        "1623769134464/Making sense of MVP (Minimum Viable Product) - and why I prefer Earliest Testable_Usable_Lovable - Crisp's Blog.pdf",
+      filename:
+        "Making sense of MVP (Minimum Viable Product) - and why I prefer Earliest Testable_Usable_Lovable - Crisp's Blog.pdf",
+      title: "CMS Form 179",
+      contentType: "application/pdf",
+      url: "https://uploads-develop-attachments-116229642442.s3.us-east-1.amazonaws.com/protected/us-east-1%3A4267bada-207d-47f2-8cf9-e80150f3e670/1623769134464/Making%20sense%20of%20MVP%20%28Minimum%20Viable%20Product%29%20-%20and%20why%20I%20prefer%20Earliest%20Testable_Usable_Lovable%20-%20Crisp%27s%20Blog.pdf",
+    },
+  ],
+  createdAt: 1623769240885,
+  actionType: "",
+  state: "submitted",
+  id: "7381a650-cdea-11eb-9b12-99fa096a7e36",
+  transmittalNumber: "MI-11-1114",
+  submittedAt: 1623769241170,
+  user: {
+    authProvider:
+      "cognito-idp.us-east-1.amazonaws.com/us-east-1_KDU39SsRi,cognito-idp.us-east-1.amazonaws.com/us-east-1_KDU39SsRi:CognitoSignIn:3554f7a8-b563-4f7a-bc90-447d44ee3d82",
+    firstName: "Angie",
+    lastName: "Active",
+    id: "us-east-1:4267bada-207d-47f2-8cf9-e80150f3e670",
+    email: "statesubmitteractive@cms.hhs.local",
+  },
+  territory: "MI",
+};
+
+export const chipSpaSubmission = {
+  summary: "Testinggggg",
+  waiverAuthority: "",
+  ninetyDayClockEnd: 1627406407488,
+  type: "chipspa",
+  userId: "us-east-1:2df6698f-7718-4571-bcfc-c4a6dc833595",
+  uploads: [
+    {
+      s3Key: "1619630406661/sample copy.pdf",
+      filename: "sample copy.pdf",
+      title: "Cover Letter",
+      contentType: "application/pdf",
+      url: "https://uploads-develop-attachmentsbucket-10wg5kiraihu1.s3.amazonaws.com/protected/us-east-1%3A2df6698f-7718-4571-bcfc-c4a6dc833595/1619630406661/sample%20copy.pdf",
+    },
+  ],
+  createdAt: 1619630407128,
+  actionType: "",
+  state: "submitted",
+  id: "fa590580-a845-11eb-b4fb-770e1b508b91",
+  transmittalNumber: "MI-00-1234-CHIP",
+  submittedAt: 1619630407488,
+  user: {
+    authProvider:
+      "cognito-idp.us-east-1.amazonaws.com/us-east-1_KDU39SsRi,cognito-idp.us-east-1.amazonaws.com/us-east-1_KDU39SsRi:CognitoSignIn:e5fc502d-01cb-4523-aece-45fcb6eaa360",
+    firstName: "Randy",
+    lastName: "Revoked",
+    id: "us-east-1:2df6698f-7718-4571-bcfc-c4a6dc833595",
+    email: "statesubmitterrevoked@cms.hhs.local",
+  },
+  territory: "MI",
+};
