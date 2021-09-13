e83996c81783c1c5fe39b420e538a875a83e3899
Jesse James <jjames@fearless.tech>
2021-09-13 15:28:09 -0400

    rel changes

--- services/ui-src/src/components/Header.tsx
+++ services/ui-src/src/components/Header.tsx
@@ -129,7 +129,7 @@ export function Header() {
         href={ROUTES.FAQ}
         className={getActiveClass(currentRoute, RouteList.FAQ_TOP)}
         target="_blank"
-        rel="noreferrer"
+        rel="noreferrer noopener"
       >
         FAQ
       </a>
