b69306bccedf7fa6c884d4855829ef09feb52371
Kristin_Grue <k.grue@theta-llc.com>
2021-09-16 13:54:30 -0400

    a different way to mock

--- services/app-api/utils/newSubmission.test.js
+++ services/app-api/utils/newSubmission.test.js
@@ -1,20 +1,18 @@
 import dynamoDb from "../libs/dynamodb-lib";
+import { ChangeRequest } from "cmscommonlib";
 import newSubmission from "./newSubmission";
-
-jest.mock("cmscommonlib", () => ({
-  decodeId: () => {
-    return {
-      packageId: "MI-11-1112",
-      parentType: "spa",
-      componentId: "MI-11-1111",
-      componentType: "spa",
-      isNewPackage: true,
-    };
-  },
-}));
 
 jest.mock("../libs/dynamodb-lib");
 dynamoDb.put.mockResolvedValue({});
+
+jest.mock("cmscommonlib");
+ChangeRequest.decodeId.mockReturnValue({
+  packageId: "MI-11-1112",
+  parentType: "spa",
+  componentId: "MI-11-1111",
+  componentType: "spa",
+  isNewPackage: true,
+});
 
 describe("submissions are properly captured into the database", () => {
   it("recognizes a new Package", () => {
