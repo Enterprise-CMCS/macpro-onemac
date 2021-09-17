034bddb1ac74df6aa340ef8e44aa67d806a212fe
Ahmad Al Alousi <aalousi@collabralink.com>
2021-09-16 20:18:27 -0400

    Modifying structure

--- tests/cypress/support/index.js
+++ tests/cypress/support/index.js
@@ -12,12 +12,11 @@
 // You can read more here:
 // https://on.cypress.io/configuration
 // ***********************************************************
-
+{
+  require("cypress-xpath");
+}
 // Import commands.js using ES2015 syntax:
 import "./commands";
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
-{
-  require("cypress-xpath");
-}
