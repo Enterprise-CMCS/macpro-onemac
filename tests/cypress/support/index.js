d5adccd6bfa37e73d0b736de674a3264d42d95d8
Ahmad Al Alousi <aalousi@collabralink.com>
2021-09-19 19:16:17 -0400

    adding cypress job, commented out. Will be active once its ready.

--- tests/cypress/support/index.js
+++ tests/cypress/support/index.js
@@ -12,30 +12,10 @@
 // You can read more here:
 // https://on.cypress.io/configuration
 // ***********************************************************
-<<<<<<< HEAD
-<<<<<<< HEAD
-{
-  require("cypress-xpath");
-}
-=======
 
->>>>>>> b8967ae (fixing branch)
-=======
 {
   require("cypress-xpath");
 }
->>>>>>> 034bddb (Modifying structure)
+
 // Import commands.js using ES2015 syntax:
 import "./commands";
-
-// Alternatively you can use CommonJS syntax:
-// require('./commands')
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-{
-  require("cypress-xpath");
-}
->>>>>>> b8967ae (fixing branch)
-=======
->>>>>>> 034bddb (Modifying structure)
