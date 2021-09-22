dfc3df01ff524745b37cb07a2183da70387972de
George Kaplan <gkaplan@fearless.tech>
2021-09-22 17:12:15 -0400

    try once more to hit the diff coverage threshhold with Header tests

--- services/ui-src/src/components/Header.test.js
+++ services/ui-src/src/components/Header.test.js
@@ -116,3 +116,74 @@ describe("left side navigation", () => {
     }
   );
 });
+
+describe("right side account management", () => {
+  it.each(["Register", "Login"])(
+    "renders a %s button when not authenticated",
+    (name) => {
+      const history = createMemoryHistory();
+      render(
+        <Router history={history}>
+          <Header />
+        </Router>
+      );
+
+      expect(screen.getByText(name, { selector: "a" })).toBeVisible();
+    }
+  );
+
+  it.each(["Register", "Login"])(
+    "does not render a %s button when authenticated",
+    (name) => {
+      const history = createMemoryHistory();
+      render(
+        <AppContext.Provider
+          value={{
+            isAuthenticated: true,
+          }}
+        >
+          <Router history={history}>
+            <Header />
+          </Router>
+        </AppContext.Provider>
+      );
+
+      expect(screen.queryByText(name, { selector: "a" })).toBeNull();
+    }
+  );
+
+  it("renders a Log out button when authenticated but no user type is present", () => {
+    const history = createMemoryHistory();
+    render(
+      <AppContext.Provider
+        value={{
+          isAuthenticated: true,
+        }}
+      >
+        <Router history={history}>
+          <Header />
+        </Router>
+      </AppContext.Provider>
+    );
+
+    expect(screen.getByText("Log out", { selector: "a" })).toBeVisible();
+  });
+
+  it("does not render a plain Log out button when user is authenticated and has a type specified", () => {
+    const history = createMemoryHistory();
+    render(
+      <AppContext.Provider
+        value={{
+          isAuthenticated: true,
+          userProfile: { userData: { type: "statesubmitter" } },
+        }}
+      >
+        <Router history={history}>
+          <Header />
+        </Router>
+      </AppContext.Provider>
+    );
+
+    expect(screen.queryByText("Log out", { selector: "a" })).toBeNull();
+  });
+});
