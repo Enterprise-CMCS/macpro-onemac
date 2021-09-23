c0e920e6941b709abef48e21913ccdf6a2d0b3e1
Kristin_Grue <k.grue@theta-llc.com>
2021-09-23 11:52:14 -0400

    removed stub

--- services/common/changeRequest.js
+++ services/common/changeRequest.js
@@ -57,8 +57,6 @@ export const decodeId = (inId, inType) => {
   return returnInfo;
 };
 
-export const formatDynamoDbPut = () => {};
-
 const commonSubheaderMessage =
   "Once you submit this form, a confirmation email is sent to you and to CMS. CMS will use this content to review your package, and you will not be able to edit this form. If CMS needs any additional information, they will follow up by email.<b> If you leave this page, you will lose your progress on this form.</b>";
 
