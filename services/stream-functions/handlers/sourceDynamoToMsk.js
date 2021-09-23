35eea5d4f2ef52054fddf9562517f590485e1ff4
Kristin_Grue <k.grue@theta-llc.com>
2021-09-23 15:09:42 -0400

    updated source kafka

--- services/stream-functions/handlers/sourceDynamoToMsk.js
+++ services/stream-functions/handlers/sourceDynamoToMsk.js
@@ -16,7 +16,6 @@ const kafka = new Kafka({
 });
 
 const producer = kafka.producer();
-let connected = false;
 const signalTraps = ["SIGTERM", "SIGINT", "SIGUSR2", "beforeExit"];
 
 signalTraps.map((type) => {
@@ -29,7 +28,10 @@ signalTraps.map((type) => {
 
 const publish = async (onemacActions) => {
   await producer.connect();
-  await producer.sendBatch(onemacActions);
+  await producer.send({
+    topic: ONEMAC_TOPIC,
+    messages: onemacActions,
+  });
   await producer.disconnect();
 };
 
@@ -39,9 +41,6 @@ function myHandler(event) {
     return null;
   }
   
-  //map fields that don't directly correlate
-  //event = mapFields(event, callback);
-
   console.log('Received event:', JSON.stringify(event, null, 2));
 
   const onemacActions = [];
@@ -52,15 +51,7 @@ function myHandler(event) {
         console.log("that submission came from SEA Tool, don't send back: ", dbAction.dynamodb.NewImage);
       else {
         console.log("New oneMAC Record inserted?? ", JSON.stringify(dbAction.dynamodb.NewImage, null, 2));
-        onemacActions.push(
-          {
-            topic: ONEMAC_TOPIC,
-            messages: [
-              {
-                value: JSON.stringify(dbAction.dynamodb.NewImage, null, 2)
-              }
-            ]
-          });
+        onemacActions.push({value: JSON.stringify(dbAction.dynamodb.NewImage, null, 2)});
       }
     }
   });
@@ -69,113 +60,3 @@ function myHandler(event) {
 }
 
 exports.handler = myHandler;
-
-
-
-
-class KafkaSourceLib {
-  /*
-  Event types:
-  cmd – command; restful publish
-  cdc – change data capture; record upsert/delete in data store
-  sys – system event; send email, archive logs
-  fct – fact; user activity, notifications, logs
-
-  topicPrefix = "[data_center].[system_of_record].[business_domain].[event_type]";
-  version = "some version";
-  tables = [list of tables];
-  */
-
-  unmarshallOptions = {
-    convertEmptyValues: true,
-    wrapNumbers: true,
-  };
-
-  stringify(e, prettyPrint) {
-    if (prettyPrint === true) return JSON.stringify(e, null, 2);
-    return JSON.stringify(e);
-  }
-
-  determineTopicName(streamARN) {
-    for (const table of this.tables) {
-      if (streamARN.includes(`/${STAGE}-${table}/`)) return this.topic(table);
-    }
-  }
-
-  unmarshall(r) {
-    return AWS.DynamoDB.Converter.unmarshall(r, this.unmarshallOptions);
-  }
-
-  createPayload(record) {
-    return this.createDynamoPayload(record);
-  }
-
-  createDynamoPayload(record) {
-    const dynamodb = record.dynamodb;
-    const { eventID, eventName } = record;
-    const dynamoRecord = {
-      NewImage: this.unmarshall(dynamodb.NewImage),
-      OldImage: this.unmarshall(dynamodb.OldImage),
-      Keys: this.unmarshall(dynamodb.Keys),
-    };
-    return {
-      key: Object.values(dynamoRecord.Keys).join("#"),
-      value: this.stringify(dynamoRecord),
-      partition: 0,
-      headers: { eventID: eventID, eventName: eventName },
-    };
-  }
-
-  topic(t) {
-    if(this.version) {
-      return `${this.topicPrefix}.${t}.${this.version}`;
-    } else {
-      return `${this.topicPrefix}.${t}`;
-    }
-  }
-
-  createOutboundEvents(records) {
-    let outboundEvents = {};
-    for (const record of records) {
-      const topicName = this.determineTopicName(
-        String(record.eventSourceARN.toString())
-      );
-
-      const dynamoPayload = this.createPayload(record);
-
-      //initialize configuration object keyed to topic for quick lookup
-      if (!(outboundEvents[topicName] instanceof Object))
-        outboundEvents[topicName] = {
-          topic: topicName,
-          messages: [],
-        };
-
-      //add messages to messages array for corresponding topic
-      outboundEvents[topicName].messages.push(dynamoPayload);
-    }
-    return outboundEvents;
-  }
-
-  async handler(event) {
-    if (!connected) {
-      await producer.connect();
-      connected = true;
-    }
-    console.log("Raw event", this.stringify(event, true));
-
-    if (event.Records) {
-      const outboundEvents = this.createOutboundEvents(event.Records);
-
-      const topicMessages = Object.values(outboundEvents);
-      console.log(
-        `Batch configuration: ${this.stringify(topicMessages, true)}`
-      );
-
-      await producer.sendBatch({ topicMessages });
-    }
-
-    console.log(`Successfully processed ${event.Records.length} records.`);
-  }
-}
-
-export default KafkaSourceLib;
