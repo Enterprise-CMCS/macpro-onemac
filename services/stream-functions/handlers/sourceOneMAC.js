f47b3bde79671d0f29fdb2a3328021fa66f12190
Kristin_Grue <k.grue@theta-llc.com>
2021-09-27 13:05:18 -0400

    use the new Kafka source library

--- services/stream-functions/handlers/sourceOneMAC.js
+++ services/stream-functions/handlers/sourceOneMAC.js
@@ -6,6 +6,29 @@ class SourceOneMAC extends KafkaSourceLib {
   tables = [
     "updates"
   ];
+
+  createOutboundEvents(inRecords) {
+    const records = [];
+    
+    inRecords.forEach( (dbAction) => {
+      if (dbAction.eventName === "INSERT") {
+        const oneMACSubmission = dbAction.dynamodb.NewImage;
+        if (oneMACSubmission.sk === "SEATool")
+          console.log("sourceOneMAC! that submission came from SEA Tool, don't send back: ", oneMACSubmission);
+        else {
+          console.log("sourceOneMAC! New oneMAC Record inserted?? ", JSON.stringify(oneMACSubmission, null, 2));
+          const toPublish = {
+            PackageID: oneMACSubmission.pk,
+            SubmitterName: oneMACSubmission.submitterName,
+            SubmitterEmail: oneMACSubmission.submitterEmail,
+            PackageType: ONEMAC_SUBMISSION_TYPE[oneMACSubmission.componentType],
+          }
+          records.push({value: JSON.stringify(toPublish)});
+        }
+      }
+    });
+    return super.createOutboundEvents(records);
+  }
 }
 
 const sourceOneMAC = new SourceOneMAC();
