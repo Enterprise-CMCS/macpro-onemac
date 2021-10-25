8232aab17887c0e4dae2a173f0d6da530e3919b3
aalousi <aalousi@collabralink.com>
2021-10-25 17:55:31 -0400

    faq tests

--- tests/cypress/support/pages/oneMacHomePage.js
+++ tests/cypress/support/pages/oneMacHomePage.js
@@ -1,12 +1,23 @@
 const DevelopmentLogin = "#devloginBtn";
+//xpath, use cy.xpath
+const FAQPage =
+  "//body/reference[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[2]/a[1]";
 
 export class oneMacHomePage {
   launch() {
-    cy.visit("https://d2dr7dgo9g0124.cloudfront.net/");
+    cy.visit("https://d1pfv1f0iwle3.cloudfront.net/");
   }
 
   clickDevelopmentLogin() {
     cy.get(DevelopmentLogin).click();
+  }
+
+  clickFAQPage() {
+    cy.xpath(FAQPage)
+      .invoke("attr", "href")
+      .then((href) => {
+        cy.visit(href);
+      });
   }
 }
 export default oneMacHomePage;
