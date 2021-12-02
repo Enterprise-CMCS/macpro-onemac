8374516ee48c9aa2cb8261a092119c142547a59f
zach lewis <zlewis@clarityinnovates.com>
2021-12-02 12:46:38 -0600

    system date

--- services/ui-src/src/utils/date-utils.test.js
+++ services/ui-src/src/utils/date-utils.test.js
@@ -2,7 +2,7 @@ import {formatDate, formatDateOnly} from "./date-utils";
 
 it("formats correctly", () => {
     const response = formatDate(1638462874431)
-    expect(response).toBe("Thu, Dec 2 2021, 10:34:34 AM")
+    expect(response).toBe("Thu, Dec 2 2021, 4:34:34 PM")
 
     const response2 = formatDateOnly(1638462874431)
     expect(response2).toBe("Thu, Dec 2 2021")
