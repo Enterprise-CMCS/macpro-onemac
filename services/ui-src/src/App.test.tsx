23ef56fb78d6822ee686bd8a26469a1d6a1f39d8
zach lewis <zlewis@clarityinnovates.com>
2021-12-02 17:00:38 -0600

    more test

--- services/ui-src/src/App.test.tsx
+++ services/ui-src/src/App.test.tsx
@@ -1,6 +1,5 @@
 import { render } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
-import { Auth } from "aws-amplify";
 
 import {App} from "./App";
 import React from "react";
---
+++ services/ui-src/src/libs/appContext.test.js
@@ -0,0 +1,7 @@
+
+const appContent = require("./appContent");
+
+it("formats correctly", () => {
+    expect(appContent.footerEmail).toBe("sample@cms.gov")
+})
+
