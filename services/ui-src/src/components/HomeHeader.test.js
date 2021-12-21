eca872a25dc450866380b22b3dbd86b969559f72
Jesse James <jjames@fearless.tech>
2021-12-21 14:59:43 -0500

    home header test add

--- services/ui-src/src/components/HomeHeader.test.js
+++ services/ui-src/src/components/HomeHeader.test.js
@@ -1,9 +1,21 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, act, screen, fireEvent } from "@testing-library/react";
 import HomeHeader from "./HomeHeader";
 
 describe("HomeHeader", () => {
   it("renders without crashing", () => {
     render(<HomeHeader />);
+  });
+
+  it("show/hide image when scrolling from top", () => {
+    render(<HomeHeader />);
+    const scrollMock = { addEventListener: jest.fn() };
+    const polygonGroupImage = screen.getByAltText("polygon-img");
+    expect(polygonGroupImage).toBeVisible();
+    jest.spyOn(React, "useEffect").mockImplementation(() => scrollMock);
+    act(() => {
+      fireEvent.scroll(global, { target: { scrollY: 700 } });
+    });
+    expect(scrollMock.addEventListener).toBeTruthy();
   });
 });
