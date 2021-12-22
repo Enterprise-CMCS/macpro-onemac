736bb77fac74bc7b62561fe8ea715fd7db592226
Jesse James <62292793+jjames521@users.noreply.github.com>
2021-12-22 09:35:20 -0500

    Home Header/Scroll To Top Unit Test 14173 (#566)
    * home header test add
    
    * changed title to better reflect the test logic
    
    * maybe a better test? hard to say
    
    * scroll to top unit test add
    
    Co-authored-by: Kristin_Grue <k.grue@theta-llc.com>

--- services/ui-src/src/components/HomeHeader.test.js
+++ services/ui-src/src/components/HomeHeader.test.js
@@ -1,9 +1,31 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, act, screen, fireEvent } from "@testing-library/react";
 import HomeHeader from "./HomeHeader";
 
 describe("HomeHeader", () => {
   it("renders without crashing", () => {
     render(<HomeHeader />);
+  });
+
+  it("image disappears with scrollTop too large", () => {
+    render(<HomeHeader />);
+    const polygonGroupImage = screen.getByAltText("polygon-img");
+    expect(polygonGroupImage).toBeVisible();
+    document.documentElement.scrollTop = 1000;
+    act(() => {
+      fireEvent.scroll(global, { target: { scrollY: 70 } });
+    });
+    expect(polygonGroupImage).not.toBeVisible();
+  });
+
+  it("image remains with scrollTop less than 500", () => {
+    render(<HomeHeader />);
+    const polygonGroupImage = screen.getByAltText("polygon-img");
+    expect(polygonGroupImage).toBeVisible();
+    document.documentElement.scrollTop = 100;
+    act(() => {
+      fireEvent.scroll(global, { target: { scrollY: 70 } });
+    });
+    expect(polygonGroupImage).toBeVisible();
   });
 });
