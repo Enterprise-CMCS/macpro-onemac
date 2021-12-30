3f973eb81a376cfb938677e5cb7b3d85c123161c
Jesse James <jjames@fearless.tech>
2021-12-29 19:55:35 -0500

    consolidated tests

--- services/ui-src/src/containers/StateSignup.test.js
+++ services/ui-src/src/containers/StateSignup.test.js
@@ -1,13 +1,12 @@
 import React from "react";
 
-import { render, screen, act } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { AppContext } from "../libs/contextLib";
 import { stateUserNoAuthState } from "../libs/testDataAppContext";
 import { createMemoryHistory } from "history";
 import { StateSignup } from "./StateSignup";
 import { Router, Route } from "react-router-dom";
 import { MemoryRouter } from "react-router-dom";
-import userEvent from "@testing-library/user-event";
 
 describe("StateSignup", () => {
   let history;
@@ -16,7 +15,7 @@ describe("StateSignup", () => {
     history = createMemoryHistory();
   });
 
-  it("locates role and defines it", () => {
+  it("renders user role and makes sure a list is visible", () => {
     const route = "mockPath/mocksignup/mockstate";
     history.push(route, { role: "State Submitter" });
 
@@ -38,26 +37,6 @@ describe("StateSignup", () => {
     expect(stateSignup).toBeVisible();
     const userRole = screen.getByText("State Submitter");
     expect(userRole).toBeVisible();
-  });
-
-  it("renders a state list array", () => {
-    const route = "mockPath/mocksignup/mockstate";
-    history.push(route, { role: "State Submitter" });
-
-    render(
-      <AppContext.Provider
-        value={{
-          ...stateUserNoAuthState,
-        }}
-      >
-        <Router history={history}>
-          <Route path="/mockPath/:signup/:State">
-            <StateSignup role="state submitter" />
-          </Route>
-        </Router>
-      </AppContext.Provider>,
-      { wrapper: MemoryRouter }
-    );
     const stateList = screen.getByRole("list");
     expect(stateList).toBeVisible();
     const stateOption = screen.getByRole("option", { name: "Alabama" });
