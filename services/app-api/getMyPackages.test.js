b3112be1a4029962df92ed840ca6ee7ae6a51e18
Kristin_Grue <k.grue@theta-llc.com>
2022-01-04 17:21:55 -0500

    start on getMyPackages test

--- services/app-api/getMyPackages.test.js
+++ services/app-api/getMyPackages.test.js
@@ -1,8 +1,28 @@
-import {main} from "./getMyPackages";
+import { main } from "./getMyPackages";
+import { RESPONSE_CODE } from "cmscommonlib";
+const expectedResponse = {
+  statusCode: 200,
+  body: RESPONSE_CODE.SUBMISSION_SUCCESS,
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Credentials": true,
+  },
+};
 
-it('Get Stub', async () => {
+const testUserEvent = {
+  queryStringParameters: {
+    email: "testEmail",
+  },
+};
 
-    const response =  main( {"source": "serverless-plugin-warmup"}, "foo")
-    expect(response).toBeInstanceOf(Promise)
+it(`returns an error if no user email is sent`, async () => {
+  expectedResponse.body = JSON.stringify(RESPONSE_CODE.USER_NOT_FOUND);
 
-})
+  testUserEvent.queryStringParameters.email = null;
+
+  expect(main(testUserEvent))
+    .resolves.toStrictEqual(expectedResponse)
+    .catch((error) => {
+      console.log("caught test error: ", error);
+    });
+});
