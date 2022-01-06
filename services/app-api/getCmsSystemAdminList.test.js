08444ffef6ddb8c5c0404c28c56763f18e670fd1
Jesse James <jjames@fearless.tech>
2022-01-06 17:20:43 -0500

    test get role and admin lists

--- services/app-api/getCmsSystemAdminList.test.js
+++ services/app-api/getCmsSystemAdminList.test.js
@@ -1,8 +1,72 @@
-import {main} from "./getCmsSystemAdminList";
+import { main } from "./getCmsSystemAdminList";
+import { queryForUserType } from "./libs/user-table-lib";
 
-it('Get Stub', async () => {
+jest.mock("./libs/user-table-lib");
 
-    const response =  main( {"source": "serverless-plugin-warmup"}, "foo")
-    expect(response).toBeInstanceOf(Promise)
+queryForUserType.mockImplementation(() => {
+  return [
+    {
+      id: "sabrina.mccrae@cms.hhs.gov",
+      firstName: "Sabrina",
+      lastName: "McCrae",
+    },
+    {
+      id: "systemadmintest@cms.hhs.local",
+      firstName: "Teresa",
+      lastName: "Test",
+    },
+    {
+      id: "systemadmin@nightwatch.test",
+      firstName: "Systemadmin",
+      lastName: "Nightwatch",
+    },
+    {
+      id: "ONEMACUSERCMS@gmail.com",
+      firstName: "NWCMSADMIN",
+      lastName: "ADMINUSER",
+    },
+  ];
+});
 
-})
+afterAll(() => {
+  jest.clearAllMocks();
+});
+
+it("gets list of system admins", async () => {
+  const emptyObject = {};
+  const expectedResponse = {
+    statusCode: 200,
+    body: JSON.stringify([
+      {
+        email: "sabrina.mccrae@cms.hhs.gov",
+        firstName: "Sabrina",
+        lastName: "McCrae",
+      },
+      {
+        email: "systemadmintest@cms.hhs.local",
+        firstName: "Teresa",
+        lastName: "Test",
+      },
+      {
+        email: "systemadmin@nightwatch.test",
+        firstName: "Systemadmin",
+        lastName: "Nightwatch",
+      },
+      {
+        email: "ONEMACUSERCMS@gmail.com",
+        firstName: "NWCMSADMIN",
+        lastName: "ADMINUSER",
+      },
+    ]),
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Credentials": true,
+    },
+  };
+
+  expect(main(emptyObject))
+    .resolves.toStrictEqual(expectedResponse)
+    .catch((error) => {
+      console.log("caught test error: ", error);
+    });
+});
