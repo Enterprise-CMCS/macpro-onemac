6f6211c74d195aa836b97ca152be9d46f9860569
Jesse James <jjames@fearless.tech>
2022-01-31 11:17:41 -0500

    Changes to table and resize response

--- services/ui-src/src/components/PortalTable.tsx
+++ services/ui-src/src/components/PortalTable.tsx
@@ -91,93 +91,72 @@ export default function PortalTable<V extends {} = {}>({
           TEMP_onReset={TEMP_onReset}
         />
       )}
-      <table className={props.className} {...getTableProps()}>
-        <thead>
-          {headerGroups.map((headerGroup) => (
-            <tr {...headerGroup.getHeaderGroupProps()}>
-              {expandable && <th />}
-              {headerGroup.headers.map((column) => (
-                <th
-                  // @ts-ignore FIXME remove when react-table types are improved
-                  {...column.getHeaderProps(column.getSortByToggleProps())}
-                  id={`${column.id}ColHeader`}
-                >
-                  {column.render("Header")}
-                  <span>
-                    {/* @ts-ignore FIXME remove when react-table types are improved */}
-                    {column.isSorted ? (
-                      // @ts-ignore FIXME remove when react-table types are improved
-                      column.isSortedDesc ? (
-                        <>
-                          {" "}
-                          <img
-                            src={Expand}
-                            alt="🔼"
-                            style={{ transform: "rotate(180deg)" }}
-                          />
-                        </>
+      <div className="table-wrapper">
+        <table className={props.className} {...getTableProps()}>
+          <thead>
+            {headerGroups.map((headerGroup) => (
+              <tr {...headerGroup.getHeaderGroupProps()}>
+                {headerGroup.headers.map((column) => (
+                  <th
+                    // @ts-ignore FIXME remove when react-table types are improved
+                    {...column.getHeaderProps(column.getSortByToggleProps())}
+                    id={`${column.id}ColHeader`}
+                  >
+                    {column.render("Header")}
+                    <span>
+                      {/* @ts-ignore FIXME remove when react-table types are improved */}
+                      {column.isSorted ? (
+                        // @ts-ignore FIXME remove when react-table types are improved
+                        column.isSortedDesc ? (
+                          <>
+                            {" "}
+                            <img
+                              src={Expand}
+                              alt="🔼"
+                              style={{ transform: "rotate(180deg)" }}
+                            />
+                          </>
+                        ) : (
+                          <>
+                            {" "}
+                            <img src={Expand} alt="🔽" />
+                          </>
+                        )
                       ) : (
-                        <>
-                          {" "}
-                          <img src={Expand} alt="🔽" />
-                        </>
-                      )
-                    ) : (
-                      ""
-                    )}
-                  </span>
-                </th>
-              ))}
-            </tr>
-          ))}
-        </thead>
-        {rows.length > 0 && (
-          <tbody {...getTableBodyProps()}>
-            {rows.map((row, rowIndex) => {
-              // @ts-ignore FIXME remove when react-table types are improved
-              prepareRow(row, rowIndex);
-              return (
-                <tr
-                  {...row.getRowProps()}
-                  className={cx({
-                    // @ts-ignore FIXME remove when react-table types are improved
-                    "parent-row-expanded": row.isExpanded,
-                    // @ts-ignore FIXME remove when react-table types are improved
-                    "child-row-expanded": row.depth > 0,
-                  })}
-                >
-                  {expandable && (
-                    <td className="row-expander-cell">
-                      {/* @ts-ignore */}
-                      {row.depth === 0 && (
-                        <button
-                          aria-label="Expand row"
-                          // @ts-ignore FIXME remove when react-table types are improved
-                          disabled={!row.canExpand}
-                          // @ts-ignore FIXME remove when react-table types are improved
-                          onClick={() => row.toggleRowExpanded()}
-                        >
-                          <FontAwesomeIcon icon={faChevronDown} />
-                        </button>
+                        ""
                       )}
-                    </td>
-                  )}
-                  {row.cells.map((cell, index) => {
-                    return (
-                      <td
-                        id={headerGroups[0].headers[index].id + "-" + rowIndex}
-                        {...cell.getCellProps()}
-                      >
-                        {cell.render("Cell")}
-                      </td>
-                    );
-                  })}
-                </tr>
-              );
-            })}
-          </tbody>
-        )}
-      </table>
+                    </span>
+                  </th>
+                ))}
+              </tr>
+            ))}
+          </thead>
+          {rows.length > 0 && (
+            <tbody {...getTableBodyProps()}>
+              {rows.map((row, rowIndex) => {
+                // @ts-ignore FIXME remove when react-table types are improved
+                prepareRow(row, rowIndex);
+                return (
+                  <tr {...row.getRowProps()}>
+                    {row.cells.map((cell, index) => {
+                      return (
+                        <td
+                          id={
+                            headerGroups[0].headers[index].id + "-" + rowIndex
+                          }
+                          {...cell.getCellProps()}
+                        >
+                          {cell.render("Cell")}
+                        </td>
+                      );
+                    })}
+                  </tr>
+                );
+              })}
+            </tbody>
+          )}
+        </table>
+      </div>
       {rows.length === 0 &&
         (preFilteredRows.length > 0 || preGlobalFilteredRows.length > 0) && (
           <div className="no-results" role="status">
