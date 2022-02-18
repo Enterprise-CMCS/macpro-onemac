349da4d0584aaa2c3af69b19f5327c829edc3488
Kristin Grue <k.grue@theta-llc.com>
2022-02-18 15:26:43 -0500

    OY2 - 16347 Temporary Extension form - Add warning messaging and relaxing the ID validation rules (#644)

--- services/ui-src/src/changeRequest/SubmissionForm.tsx
+++ services/ui-src/src/changeRequest/SubmissionForm.tsx
@@ -119,14 +119,17 @@ export const SubmissionForm: React.FC<{
       }
       // state code must be on the User's active state list
       else if (
-        newTransmittalNumber.length >= 2 &&
-        activeTerritories &&
-        !activeTerritories.includes(newTransmittalNumber.substring(0, 2))
+        (newTransmittalNumber.length >= 2 && !activeTerritories) ||
+        (activeTerritories &&
+          !activeTerritories.includes(
+            getTerritoryFromTransmittalNumber(newTransmittalNumber)
+          ))
       ) {
         errorMessage = `You can only submit for a state you have access to. If you need to add another state, visit your user profile to request access.`;
       }
       // must match the associated Regex string for format
       else if (
+        transmittalNumberDetails.idRegex &&
         !matchesRegex(newTransmittalNumber, transmittalNumberDetails.idRegex)
       ) {
         errorMessage = `The ${transmittalNumberDetails.idLabel} must be in the format of ${transmittalNumberDetails.idFormat}`;
@@ -218,7 +221,8 @@ export const SubmissionForm: React.FC<{
     try {
       if (
         formatMessage.statusMessage === "" &&
-        changeRequest.transmittalNumber
+        changeRequest.transmittalNumber &&
+        transmittalNumberDetails.idExistValidations
       ) {
         const promises = transmittalNumberDetails.idExistValidations.map(
           async (idExistValidation) => {
