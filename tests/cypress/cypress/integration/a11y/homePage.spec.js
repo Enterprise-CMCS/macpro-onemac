f1643e664b4920cf4cb84b43b7d3081b568baf22
aalousi <aalousi@collabralink.com>
2022-02-25 12:50:09 -0500

    adding click

--- tests/cypress/cypress/integration/a11y/homePage.spec.js
+++ tests/cypress/cypress/integration/a11y/homePage.spec.js
@@ -1,7 +1,7 @@
 describe("HomePage 508 test", () => {
   beforeEach(() => {
     cy.visit("/");
-    cy.get("#devloginBtn");
+    cy.get("#devloginBtn").click();
     cy.get("#email").type("statesubmitter@nightwatch.test");
     cy.get("#password").type("Passw0rd!");
     cy.get("#loginDevUserBtn").click();
