581cfbff0512ab4c70eabe4d6ac7f98a0286c0ba
aalousi <aalousi@collabralink.com>
2022-03-09 13:31:05 -0500

    adding 508 tests

---
+++ tests/cypress/cypress/integration/a11y/MedicaidSpaPage.spec.js
@@ -0,0 +1,19 @@
+describe("Medicaid SPA Page 508 test", () => {
+  beforeEach(() => {
+    cy.visit("/");
+    cy.get("#devloginBtn").click();
+    cy.get("#email").type("statesubmitter@nightwatch.test");
+    cy.get("#password").type("Passw0rd!");
+    cy.get("#loginDevUserBtn").click();
+    cy.xpath(
+      "//p[contains(text(),'Submit a new Medicaid & CHIP State Plan Amendments')]"
+    ).click();
+    cy.xpath(
+      "//body[1]/reference[1]/div[1]/div[1]/div[4]/ul[1]/li[1]/a[1]/div[1]/h4[1]"
+    ).click();
+  });
+
+  it("Check a11y on Medicaid SPA Page", () => {
+    cy.checkA11yOfPage();
+  });
+});
