eccc71dd284f918638b614809424731719a7c1f8
aalousi <aalousi@collabralink.com>
2022-03-09 14:05:19 -0500

    fixing broken tests for 508

--- tests/cypress/cypress/integration/a11y/CHIPSPAPage.spec.js
+++ tests/cypress/cypress/integration/a11y/CHIPSPAPage.spec.js
@@ -5,6 +5,7 @@ describe("Chip SPA Page 508 test", () => {
     cy.get("#email").type("statesubmitter@nightwatch.test");
     cy.get("#password").type("Passw0rd!");
     cy.get("#loginDevUserBtn").click();
+    cy.xpath("//a[@id='new-submission-button']").click();
     cy.xpath(
       "//p[contains(text(),'Submit a new Medicaid & CHIP State Plan Amendments')]"
     ).click();
