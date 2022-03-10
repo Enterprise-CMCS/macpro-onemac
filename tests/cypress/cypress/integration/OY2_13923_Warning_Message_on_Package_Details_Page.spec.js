4a45a74221c582229d3ab330684bcc5380d9da35
aalousi <aalousi@collabralink.com>
2022-03-10 13:43:59 -0500

    adding test files

---
+++ tests/cypress/cypress/integration/OY2_13923_Warning_Message_on_Package_Details_Page.spec.js
@@ -0,0 +1,11 @@
+describe("Warning Message on Package Details Page", () => {
+  beforeEach(() => {
+    cy.visit("/");
+    cy.get("#devloginBtn").click();
+    cy.get("#email").type("statesubmitter@nightwatch.test");
+    cy.get("#password").type("Passw0rd!");
+    cy.get("#loginDevUserBtn").click();
+  });
+
+  it("Check if warning message displays properly", () => {});
+});
