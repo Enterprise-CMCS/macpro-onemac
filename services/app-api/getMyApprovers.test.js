ef78f35e59557e167561113d50784982da7b43ac
Kristin_Grue <k.grue@theta-llc.com>
2022-03-28 11:28:10 -0400

    Unit Test cleanup

--- services/app-api/getMyApprovers.test.js
+++ services/app-api/getMyApprovers.test.js
@@ -38,3 +38,26 @@ it("gets approver list from role and territory", async () => {
       console.log("caught test error: ", error);
     });
 });
+
+it("catches any exception from dynamo query", async () => {
+  const eventObject = {
+    queryStringParameters: { role: "statesystemadmin", territory: "VA" },
+  };
+  const expectedResponse = {
+    statusCode: 500,
+    body: `{"error":"Cannot read property 'Items' of undefined"}`,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Credentials": true,
+    },
+  };
+  dynamoDb.query.mockImplementationOnce(() => {
+    throw new Error();
+  });
+
+  expect(main(eventObject))
+    .resolves.toStrictEqual(expectedResponse)
+    .catch((error) => {
+      console.log("caught test error: ", error);
+    });
+});
--- services/app-api/libs/dynamodb-lib.test.js
+++
@@ -1,7 +0,0 @@
-const dynamodblib = require('./dynamodb-lib')
-
-it('Get DocumentClient Stub', async () => {
-
-  expect(dynamodblib.default.get({})).toBeInstanceOf(Promise)
-
-});
