dd51593d8ddb7f89ed0c5d13d22727aff9e73bdd
Kristin_Grue <k.grue@theta-llc.com>
2022-03-31 15:57:58 -0400

    massaged dynamodb-lib for better test coverage

--- services/app-api/libs/dynamodb-lib.js
+++ services/app-api/libs/dynamodb-lib.js
@@ -1,18 +1,12 @@
 import AWS from "aws-sdk";
 
-//Setup the database connection for localhost when running in offline mode.
-let dbOptions = {};
-if (process.env.IS_OFFLINE) {
-  dbOptions = {
-    region: "localhost",
-    endpoint: "http://localhost:8000",
-    accessKeyId: "DEFAULT_ACCESS_KEY", //Default key for local testing.
-    secretAccessKey: "DEFAULT_SECRET", //Default secret for local testing.
-  };
-  console.log("Using database in localhost.");
-}
-
-const client = new AWS.DynamoDB.DocumentClient(dbOptions);
+const client = new AWS.DynamoDB.DocumentClient(
+  process.env.IS_OFFLINE
+    ? {
+        endpoint: "http://localhost:8000",
+      }
+    : {}
+);
 
 export default {
   get: (params) => client.get(params).promise(),
