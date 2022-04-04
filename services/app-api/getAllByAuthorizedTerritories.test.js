a048e07534b78deeaff2e1978cc63128e10c5adb
Kristin_Grue <k.grue@theta-llc.com>
2022-04-04 16:20:03 -0400

    multi-mocking fun

--- services/app-api/getAllByAuthorizedTerritories.test.js
+++ services/app-api/getAllByAuthorizedTerritories.test.js
@@ -171,3 +171,42 @@ it("Get Stub", async () => {
   const response = main({ source: "serverless-plugin-warmup" }, "foo");
   expect(response).toBeInstanceOf(Promise);
 });
+/*
+it("handles multiple scans", async () => {
+  getUser.mockResolvedValueOnce(testHelpdesk);
+  expectedResponse.statusCode = 200;
+  expectedResponse.body =
+    '[{"componentType":"waivernew","componentId":"VA.1117","submissionId":"9c5c8b70-53a6-11ec-b5bc-c9173b9fa278","currentStatus":"Package In Review","submitterId":"us-east-1:3211a6ff-043f-436b-8313-1b314582b2a5","submitterName":"Angie Active","submissionTimestamp":1638473560098,"submitterEmail":"statesubmitteractive@cms.hhs.local"},{"componentType":"spa","componentId":"VA-45-5913","submissionId":"cb9978d0-5dfb-11ec-a7a2-c5995198046c","currentStatus":"Disapproved","submitterId":"us-east-1:86a190fe-b195-42bf-9685-9761bf0ff14b","submitterName":"Statesubmitter Nightwatch","submissionTimestamp":1639609658284,"submitterEmail":"statesubmitter@nightwatch.test"},{"componentType":"chipspa","componentId":"VA-33-2244-CHIP","submissionId":"41103ac0-61aa-11ec-af2f-49cb8bfb8860","currentStatus":"Submitted","submitterId":"us-east-1:3211a6ff-043f-436b-8313-1b314582b2a5","submitterName":"Angie Active","submissionTimestamp":1640014441278,"submitterEmail":"statesubmitteractive@cms.hhs.local"}]';
+  const thisTestUserEvent = {
+    queryStringParameters: {
+      email: null,
+    },
+  };
+  dynamoDb.scan.mockResolvedValueOnce({
+    Items: [
+      {
+        componentType: "waivernew",
+        componentId: "VA.1117",
+      }
+    ],
+    Count: 1,
+    LastEvaluatedKey: 3,
+    ScannedCount: 1,
+  }).mockResolvedValueOnce({
+    Items: [
+      {
+        componentType: "waiverrenewal",
+        componentId: "VA.1117.R01",
+      }
+    ],
+    Count: 1,
+    ScannedCount: 1,
+  });
+
+  expect(main(thisTestUserEvent))
+  .resolves.toStrictEqual(expectedResponse)
+  .catch((error) => {
+    console.log("caught test error: ", error);
+  });
+});
+*/
