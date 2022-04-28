f521d690f6c1a39365ba2926d1771ef3e00f0d96
Kristin_Grue <k.grue@theta-llc.com>
2022-04-28 10:25:13 -0400

    turn off outgoing stream with debugging

--- services/stream-functions/handlers/sourceOneMAC.js
+++ services/stream-functions/handlers/sourceOneMAC.js
@@ -7,7 +7,12 @@ class SourceOneMAC extends KafkaSourceLib {
   determineTopicName(record) {
     const recordDetails = JSON.parse(record.value);
     const skSlice = JSON.stringify(recordDetails.NewImage.sk).slice(0,7);
-    if (record.headers.eventName === "INSERT" && skSlice !== "SEATool") return staticTopic;
+    if (record.headers.eventName === "INSERT" && skSlice !== "SEATool") {
+      console.log("Record Details being sent: ", recordDetails);
+      //return staticTopic;
+    } else {
+      console.log("Record Details being ignored: ", recordDetails);
+    }
     return null;
   }
 }
