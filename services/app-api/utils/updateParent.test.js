d7119da333e1015e25996eb11deb515484973542
Brandon Flynn <bflynn@collabralink.com>
2022-05-12 16:27:50 -0500

    updateParent test

--- services/app-api/utils/updateParent.test.js
+++ services/app-api/utils/updateParent.test.js
@@ -4,6 +4,25 @@ import updateParent from "./updateParent";
 jest.mock("../libs/dynamodb-lib");
 dynamoDb.update.mockResolvedValue({});
 
+const mockReturnedParent = {
+  Item: {
+    children: [
+      {
+        componentId: "componentId",
+        componentType: "componentType",
+        submissionTimestamp: "timestamp",
+      },
+    ],
+    sk: "",
+  },
+};
+
+const mockReturnedUpdate = {
+  Attributes: {
+    Latest: "1",
+  },
+};
+
 describe("parents can be updated", () => {
   it("calls the update", () => {
     const testData = {
@@ -14,7 +33,45 @@ describe("parents can be updated", () => {
       componentType: "componentType",
       currentStatus: "currentStatus",
     };
+    dynamoDb.update.mockResolvedValue(mockReturnedUpdate);
+    expect(updateParent(testData)).resolves.toBe(mockReturnedUpdate);
+  });
 
-    expect(updateParent(testData)).resolves.toBe({});
+  it("calls update children if found", () => {
+    dynamoDb.get.mockResolvedValue(mockReturnedParent);
+    dynamoDb.update.mockResolvedValue(mockReturnedUpdate);
+    const testData = {
+      submissionTimestamp: "timestamp",
+      parentId: "parentId",
+      parentType: "parentType",
+      componentId: "componentId",
+      componentType: "componentType",
+      currentStatus: "currentStatus",
+    };
+
+    expect(updateParent(testData)).resolves.toBe(mockReturnedUpdate);
+  });
+
+  it("catches update error and logs it", () => {
+    const testData = {
+      submissionTimestamp: "timestamp",
+      parentId: "parentId",
+      parentType: "parentType",
+      componentId: "componentId",
+      componentType: "componentType",
+      currentStatus: "currentStatus",
+    };
+    dynamoDb.update.mockResolvedValue(mockReturnedUpdate);
+    const mockError = new Error("Put error");
+    dynamoDb.put.mockImplementation(() => {
+      throw mockError;
+    });
+    const logSpy = jest.spyOn(console, "log");
+
+    expect(updateParent(testData)).resolves.toBe(mockReturnedUpdate);
+
+    // expect(logSpy).toHaveBeenCalledWith(
+    //   `Error happened updating DB:  ${mockError.message}`
+    // );
   });
 });
