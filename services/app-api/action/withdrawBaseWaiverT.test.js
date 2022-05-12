3fb77cbd574fc16fc650fc10b1509dc645b46cf2
Brandon Flynn <bflynn@collabralink.com>
2022-05-12 11:14:02 -0500

    more changeStatusAny tests

--- services/app-api/action/withdrawBaseWaiverT.test.js
+++ services/app-api/action/withdrawBaseWaiverT.test.js
@@ -1,6 +1,25 @@
 import { main } from "./withdrawBaseWaiver";
+import { changeStatusAny } from "./changeStatusAny";
 
-it("calls main", async () => {
-  const response = main({ source: "serverless-plugin-warmup" }, "foo");
+jest.mock("./changeStatusAny");
+
+const testEvent = {
+  this: "is an event object",
+};
+
+it("calls changeStatusAny", async () => {
+  const response = main(testEvent);
   expect(response).toBeInstanceOf(Promise);
+  expect(changeStatusAny).toHaveBeenCalled();
+});
+
+it("cathes logs and rethrows submit error", async () => {
+  const mockError = Error("changeStatusAny error");
+  changeStatusAny.mockImplementationOnce(() => {
+    throw mockError;
+  });
+  const logSpy = jest.spyOn(console, "log");
+  //console.log("Exception: ", Error("changeStatusAny error"));
+  expect(main(testEvent)).rejects.toThrow(mockError);
+  expect(logSpy).toHaveBeenCalledWith("Exception: ", mockError);
 });
