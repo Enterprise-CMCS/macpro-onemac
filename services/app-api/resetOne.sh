06b00c2766ba0b90471a4af203d4fc5d4f705712
Kristin_Grue <k.grue@theta-llc.com>
2022-05-17 11:06:41 -0400

    give permissions to execute resetOne

--- services/app-api/resetOne.sh
+++ services/app-api/resetOne.sh
@@ -2,7 +2,7 @@ stage=${1:-dev}
 
 # plan is to:
 # -  verify one table exists
-# -  download things we need to keep
+# -  download things we need to keep (maybe only in production)
 # -  create seed json
 # -  attach test seed data for branches using test data
 # -  destroy one table
