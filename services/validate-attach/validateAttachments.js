695230c2b83e9fc333ce29a778d06cfa9059539c
Brandon Flynn <bflynn@collabralink.com>
2022-05-20 10:21:34 -0500

    log out event in lambda

--- services/validate-attach/validateAttachments.js
+++ services/validate-attach/validateAttachments.js
@@ -1,11 +1,12 @@
 import handler from "./libs/handler-lib";
-import dynamoDb from "./libs/dynamodb-lib";
+//import dynamoDb from "./libs/dynamodb-lib";
 
 export const validate = async () => {
   console.log("Im validating......that I ran");
 };
 
 export const main = handler(async (event) => {
+  console.log("event", event);
   await validate();
   return "validated";
 });
