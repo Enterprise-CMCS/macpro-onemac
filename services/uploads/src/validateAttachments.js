ff7e3068c7e706c3f0e87ae2dffca7524a9f3ad3
Brandon Flynn <bflynn@collabralink.com>
2022-05-23 11:03:24 -0500

    temp function

--- services/uploads/src/validateAttachments.js
+++ services/uploads/src/validateAttachments.js
@@ -1,8 +1,8 @@
-import AWS from "aws-sdk";
+//import AWS from "aws-sdk";
 
-import { ATTACHMENTS_BUCKET } from "./constants";
+//import { ATTACHMENTS_BUCKET } from "./constants";
 
-const s3 = new AWS.S3();
+//const s3 = new AWS.S3();
 
 export async function validate(event) {
   console.log("In validate....", event);
