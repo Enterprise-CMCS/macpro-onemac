a445e5cdbafb710cd54654d4a8e7ba0c3d619477
Brandon Flynn <bflynn@collabralink.com>
2022-05-23 12:17:36 -0500

    first db test

--- services/uploads/src/validateAttachments.js
+++ services/uploads/src/validateAttachments.js
@@ -1,9 +1,25 @@
 //import AWS from "aws-sdk";
 
-//import { ATTACHMENTS_BUCKET } from "./constants";
+import { ATTACHMENTS_BUCKET } from "./constants";
+import dynamoDb from "../libs/dynamodb-lib";
 
 //const s3 = new AWS.S3();
 
 export async function validate(event) {
   console.log("In validate....", event);
+
+  const params = {
+    TableName: process.env.UPLOADS_TABLE_NAME,
+    Key: {
+      userId: "us-east-1:4480cb87-b4ab-4556-acef-d91f38901059",
+      id: "00748560-77be-11eb-8c50-7b02a5126084",
+    },
+  };
+
+  const result = await dynamoDb.get(params);
+  if (!result.Item) {
+    throw new Error("Item not found.");
+  }
+
+  console.log("retrieved record", result);
 }
