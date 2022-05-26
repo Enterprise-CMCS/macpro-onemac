78dbb357a337ecd1f8c3219821faa2250f4b919e
Kristin_Grue <k.grue@theta-llc.com>
2022-05-26 16:52:00 -0400

    Lambdas need delete role, add change-request deletions

--- services/app-api/resetData.js
+++ services/app-api/resetData.js
@@ -25,33 +25,67 @@ export const main = handler(async (event) => {
   console.log("resetData was called with event: ", event);
 
   const promiseItems = [];
-  resetIds.forEach(async (id) => {
-    const qParams = {
-      TableName: process.env.oneMacTableName,
-      KeyConditionExpression: "pk = :inPk",
-      ExpressionAttributeValues: {
-        ":inPk": id,
-      },
-      ProjectionExpression: "pk,sk",
-    };
 
-    const results = await dynamoDb.query(qParams);
-
-    for (const item of results.Items) {
-      promiseItems.push({
+  await Promise.all(
+    resetIds.map(async (id) => {
+      console.log("Checking for id: ", id);
+      const qParams = {
         TableName: process.env.oneMacTableName,
-        Key: {
-          pk: item.pk,
-          sk: item.sk,
+        KeyConditionExpression: "pk = :inPk",
+        ExpressionAttributeValues: {
+          ":inPk": id,
         },
-      });
-    }
-  });
+        ProjectionExpression: "pk,sk",
+      };
+      try {
+        const results = await dynamoDb.query(qParams);
+        console.log("Found these results in One Table: ", results);
+        for (const item of results.Items) {
+          promiseItems.push({
+            TableName: process.env.oneMacTableName,
+            Key: {
+              pk: item.pk,
+              sk: item.sk,
+            },
+          });
+        }
+      } catch (e) {
+        console.log("query error: ", e.message);
+      }
+
+      // scan changeRequest table
+      const scanparams = {
+        TableName: process.env.tableName,
+        // ProjectionExpression: "userId,id,transmittalNumber",
+        ExclusiveStartKey: null,
+      };
+      try {
+        do {
+          const newresults = await dynamoDb.scan(scanparams);
+          console.log("a page of scans");
+          for (const item of newresults.Items) {
+            if (item.transmittalNumber !== id) continue;
+            console.log("Found an entry with id: ", id);
+            promiseItems.push({
+              TableName: process.env.tableName,
+              Key: {
+                userId: item.userId,
+                id: item.id,
+              },
+            });
+          }
+          scanparams.ExclusiveStartKey = newresults.LastEvaluatedKey;
+        } while (scanparams.ExclusiveStartKey);
+      } catch (e) {
+        console.log("scan error: ", e.message);
+      }
+    })
+  );
 
   await Promise.all(
     promiseItems.map(async (deleteParams) => {
       try {
-        console.log(`Update Params are ${JSON.stringify(deleteParams)}`);
+        console.log(`Delete Params are ${JSON.stringify(deleteParams)}`);
 
         const result = await dynamoDb.delete(deleteParams);
         console.log("The deleted record: ", result);
