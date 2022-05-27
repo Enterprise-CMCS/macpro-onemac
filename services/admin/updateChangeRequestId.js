7fec9120d7f81019ce97686bb54a73d04f7f6a52
Brandon Flynn <bflynn@collabralink.com>
2022-05-27 15:43:31 -0500

    grr... linter... I blame Kristin!

--- services/admin/updateChangeRequestId.js
+++ services/admin/updateChangeRequestId.js
@@ -37,7 +37,7 @@ export async function main(event) {
   const results = await dynamoDb
     .query({
       TableName: process.env.tableName,
-      ProjectionExpression: "transmittalNumber,userId,id",
+      ProjectionExpression: "transmittalNumber,summary,userId,id",
       IndexName: "territory-submittedAt-index",
       KeyConditionExpression:
         "territory = :v_territory and submittedAt between :v_submittedAt and :v_submittedEnd",
@@ -56,20 +56,25 @@ export async function main(event) {
     throw new Error("Item not found.");
   } else if (results.Items.length > 1) {
     throw new Error("Multiple records found - can not do auto update");
-  } else if (results.Items[0].transmittalNumber != fromTransmittalNumber) {
+  } else if (
+    results.Items[0].transmittalNumber != process.env.fromTransmittalNumber
+  ) {
     throw new Error(
       "Incorrect Transmittal number found - can not do auto update"
     );
   }
 
-  //update the transmittalNumber to the input transmittalNumber
+  //update the transmittalNumber to the input transmittalNumber and append the additional info
   await dynamoDb
     .update({
       TableName: process.env.tableName,
       Key: { userId: results.Items[0].userId, id: results.Items[0].id },
-      UpdateExpression: "SET transmitttalNumber = :toTransmittalNumber",
+      UpdateExpression:
+        "SET transmitttalNumber = :toTransmittalNumber, summary = :toSummary",
       ExpressionAttributeValues: {
         ":toTransmittalNumber": process.env.toTransmittalNumber,
+        ":toSummary":
+          process.env.appendAdditionalInfo + " " + results.Items[0].summary,
       },
     })
     .promise();
