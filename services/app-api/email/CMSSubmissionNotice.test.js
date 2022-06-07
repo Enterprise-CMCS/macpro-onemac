526688b2bd75d5c1047908c320713f17dd72d60b
Brandon Flynn <bflynn@collabralink.com>
2022-06-07 14:55:08 -0500

    turn back on email tests

--- services/app-api/email/CMSSubmissionNotice.test.js
+++ services/app-api/email/CMSSubmissionNotice.test.js
@@ -4,6 +4,7 @@ it("builds the CMS Submission Notice Email", async () => {
   const testData = {
     submitterName: "name",
     componentId: "MI-11-1111-22",
+    proposedEffectiveDate: "2022-06-07",
   };
   const testConfig = {
     typeLabel: "Test Type",
@@ -11,5 +12,12 @@ it("builds the CMS Submission Notice Email", async () => {
   const warnings = [];
 
   const response2 = CMSSubmissionNotice(testData, testConfig, warnings);
-  //expect(response2.HTML.length).toBe(1158);
+
+  expect(response2.Subject).toBe(
+    testConfig.typeLabel + " " + testData.componentId + " Submitted"
+  );
+
+  expect(response2.HTML.includes(testData.componentId)).toBe(true);
+  expect(response2.HTML.includes(process.env.applicationEndpoint)).toBe(true);
+  expect(response2.HTML.length).toBe(1213);
 });
