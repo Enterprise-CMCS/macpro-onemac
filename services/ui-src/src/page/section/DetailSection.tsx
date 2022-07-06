0e12ab27b73ca76ab146bfb0f5083b6f72bd16ac
ZacharyRizer <zacharyrizer@gmail.com>
2022-07-06 12:51:58 -0400

    OY2-19034: RAI Responses changed to Formal RAI Responses

--- services/ui-src/src/page/section/DetailSection.tsx
+++ services/ui-src/src/page/section/DetailSection.tsx
@@ -185,7 +185,7 @@ export const DetailSection = ({
         </section>
         {detail.raiResponses && (
           <section className="detail-section">
-            <h2>RAI Responses</h2>
+            <h2>Formal RAI Responses</h2>
             <Accordion>
               {detail.raiResponses?.map((raiResponse, index) => {
                 return (
