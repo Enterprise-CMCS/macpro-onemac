4bfd37a5b224d33d9e61c427124c53bdefa6e13c
Kristin_Grue <k.grue@theta-llc.com>
2022-07-13 12:00:14 -0400

    send new Medicaid SPA submissions

--- services/stream-functions/handlers/sourceOneMAC.js
+++ services/stream-functions/handlers/sourceOneMAC.js
@@ -1,17 +1,39 @@
+//import { join } from "lodash";
 import KafkaSourceLib from "../libs/kafka-source-lib";
 
 const staticTopic = "aws.onemac.updates.cdc.one";
+
+const filterDetails = (inRecord) => {
+  const filteredRecord = {
+    NewImage: {
+    additionalInformation: inRecord.NewImage.additionalInformation,
+    componentType: inRecord.NewImage.componentType,
+    attachments: inRecord.NewImage.attachments,
+    componentId: inRecord.NewImage.componentId,
+    currentStatus: inRecord.NewImage.currentStatus,
+    submissionTimestamp: inRecord.NewImage.submissionTimestamp,
+    proposedEffectiveDate: inRecord.NewImage.proposedEffectiveDate,
+    submitterName: inRecord.NewImage.submitterName,
+    submitterEmail: inRecord.NewImage.submitterEmail
+    },
+    OldImage: {},
+    Keys: inRecord.Keys,
+  };
+
+  return JSON.stringify(filteredRecord);
+};
 
 class SourceOneMAC extends KafkaSourceLib {
 
   determineTopicName(record) {
     const recordDetails = JSON.parse(record.value);
-    const isNewMedicaidSPA = (JSON.stringify(recordDetails.NewImage.sk) === "v0#medicaidspa");
+    const isNewMedicaidSPA = (recordDetails.NewImage.sk === "v0#medicaidspa");
 
     if (record.headers.eventName === "INSERT" && isNewMedicaidSPA) {
-      console.log("Sending: ", recordDetails);
+      record.value = filterDetails(recordDetails);
+      console.log("Sending: ", record.value);
       console.log("would be sent to: ", staticTopic);
-      //return staticTopic;
+      return staticTopic;
     } else {
       console.log("Record Details being ignored: ", recordDetails);
     }
