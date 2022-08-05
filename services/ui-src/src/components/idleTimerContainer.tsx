b42428163e518a47cd6d1b64824d52b5a7e60db4
ZacharyRizer <zacharyrizer@gmail.com>
2022-08-04 21:01:24 -0400

    convert js to tsx file

--- services/ui-src/src/components/idleTimerContainer.tsx
+++ services/ui-src/src/components/idleTimerContainer.tsx
@@ -1,12 +1,16 @@
 import React, { useState, useEffect } from "react";
-import { useIdleTimer } from "react-idle-timer";
+import { useIdleTimer, IIdleTimer } from "react-idle-timer";
 import jwt_decode from "jwt-decode";
 
-const IdleTimerContainer = ({ isAuthenticated }) => {
-  const LOCAL_STORAGE_KEY = "accessToken";
-  const TOTAL_TIMEOUT_TIME = 60 * 60 * 1000; // default of 1 hour total
-  const PROMPT_TIME = 45 * 60 * 1000; // default of 45 minutes to warning
-  const LOGOUT_TIME = TOTAL_TIMEOUT_TIME - PROMPT_TIME; // default logout 15 minutes after warning
+interface idleTimerProps {
+  isAuthenticated: boolean;
+}
+
+const IdleTimerContainer = ({ isAuthenticated }: idleTimerProps) => {
+  const STORAGE_KEY: string = "accessToken";
+  const TOTAL_TIMEOUT_TIME: number = 60 * 60 * 1000; // default of 1 hour total
+  const PROMPT_TIME: number = 45 * 60 * 1000; // default of 45 minutes to warning
+  const LOGOUT_TIME: number = TOTAL_TIMEOUT_TIME - PROMPT_TIME; // default logout 15 minutes after warning
 
   const [promptTimeout, setPromptTimeout] = useState(PROMPT_TIME);
   const [logoutTimeout, setLogoutTimeout] = useState(LOGOUT_TIME);
@@ -20,7 +24,7 @@ const IdleTimerContainer = ({ isAuthenticated }) => {
     // Do some idle action like log out your user
   };
 
-  const idleTimer = useIdleTimer({
+  const idleTimer: IIdleTimer = useIdleTimer({
     onPrompt,
     onIdle,
     timeout: promptTimeout,
@@ -41,22 +45,21 @@ const IdleTimerContainer = ({ isAuthenticated }) => {
   }, [isAuthenticated]);
 
   const setTimeoutTimes = () => {
-    const accessTokenKey = Object.keys(localStorage).filter((k) =>
-      k.includes(LOCAL_STORAGE_KEY)
+    const accessTokenKey: string[] = Object.keys(localStorage).filter((k) =>
+      k.includes(STORAGE_KEY)
     );
-    const loginToken = accessTokenKey
-      ? localStorage.getItem(accessTokenKey)
-      : null;
+    const loginToken: string | null =
+      accessTokenKey && localStorage.getItem(accessTokenKey[0]);
     if (!loginToken) return;
 
-    const decodedToken = jwt_decode(loginToken);
-    const epochAuthTime = decodedToken?.auth_time;
+    const decodedToken: any = jwt_decode(loginToken);
+    const epochAuthTime: number | undefined = decodedToken?.auth_time;
     if (!epochAuthTime) return;
 
-    const authTime = new Date(epochAuthTime * 1000);
-    const currentTime = new Date();
-    const timeLoggedIn = currentTime - authTime; // in milliseconds
-    const timeLeft = TOTAL_TIMEOUT_TIME - timeLoggedIn;
+    const authTime: number = new Date(epochAuthTime * 1000).valueOf();
+    const currentTime: number = new Date().valueOf();
+    const timeLoggedIn: number = currentTime - authTime; // in milliseconds
+    const timeLeft: number = TOTAL_TIMEOUT_TIME - timeLoggedIn;
 
     // time has already expired for this session - logout user and redirect to home
     if (timeLeft <= 0) {
