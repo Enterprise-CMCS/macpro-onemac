745e496dce5fa03a251418e0c9aa4aa1a8a49e7e
Kristin_Grue <k.grue@theta-llc.com>
2022-08-16 15:42:28 -0400

    added unit test for validate

---
+++ services/app-api/validation/validateParentOfWaiverRenewal.test.js
@@ -0,0 +1,22 @@
+import { validateParentOfAny } from "./validateParentOfAny";
+import { main } from "./validateParentOfWaiverRenewal";
+
+jest.mock("./validateParentOfAny");
+validateParentOfAny.mockResolvedValue(true);
+
+const testEvent = {
+  this: "is an event object",
+};
+
+const expectedResponse = {
+  body: "true",
+  headers: {
+    "Access-Control-Allow-Credentials": true,
+    "Access-Control-Allow-Origin": "*",
+  },
+  statusCode: 200,
+};
+
+it("calls validateParentOfAny", async () => {
+  expect(main(testEvent)).resolves.toStrictEqual(expectedResponse);
+});
