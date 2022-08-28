d96b7387d8db46ce402f848c1c7a8e4f886ea5a2
ZacharyRizer <zacharyrizer@gmail.com>
2022-08-27 20:35:10 -0400

    adding some extra tests for the WaiverAppendixKDetail component

--- services/ui-src/src/page/waiver-appendix-k/WaiverAppendixKDetail.test.js
+++ services/ui-src/src/page/waiver-appendix-k/WaiverAppendixKDetail.test.js
@@ -2,6 +2,7 @@ import React from "react";
 import {
   render,
   screen,
+  waitFor,
   waitForElementToBeRemoved,
 } from "@testing-library/react";
 import { Router, Route } from "react-router-dom";
@@ -20,7 +21,7 @@ const testWaiverAppendixKDetail = require("./testWaiverAppendixKDetail");
 window.HTMLElement.prototype.scrollIntoView = jest.fn();
 
 describe("Waiver Appendix K Detail Tests", () => {
-  it("shows Waiver Appendix K Details", async () => {
+  it("WaiverAppendixKDetails renders", async () => {
     PackageApi.getDetail.mockResolvedValue(testWaiverAppendixKDetail);
 
     let history;
@@ -43,5 +44,94 @@ describe("Waiver Appendix K Detail Tests", () => {
 
     // wait for loading screen to disappear
     await waitForElementToBeRemoved(() => screen.getByTestId(LOADER_TEST_ID));
+  });
+
+  it("shows Package Details link in side nav", async () => {
+    PackageApi.getDetail.mockResolvedValue(testWaiverAppendixKDetail);
+
+    let history;
+    history = createMemoryHistory();
+    history.push("/detail/waiver-app-k/MD-13150.R00.01");
+
+    render(
+      <AppContext.Provider
+        value={{
+          ...stateSubmitterInitialAuthState,
+        }}
+      >
+        <Router history={history}>
+          <Route path="/detail/waiver-app-k/:componentId">
+            <WaiverAppendixKDetail />
+          </Route>
+        </Router>
+      </AppContext.Provider>
+    );
+
+    // wait for loading screen to disappear
+    await waitForElementToBeRemoved(() => screen.getByTestId(LOADER_TEST_ID));
+
+    await waitFor(() => {
+      expect(
+        screen.getByRole("link", { name: "Package Details" })
+      ).toBeInTheDocument();
+    });
+  });
+
+  it("shows Additional Information link in side nav", async () => {
+    PackageApi.getDetail.mockResolvedValue(testWaiverAppendixKDetail);
+
+    let history;
+    history = createMemoryHistory();
+    history.push("/detail/waiver-app-k/MD-13150.R00.01");
+
+    render(
+      <AppContext.Provider
+        value={{
+          ...stateSubmitterInitialAuthState,
+        }}
+      >
+        <Router history={history}>
+          <Route path="/detail/waiver-app-k/:componentId">
+            <WaiverAppendixKDetail />
+          </Route>
+        </Router>
+      </AppContext.Provider>
+    );
+
+    // wait for loading screen to disappear
+    await waitForElementToBeRemoved(() => screen.getByTestId(LOADER_TEST_ID));
+
+    await waitFor(() => {
+      expect(
+        screen.getByRole("link", { name: "Additional Information" })
+      ).toBeInTheDocument();
+    });
+  });
+
+  it("shows Attachments section", async () => {
+    PackageApi.getDetail.mockResolvedValue(testWaiverAppendixKDetail);
+
+    let history;
+    history = createMemoryHistory();
+    history.push("/detail/waiver-app-k/MD-13150.R00.01");
+
+    render(
+      <AppContext.Provider
+        value={{
+          ...stateSubmitterInitialAuthState,
+        }}
+      >
+        <Router history={history}>
+          <Route path="/detail/waiver-app-k/:componentId">
+            <WaiverAppendixKDetail />
+          </Route>
+        </Router>
+      </AppContext.Provider>
+    );
+
+    // wait for loading screen to disappear
+    await waitForElementToBeRemoved(() => screen.getByTestId(LOADER_TEST_ID));
+
+    screen.getByText("Attachments", { selector: "h2" });
   });
 });
