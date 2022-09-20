f96495e624797d5ed491bc94ff11e845960c5965
Kristin Grue <k.grue@theta-llc.com>
2022-09-20 15:28:13 -0400

    more test cleanup

--- services/ui-src/src/utils/ChangeRequestDataApi.test.js
+++ services/ui-src/src/utils/ChangeRequestDataApi.test.js
@@ -1,16 +1,38 @@
 const ChangeRequestDataApi = require("./ChangeRequestDataApi");
+import { Auth, API } from "aws-amplify";
+jest.mock("aws-amplify");
 
 it("exists without crashing", () => {
+  const apiObject = { key: "theKeyToTheUniverse" };
+  const testUser = {
+    signInUserSession: {
+      idToken: {
+        payload: {
+          email: "userEmail",
+          given_name: "Given",
+          family_name: "Family",
+        },
+      },
+    },
+  };
+
+  API.get.mockResolvedValue(apiObject);
+  API.post.mockResolvedValue(apiObject);
+  Auth.currentAuthenticatedUser.mockResolvedValue(testUser);
+
   const response = ChangeRequestDataApi.default.get("aa", "aa");
   expect(response)
-    .resolves.toBeInstanceOf(Promise)
+    .resolves.toBe(apiObject)
     .catch((error) => {
       console.log("caught test error: ", error);
     });
 
-  const response2 = ChangeRequestDataApi.default.submit("aa", []);
+  const response2 = ChangeRequestDataApi.default.submit({ type: "aa" }, [
+    { file: "One" },
+    { file: "Two" },
+  ]);
   expect(response2)
-    .resolves.toBeInstanceOf(Promise)
+    .resolves.toBe(apiObject)
     .catch((error) => {
       console.log("caught test error: ", error);
     });
@@ -18,14 +40,14 @@ it("exists without crashing", () => {
   const response3 =
     ChangeRequestDataApi.default.getAllByAuthorizedTerritories("aa");
   expect(response3)
-    .resolves.toBeInstanceOf(Promise)
+    .resolves.toBe(apiObject)
     .catch((error) => {
       console.log("caught test error: ", error);
     });
 
   const response4 = ChangeRequestDataApi.default.packageExists("aa");
   expect(response4)
-    .resolves.toBeInstanceOf(Promise)
+    .resolves.toBe(apiObject)
     .catch((error) => {
       console.log("caught test error: ", error);
     });
