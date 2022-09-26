a9f608c462f891a3d6daf3961d69d73211570550
Kristin Grue <k.grue@theta-llc.com>
2022-09-26 13:25:06 -0400

    no freezing

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ stage=${1:-dev}
 install_deps() {
   if [ "$CI" == "true" ]; then # If we're in a CI system
     if [ ! -d "node_modules" ]; then # If we don't have any node_modules (CircleCI cache miss scenario), run yarn install --frozen-lockfile.  Otherwise, we're all set, do nothing.
-      yarn install --frozen-lockfile
+      yarn install # --frozen-lockfile
     fi
   else # We're not in a CI system, let's yarn install
     yarn install
