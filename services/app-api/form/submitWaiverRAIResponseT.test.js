d0092e73b18a8e09f4ad1014132a0931cd2f9215
Kristin Grue <k.grue@theta-llc.com>
2022-09-30 11:18:35 -0400

    hah, a test was actually broken

--- services/app-api/form/submitWaiverRAIResponseT.test.js
+++ services/app-api/form/submitWaiverRAIResponseT.test.js
@@ -21,19 +21,3 @@ const expectedResponse = {
 it("calls submitAny", async () => {
   expect(main(testEvent)).resolves.toStrictEqual(expectedResponse);
 });
-
-it.each`
-  id                   | parentType
-  ${"MI.1234.R00.00"}  | ${Workflow.ONEMAC_TYPE.WAIVER_INITIAL}
-  ${"MI.1234.R01.00"}  | ${Workflow.ONEMAC_TYPE.WAIVER_RENEWAL}
-  ${"MI.1234.R00.M01"} | ${Workflow.ONEMAC_TYPE.WAIVER_AMENDMENT}
-  ${"MI.1234.R00.01"}  | ${Workflow.ONEMAC_TYPE.WAIVER_APP_K}
-`(
-  "returns parent type of $parentType when given id of $id",
-  ({ id, parentType }) => {
-    expect(waiverRAIResponseFormConfig.getParentInfo(id)).toStrictEqual([
-      id,
-      parentType,
-    ]);
-  }
-);
