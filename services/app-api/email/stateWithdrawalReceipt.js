718c96555f9856a115254c676c9bc13b5a32ab5d
Kristin Grue <k.grue@theta-llc.com>
2022-11-03 15:17:56 -0400

    turn the address entry into a function call

--- services/app-api/email/stateWithdrawalReceipt.js
+++ services/app-api/email/stateWithdrawalReceipt.js
@@ -1,5 +1,12 @@
 import { formatPackageDetails } from "./formatPackageDetails.js";
 
+export const getAllActiveStateUserEmailAddresses = (territory) => {
+  return [
+    `Kristin ${territory} Grue <k.grue.stateadmin@gmail.com>`,
+    `Kristin  ${territory} Grue2 <k.grue.stateuser@gmail.com>`,
+  ];
+};
+
 /**
  * Package withdrawal email to state user(s)
  * @param {Object} data from the package.
@@ -7,7 +14,7 @@ import { formatPackageDetails } from "./formatPackageDetails.js";
  * @returns {Object} email parameters in generic format.
  */
 export const stateWithdrawalReceipt = (data, config) => ({
-  ToAddresses: [`${data.submitterName} <${data.submitterEmail}>`],
+  ToAddresses: getAllActiveStateUserEmailAddresses(data.territory),
   Subject: `${config.typeLabel} Package ${data.componentId} Withdraw Request`,
   HTML: `
       <p>This is confirmation that you have requested to withdraw the package below. The package will no longer be considered for CMS review:</p>
