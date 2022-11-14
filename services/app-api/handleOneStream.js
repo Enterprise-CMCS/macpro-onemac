97d0f6f81d6afce7fc0e2aae86d447c3a7a65217
Kristin Grue <k.grue@theta-llc.com>
2022-11-14 15:32:18 -0500

    stream management needs to be in separate service

--- services/app-api/handleOneStream.js
+++
@@ -1,5 +0,0 @@
-import handler from "./libs/handler-lib";
-
-export const main = handler(async (event) => {
-  console.log("One Stream event: ", event);
-});
---
+++ services/one-stream/handleOneStream.js
@@ -0,0 +1,4 @@
+export const main = async (event, context) => {
+  console.log("One Stream event: ", event);
+  console.log("Context: ", context);
+};
