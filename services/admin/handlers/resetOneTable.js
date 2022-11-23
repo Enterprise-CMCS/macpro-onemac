60cef776e29c57c48b51f964a6484b4a491bf74e
Kristin Grue <k.grue@theta-llc.com>
2022-11-23 14:13:53 -0500

    add a way to clean out the oneTable

---
+++ services/admin/handlers/resetOneTable.js
@@ -0,0 +1,52 @@
+import AWS from "aws-sdk";
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient(
+  process.env.IS_OFFLINE
+    ? {
+        endpoint: "http://localhost:8000",
+      }
+    : {}
+);
+
+/**
+ * Use this reset to remove all current OneMAC packages from the Dashboard.
+ */
+
+export const main = async (event) => {
+  console.log("resetOneTable event: ", event);
+
+  const params = {
+    TableName: process.env.oneMacTableName,
+    ExclusiveStartKey: event.ExclusiveStartKey
+      ? JSON.parse(event.ExclusiveStartKey)
+      : null,
+  };
+  if (event.Limit) params.Limit = event.Limit;
+
+  do {
+    try {
+      console.log("params are: ", params);
+      const results = await dynamoDb.scan(params).promise();
+      await Promise.all(
+        results.Items.map(async (item) => {
+          if (!item.pk.includes("@")) {
+            await dynamoDb
+              .delete({
+                TableName: process.env.oneMacTableName,
+                Key: {
+                  pk: item.pk,
+                  sk: item.sk,
+                },
+              })
+              .promise();
+          }
+        })
+      );
+      params.ExclusiveStartKey = results.LastEvaluatedKey;
+    } catch (e) {
+      console.log("error! ", e);
+    }
+  } while (!params.Limit && params.ExclusiveStartKey);
+
+  return "Done at : " + JSON.stringify(params.ExclusiveStartKey);
+};
