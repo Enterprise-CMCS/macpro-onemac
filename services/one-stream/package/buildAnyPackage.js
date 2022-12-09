7fe7c690611c11f89594d1acbe9f6c66c8a0eea1
bflynn-collabralink <99831795+bflynn-collabralink@users.noreply.github.com>
2022-12-09 09:05:20 -0600

    Oy2 21885 - dynamodb-stream-local (#1058)
    * add serverless-offline and offline-dynamodb-stream capabilities
    
    * Updated readme
    
    * update config to be more adaptable for default dev stage vs custom stage
    
    * updated README, set defaults for ARNs to allow run local without first deployment to AWS
    
    * fix unused import

--- services/one-stream/package/buildAnyPackage.js
+++ services/one-stream/package/buildAnyPackage.js
@@ -4,17 +4,21 @@ import { dynamoConfig } from "cmscommonlib";
 
 const dynamoDb = new AWS.DynamoDB.DocumentClient(dynamoConfig);
 
+const oneMacTableName = process.env.IS_OFFLINE
+  ? process.env.localTableName
+  : process.env.oneMacTableName;
+
 export const buildAnyPackage = async (packageId, config) => {
   console.log("Building package: ", packageId);
   const queryParams = {
-    TableName: process.env.oneMacTableName,
+    TableName: oneMacTableName,
     KeyConditionExpression: "pk = :pk",
     ExpressionAttributeValues: {
       ":pk": packageId,
     },
   };
   const childrenParams = {
-    TableName: process.env.oneMacTableName,
+    TableName: oneMacTableName,
     IndexName: "GSI2",
     KeyConditionExpression: "GSI2pk = :pk",
     ExpressionAttributeValues: {
@@ -28,7 +32,7 @@ export const buildAnyPackage = async (packageId, config) => {
 
     const packageSk = `Package`;
     const putParams = {
-      TableName: process.env.oneMacTableName,
+      TableName: oneMacTableName,
       Item: {
         pk: packageId,
         sk: packageSk,
