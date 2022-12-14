c83bf3465c0ff4e890122b20ec528c479362973e
Valencia McMurray <vmcmurray@collabralink.com>
2022-12-14 09:22:24 -0500

    match text changes
    oops !

--- tests/cypress/support/pages/oneMacDashboardPage.js
+++ tests/cypress/support/pages/oneMacDashboardPage.js
@@ -24,7 +24,7 @@ const exportToEXcelCSVBTN = "#new-submission-button";
 const idNumberHeader = "#transmittalNumberColHeader";
 const typeHeader = "#typeColHeader";
 const stateHeader = "#territoryColHeader";
-const dateSubmittedHeader = "#submittedAtColHeader";
+const initialSubmissionDateHeader = "#submissionTimestampColHeader";
 const submittedByHeader = "#submitterColHeader";
 //Element is Xpath use cy.xpath instead of cy.get
 const packageTab = '//a[@id="packageListLink"]';
@@ -108,8 +108,8 @@ export class oneMacDashboardPage {
   verifyStateHeaderIsDisplayed() {
     cy.get(stateHeader).should("be.visible");
   }
-  verifyDateSubmittedHeaderIsDisplayed() {
-    cy.get(dateSubmittedHeader).should("be.visible");
+  verifyInitialSubmissionDateHeaderIsDisplayed() {
+    cy.get(initialSubmissionDateHeader).should("be.visible");
   }
   verifySubmittedByHeadersDisplayed() {
     cy.get(submittedByHeader).should("be.visible");
