7314ee80cd3fced53b155638642aeb6de604adcd
Kristin Grue <k.grue@theta-llc.com>
2023-01-13 11:38:31 -0500

    see if deploying is necessary

--- services/one-stream/handleOneStream.js
+++ services/one-stream/handleOneStream.js
@@ -8,18 +8,6 @@ import { buildWaiverAmendment } from "./package/buildWaiverAmendment";
 import { buildWaiverAppendixK } from "./package/buildWaiverAppendixK";
 import { buildWaiverExtension } from "./package/buildWaiverExtension";
 
-const BUILD_PACKAGE_CALL = {
-  [Workflow.ONEMAC_TYPE.CHIP_SPA]: buildChipSpa,
-  [Workflow.ONEMAC_TYPE.CHIP_SPA_RAI]: buildChipSpa,
-  [Workflow.ONEMAC_TYPE.MEDICAID_SPA]: buildMedicaidSpa,
-  [Workflow.ONEMAC_TYPE.MEDICAID_SPA_RAI]: buildMedicaidSpa,
-  [Workflow.ONEMAC_TYPE.WAIVER_INITIAL]: buildInitialWaiver,
-  [Workflow.ONEMAC_TYPE.WAIVER_RENEWAL]: buildWaiverRenewal,
-  [Workflow.ONEMAC_TYPE.WAIVER_APP_K]: buildWaiverAppendixK,
-  [Workflow.ONEMAC_TYPE.WAIVER_APP_K_RAI]: buildWaiverAppendixK,
-  [Workflow.ONEMAC_TYPE.WAIVER_EXTENSION]: buildWaiverExtension,
-  [Workflow.ONEMAC_TYPE.WAIVER_AMENDMENT]: buildWaiverAmendment,
-};
 export const main = async (eventBatch) => {
   console.log("One Stream event: ", eventBatch);
 
@@ -42,48 +30,24 @@ export const main = async (eventBatch) => {
 
         console.log("pk: %s sk: %s", inPK, inSK);
 
-        const [eventSource] = inSK.split("#");
+        const [eventSource, , offset] = inSK.split("#");
+        if (offset) {
+          console.log("%s ignoring old %s event: ", inPK, inSK, newEventData);
+          return;
+        }
+
         switch (eventSource) {
           case "Package":
             packageToBuild.type = newEventData?.parentType?.S;
             packageToBuild.id = newEventData?.parentId?.S;
             break;
           case "OneMAC":
-            // for all but Waiver RAIs, the type maps to the build
+            // RAIs build parent type, all else build themselves
             if (
               newEventData.componentType.S === Workflow.ONEMAC_TYPE.WAIVER_RAI
             )
               packageToBuild.type = newEventData?.parentType?.S;
             else packageToBuild.type = newEventData.componentType.S;
-            // switch (packageType) {
-            //   case ONEMAC_TYPE.MEDICAID_SPA:
-            //   case ONEMAC_TYPE.MEDICAID_SPA_RAI:
-            //     await buildMedicaidSpa(inPK);
-            //     break;
-            //   case ONEMAC_TYPE.CHIP_SPA:
-            //   case ONEMAC_TYPE.CHIP_SPA_RAI:
-            //     await buildChipSpa(inPK);
-            //     break;
-            //   case ONEMAC_TYPE.WAIVER_INITIAL:
-            //     await buildInitialWaiver(inPK);
-            //     break;
-            //   case ONEMAC_TYPE.WAIVER_AMENDMENT:
-            //     await buildWaiverAmendment(inPK);
-            //     break;
-            //   case ONEMAC_TYPE.WAIVER_RENEWAL:
-            //     await buildWaiverRenewal(inPK);
-            //     break;
-            //   case ONEMAC_TYPE.WAIVER_EXTENSION:
-            //     await buildWaiverExtension(inPK);
-            //     break;
-            //   case ONEMAC_TYPE.WAIVER_APP_K:
-            //   case ONEMAC_TYPE.WAIVER_APP_K_RAI:
-            //     await buildWaiverAppendixK(inPK);
-            //     break;
-            //   default:
-            //     console.log("type not handled");
-            //     break;
-            // }
             break;
           case "SEATool": {
             const [, topic] = newEventData.GSI1pk.S.split("#");
@@ -143,14 +107,39 @@ export const main = async (eventBatch) => {
           );
           return;
         }
-        if (typeof BUILD_PACKAGE_CALL[packageToBuild.type] === "function")
-          await BUILD_PACKAGE_CALL[packageToBuild.type](packageToBuild.id);
-        else
-          console.log(
-            `%s with type <%s> has no build package function??`,
-            inPK,
-            packageToBuild.type
-          );
+        switch (packageToBuild.type) {
+          case Workflow.ONEMAC_TYPE.CHIP_SPA:
+          case Workflow.ONEMAC_TYPE.CHIP_SPA_RAI:
+            buildChipSpa(packageToBuild.id);
+            break;
+          case Workflow.ONEMAC_TYPE.MEDICAID_SPA:
+          case Workflow.ONEMAC_TYPE.MEDICAID_SPA_RAI:
+            buildMedicaidSpa(packageToBuild.id);
+            break;
+          case Workflow.ONEMAC_TYPE.WAIVER_INITIAL:
+            buildInitialWaiver(packageToBuild.id);
+            break;
+          case Workflow.ONEMAC_TYPE.WAIVER_RENEWAL:
+            buildWaiverRenewal(packageToBuild.id);
+            break;
+          case Workflow.ONEMAC_TYPE.WAIVER_APP_K:
+          case Workflow.ONEMAC_TYPE.WAIVER_APP_K_RAI:
+            buildWaiverAppendixK(packageToBuild.id);
+            break;
+          case Workflow.ONEMAC_TYPE.WAIVER_EXTENSION:
+            buildWaiverExtension(packageToBuild.id);
+            break;
+          case Workflow.ONEMAC_TYPE.WAIVER_AMENDMENT:
+            buildWaiverAmendment(packageToBuild.id);
+            break;
+          default:
+            console.log(
+              `%s with type <%s> has no build package function??`,
+              inPK,
+              packageToBuild.type
+            );
+            break;
+        }
       } else {
         console.log(`skipping %s of: `, event.eventName, event.dynamodb);
       }
