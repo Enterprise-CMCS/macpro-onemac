d5845daff4a76dd973402c816904af024dcb9749
Kevin Haube <khaube@fearless.tech>
2023-03-15 14:21:41 -0400

    Clears store on logout/idle timeout

--- services/ui-src/src/utils/StorageKeys.ts
+++ services/ui-src/src/utils/StorageKeys.ts
@@ -6,3 +6,10 @@ export const LOCAL_STORAGE_TABLE_FILTERS_SPA =
   "onemac-SPA-tableFiltersSavedState";
 export const LOCAL_STORAGE_TABLE_FILTERS_WAIVER =
   "onemac-WAIVER-tableFiltersSavedState";
+
+export const clearTableStateStorageKeys = () => {
+  localStorage.removeItem(LOCAL_STORAGE_TABLE_FILTERS_SPA);
+  localStorage.removeItem(LOCAL_STORAGE_TABLE_FILTERS_WAIVER);
+  localStorage.removeItem(LOCAL_STORAGE_COLUMN_VISIBILITY_SPA);
+  localStorage.removeItem(LOCAL_STORAGE_COLUMN_VISIBILITY_WAIVER);
+};
