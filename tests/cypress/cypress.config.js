b8ba69744321629557711e093135aa0c373eacc8
Valencia McMurray <vmcmurray@collabralink.com>
2023-04-12 14:37:39 -0400

    cypress 12 upgrade
    I think I finally got it working!

---
+++ tests/cypress/cypress.config.js
@@ -0,0 +1,43 @@
+const { defineConfig } = require("cypress");
+const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
+const preprocessor = require("@badeball/cypress-cucumber-preprocessor");
+const createEsbuildPlugin = require("@badeball/cypress-cucumber-preprocessor/esbuild");
+
+async function setupNodeEvents(on, config) {
+  // This is required for the preprocessor to be able to generate JSON reports after each run, and more,
+  await preprocessor.addCucumberPreprocessorPlugin(on, config);
+
+  on(
+    "file:preprocessor",
+    createBundler({
+      plugins: [createEsbuildPlugin.default(config)],
+    })
+  );
+
+  // Make sure to return the config object as it might have been modified by the plugin.
+  return config;
+}
+module.exports = defineConfig({
+  redirectionLimit: 20,
+  retries: 2,
+  watchForFileChanges: true,
+  fixturesFolder: "fixtures",
+  screenshotsFolder: "screenshots",
+  videosFolder: "videos",
+  video: false,
+  downloadsFolder: "downloads",
+  defaultCommandTimeout: 70001,
+  viewportWidth: 1440,
+  viewportHeight: 900,
+  experimentalStudio: true,
+  types: ["cypress", "cypress-axe"],
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents,
+    baseUrl: "https://d2dr7dgo9g0124.cloudfront.net",
+    specPattern: ["cypress/e2e/**/*.feature", "cypress/e2e/**/*.spec.js"],
+    supportFile: "support/index.js",
+    stepDefinitions: ["cypress/e2e/common/steps.js"],
+  },
+});
