1c6f52451da1fbc989e6c837dee5ec0ca5fb5518
Kristin Grue <k.grue@theta-llc.com>
2023-05-02 15:54:34 -0400

    "" not the same as null

--- services/seatool-sink/handlers/seaToolEvent.js
+++ services/seatool-sink/handlers/seaToolEvent.js
@@ -11,7 +11,8 @@ function myHandler(event) {
   }
   console.log("Received event:", JSON.stringify(event, null, 2));
   const topicName = event.topic.replace(topicPrefix, "");
-  const value = event.value ? JSON.parse(event.value) : JSON.parse(event);
+  const value =
+    event.value !== "" ? JSON.parse(event.value) : JSON.parse(event);
   console.log(`Type: ${topicName} Topic: ${event.topic}`);
 
   const pk = event.key.toUpperCase();
