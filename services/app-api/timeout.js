9eb99ee050502b9f8cf58b0f634b7302f218939a
Brandon Flynn <bflynn@luckyrabbit.tech>
2023-05-02 22:38:46 -0500

    remove test function

--- services/app-api/timeout.js
+++
@@ -1,7 +0,0 @@
-import handler from "./libs/handler-lib";
-
-export const main = handler(async () => {
-  // wait for 10 seconds before exiting
-  await new Promise(resolve => setTimeout(resolve, 10000));
-  return "Done";
-});
