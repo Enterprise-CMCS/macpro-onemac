e1d8b3aea370ff38ff41bb65286baf5646413142
Kristin Grue <k.grue@theta-llc.com>
2023-05-22 13:07:05 -0400

    Lambda Refactor: Rebuild packages (#1252)

--- services/one-stream/rebuildPackages.js
+++ services/one-stream/rebuildPackages.js
@@ -4,63 +4,29 @@ import { dynamoConfig } from "cmscommonlib";
 
 const dynamoDb = new AWS.DynamoDB.DocumentClient(dynamoConfig);
 
-const gsi1pksToRebuild = [
+const topicsToRebuild = [
   "SEATool#Medicaid_SPA",
   "SEATool#CHIP_SPA",
   "SEATool#1915b_waivers",
   "SEATool#1915c_waivers",
 ];
 
-export const main = async () => {
-  // only run this if indicated in the deploy
-  if (!process.env.deployTrigger || process.env.deployTrigger === "none")
-    return;
+const oneMacTableName = process.env.IS_OFFLINE
+  ? process.env.localTableName
+  : process.env.oneMacTableName;
 
-  const toRebuild = [];
-
+export const main = async (event) => {
   await Promise.all(
-    gsi1pksToRebuild.map(async (gsi1pk) => {
-      // need the parameters inside the scope of the Promise
-      const queryGSI1Params = {
-        TableName: process.env.oneMacTableName,
-        IndexName: "GSI1",
-        KeyConditionExpression: "GSI1pk = :topic",
-        ExpressionAttributeValues: {
-          ":topic": gsi1pk,
+    topicsToRebuild.map(async (oneTopic) => {
+      const processParams = {
+        TableName: oneMacTableName,
+        Item: {
+          pk: "Process",
+          sk: oneTopic,
+          reason: event.reason,
         },
-        ProjectionExpression: "pk, sk",
       };
-
-      do {
-        console.log(`queryGSI1Params: %s`, queryGSI1Params);
-        const results = await dynamoDb.query(queryGSI1Params).promise();
-        console.log("results: ", results);
-        results.Items.forEach((item) => {
-          const timestamp = Number(item.sk.replace("SEATool#", ""));
-          if (!toRebuild[item.pk] || timestamp > toRebuild[item.pk])
-            toRebuild[item.pk] = timestamp;
-        });
-        queryGSI1Params.ExclusiveStartKey = results.LastEvaluatedKey;
-      } while (queryGSI1Params.ExclusiveStartKey);
-    })
-  );
-
-  console.log("toRebuild: ", toRebuild);
-  await Promise.all(
-    toRebuild.map(async (item) => {
-      await dynamoDb
-        .update({
-          TableName: process.env.oneMacTableName,
-          Key: {
-            pk: item.pk,
-            sk: item.sk,
-          },
-          UpdateExpression: "SET deployTrigger = :dt",
-          ExpressionAttributeValues: {
-            ":dt": process.env.deployTrigger,
-          },
-        })
-        .promise();
+      await dynamoDb.put(processParams).promise();
     })
   );
 };
