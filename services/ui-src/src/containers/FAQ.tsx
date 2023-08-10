c19009f44fbdbed824b2eb2d2f1402db75a35a8c
Kevin Haube <khaube@fearless.tech>
2023-08-10 14:19:12 -0400

    Refactor faq display and contact card

--- services/ui-src/src/containers/FAQ.tsx
+++ services/ui-src/src/containers/FAQ.tsx
@@ -4,6 +4,7 @@ import { helpDeskContact } from "../libs/helpDeskContact";
 import { oneMACFAQContent } from "../libs/faq/faqContent";
 
 import { Accordion, AccordionItem } from "@cmsgov/design-system";
+import { MACCard } from "../components/MACCard";
 
 const FAQ = () => {
   const [hash, setHash] = useState(window.location.hash.replace("#", ""));
@@ -34,68 +35,72 @@ const FAQ = () => {
     };
   }, [hash]);
 
-  const renderFAQ = () => {
-    return oneMACFAQContent.map((section, index) => (
-      <div key={index} className="faq-section">
-        <h2 className="topic-title">{section.sectionTitle}</h2>
-        <Accordion>
-          {section.qanda.map((questionAnswer, i) => (
-            <div key={i}>
-              <AccordionItem
-                id={questionAnswer.anchorText}
-                heading={questionAnswer.question}
-                buttonClassName="accordion-button"
-                contentClassName="accordion-content"
-              >
-                {questionAnswer.answerJSX}
-              </AccordionItem>
-              <hr></hr>
-            </div>
-          ))}
-        </Accordion>
-      </div>
-    ));
-  };
-
-  const infoDetails = [
-    {
-      label: "Phone Number",
-      linkType: "phone",
-      infoValue: helpDeskContact.phone,
-    },
-    { label: "Email", linkType: "mailto", infoValue: helpDeskContact.email },
-  ];
-
-  const renderInfo = () => {
+  const FAQList = () => {
     return (
-      <dl>
-        {infoDetails.map((detail, i) => (
-          <div key={i} className="faq-info-wrapper">
-            <dt>{detail.label}</dt>
-            <dd>
-              <a href={`${detail.linkType}:${detail.infoValue}`}>
-                {detail.infoValue}
-              </a>
-            </dd>
+      <div id="faq-list">
+        {oneMACFAQContent.map((section, index) => (
+          <div key={index} className="faq-section">
+            <h2 className="topic-title">{section.sectionTitle}</h2>
+            <Accordion>
+              {section.qanda.map((questionAnswer, i) => (
+                <div key={i}>
+                  <AccordionItem
+                    id={questionAnswer.anchorText}
+                    heading={questionAnswer.question}
+                    buttonClassName="accordion-button"
+                    contentClassName="accordion-content"
+                  >
+                    {questionAnswer.answerJSX}
+                  </AccordionItem>
+                  <hr></hr>
+                </div>
+              ))}
+            </Accordion>
           </div>
         ))}
-      </dl>
+      </div>
+    );
+  };
+
+  const Info = () => {
+    const infoDetails = [
+      {
+        label: "Phone Number",
+        linkType: "phone",
+        infoValue: helpDeskContact.phone,
+      },
+      { label: "Email", linkType: "mailto", infoValue: helpDeskContact.email },
+    ];
+    return (
+      <div id="question-list">
+        <dl>
+          {infoDetails.map((detail, i) => (
+            <div key={i} className="faq-info-wrapper">
+              <dt>{detail.label}</dt>
+              <dd>
+                <a href={`${detail.linkType}:${detail.infoValue}`}>
+                  {detail.infoValue}
+                </a>
+              </dd>
+            </div>
+          ))}
+        </dl>
+      </div>
     );
   };
 
   return (
     <div>
       <PageTitleBar heading="Frequently Asked Questions" />
-      <div className="form-container" id="top">
-        <div className="faq-card">
-          <aside id="faq-contact-info-box">
-            <div className="faq-border-box"></div>
-            <div className="faq-info-box">
-              <h2>OneMAC Help Desk Contact Info</h2>
-              {renderInfo()}
-            </div>
-          </aside>
-          <div className="faq-left-column">{renderFAQ()}</div>
+      <div className="faq-display" id="top">
+        <FAQList />
+        <div id="contact-card">
+          <MACCard
+            title="OneMAC Help Desk Contact Info"
+            childContainerClassName="ds-u-padding--4"
+          >
+            <Info />
+          </MACCard>
         </div>
       </div>
     </div>
