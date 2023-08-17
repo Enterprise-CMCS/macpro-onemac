cf08bcd8e250e9ca403a1db4733bb08c71f5cc0c
Valencia McMurray <vmcmurray@collabralink.com>
2023-08-17 09:25:55 -0400

    Add log task reference for testing
    - seems like some accessibility automation tests failed due to accessibility issues that they couldn't log because the task didn't exist. This change implements the log task.
    - also I noticed some accessibility tests were duplicates so I removed those two.

--- tests/cypress/cypress.config.js
+++ tests/cypress/cypress.config.js
@@ -13,6 +13,18 @@ async function setupNodeEvents(on, config) {
       plugins: [createEsbuildPlugin.default(config)],
     })
   );
+  on("task", {
+    log(message) {
+      console.log(message);
+
+      return null;
+    },
+    table(message) {
+      console.table(message);
+
+      return null;
+    },
+  });
 
   // Make sure to return the config object as it might have been modified by the plugin.
   return config;
