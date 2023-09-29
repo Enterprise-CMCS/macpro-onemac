31c03227aab5a63954c93bd8fd23a7a66ca87d42
bflynn-cms <bflynn@luckyrabbit.tech>
2023-09-28 20:49:14 -0500

    Oy2 23799 - add emails for subsequent submissions (#1368)
    * add emails for subsequent submissions
    
    * lint

---
+++ services/app-api/email/stateSubsequentSubmissionReceipt.test.js
@@ -0,0 +1,19 @@
+import { stateSubsequentSubmissionReceipt } from "./stateSubsequentSubmissionReceipt";
+
+it("builds the State Submission Receipt Email", async () => {
+  const testData = {
+    submitterName: "name",
+    submitterEmail: "theemail@email.com",
+    componentId: "MI-11-1111-22",
+    clockEndTimestamp: 1631626754502,
+  };
+  const testConfig = {
+    typeLabel: "Test Type",
+  };
+  try {
+    const response2 = stateSubsequentSubmissionReceipt(testData, testConfig);
+    expect(response2.HTML.length).toBe(999);
+  } catch (e) {
+    console.log("reeived error: ", e);
+  }
+});
