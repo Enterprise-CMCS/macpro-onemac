58d0eedfc78e9cab9c9b80cfa652902f85029747
Kristin Grue <k.grue@theta-llc.com>
2023-09-29 14:28:52 -0400

    PI01.02: 25703: MASTER-Fix Email Content for Medicaid SPA Submission Notifications (#1374)
    Co-authored-by: Valencia McMurray <vmcmurray@collabralink.com>

--- services/app-api/form/defaultFormConfig.js
+++ services/app-api/form/defaultFormConfig.js
@@ -38,3 +38,29 @@ export const defaultWithdrawConfig = {
     parentType: defaultParentType,
   },
 };
+
+export const medicaidSpaText = `<p>This response confirms the receipt of your State Plan Amendment (SPA or your response to a SPA Request for Additional Information (RAI)). You can expect a formal response to your submittal to be issued within 90 days, before %NINETYDAYS%.</p>
+<p>This mailbox is for the submittal of State Plan Amendments and non-web-based responses to Requests for Additional Information (RAI) on submitted SPAs only.  Any other correspondence will be disregarded.</p>
+<p>If you have questions or did not expect this email, please contact <a href="mailto:spa@cms.hhs.gov">SPA@CMS.HHS.gov</a>.</p>
+<p>Thank you!</p>`;
+export const medicaidSpaRAIText = `<p>This response confirms the receipt of your State Plan Amendment (SPA or your response to a SPA Request for Additional Information (RAI)). You can expect a formal response to your submittal to be issued within 90 days. To calculate the 90th day, please count the date of receipt as day zero. The 90th day will be 90 calendar days from that date.</p>
+<p>This mailbox is for the submittal of State Plan Amendments and non-web-based responses to Requests for Additional Information (RAI) on submitted SPAs only.  Any other correspondence will be disregarded.</p>
+<p>If you have any questions, please contact <a href="mailto:spa@cms.hhs.gov">spa@cms.hhs.gov</a> or your state lead.</p>
+<p>Thank you!</p>`;
+export const chipSpaText = `<p>This response confirms the receipt of your CHIP State Plan Amendment (CHIP SPA or your response to a SPA Request for Additional Information (RAI)). You can expect a formal response to your submittal from CMS at a later date.</p>
+<p>If you have questions or did not expect this email, please contact <a href="CHIPSPASubmissionMailBox@CMS.HHS.gov">CHIPSPASubmissionMailBox@CMS.HHS.gov</a>.</p>
+<p>Thank you!</p>`;
+export const chipSpaRAIText = `<p>This response confirms the receipt of your CHIP State Plan Amendment (SPA or your response to a SPA Request for Additional Information (RAI)).</p>
+<p>If you have any questions, please contact <a href="CHIPSPASubmissionMailBox@CMS.HHS.gov">CHIPSPASubmissionMailBox@CMS.HHS.gov</a> or your state lead.</p>
+<p>Thank you!</p>`;
+export const waiverActionText = `<p>This response confirms the receipt of your Waiver request or your response to a Waiver Request for Additional Information (RAI)). You can expect a formal response to your submittal to be issued within 90 days, before %NINETYDAYS%.</p>
+<p>This mailbox is for the submittal of Section 1915(b) and 1915(c) non-web-based Waivers, responses to Requests for Additional Information (RAI) on Waivers, and extension requests on Waivers only.  Any other correspondence will be disregarded.</p>
+<p>If you have any questions, please contact <a href="mailto:spa@cms.hhs.gov">spa@cms.hhs.gov</a> or your state lead.</p>
+<p>Thank you!</p>`;
+export const waiverRAIText = `<p>This response confirms the receipt of your Waiver request or your response to a Waiver Request for Additional Information (RAI)). You can expect a formal response to your submittal to be issued within 90 days. To calculate the 90th day, please count the date of receipt as day zero. The 90th day will be 90 calendar days from that date.</p>
+<p>This mailbox is for the submittal of Section 1915(b) and 1915(c) non-web-based Waivers, responses to Requests for Additional Information (RAI) on Waivers, and extension requests on Waivers only.  Any other correspondence will be disregarded.</p>
+<p>If you have any questions, please contact <a href="mailto:spa@cms.hhs.gov">spa@cms.hhs.gov</a> or your state lead.</p>
+<p>Thank you!</p>`;
+export const waiverExtensionText = `<p>This mailbox is for the submittal of Section 1915(b) and 1915(c) non-web-based Waivers, responses to Requests for Additional Information (RAI), and extension requests on Waivers only. Any other correspondence will be disregarded.</p>
+<p>If you have any questions, please contact <a href="mailto:spa@cms.hhs.gov">spa@cms.hhs.gov</a> or your state lead.</p>
+<p>Thank you!</p>`;
