89a375f9613801bd7881d7a47a8786c277ee2fde
Kristin Grue <k.grue@theta-llc.com>
2023-10-19 11:02:14 -0400

    deleting an event should rebuild the package

--- services/one-stream/handleOneStream.js
+++ services/one-stream/handleOneStream.js
@@ -21,132 +21,130 @@ export const main = async (eventBatch) => {
 
   await Promise.all(
     eventBatch.Records.map(async (event) => {
-      if (event.eventName === "REMOVE") return;
-      if (event.eventName === "INSERT" || event.eventName === "MODIFY") {
-        const newEventData = event.dynamodb.NewImage;
-        const inPK = newEventData.pk.S;
-        const inSK = newEventData.sk.S;
-        const packageToBuild = {
-          type: "",
-          id: inPK,
-        };
+      let newEventData;
+      if (event.eventName === "REMOVE") newEventData = event.dynamodb.OldImage;
+      if (event.eventName === "INSERT" || event.eventName === "MODIFY")
+        newEventData = event.dynamodb.NewImage;
 
-        const [eventSource, , offset] = inSK.split("#");
-        if (offset) {
-          console.log("%s ignoring old %s event: ", inPK, inSK, newEventData);
-          return;
-        }
+      const inPK = newEventData.pk.S;
+      const inSK = newEventData.sk.S;
+      const packageToBuild = {
+        type: "",
+        id: inPK,
+      };
 
-        switch (eventSource) {
-          case "Package":
+      const [eventSource, , offset] = inSK.split("#");
+      if (offset) {
+        console.log("%s ignoring old %s event: ", inPK, inSK, newEventData);
+        return;
+      }
+
+      switch (eventSource) {
+        case "Package":
+          packageToBuild.type = newEventData?.parentType?.S;
+          packageToBuild.id = newEventData?.parentId?.S;
+          break;
+        case "OneMAC":
+          if (buildParentPackageTypes.includes(newEventData.componentType.S))
             packageToBuild.type = newEventData?.parentType?.S;
-            packageToBuild.id = newEventData?.parentId?.S;
-            break;
-          case "OneMAC":
-            if (buildParentPackageTypes.includes(newEventData.componentType.S))
-              packageToBuild.type = newEventData?.parentType?.S;
-            else packageToBuild.type = newEventData.componentType.S;
-            break;
-          case "SEATool": {
-            const [, topic] = newEventData.GSI1pk.S.split("#");
-            let actionType;
-            if (!newEventData.ACTIONTYPES.NULL)
-              actionType = newEventData.ACTIONTYPES.L.map((oneType) =>
-                newEventData.STATE_PLAN.M.ACTION_TYPE.N ===
-                oneType.M.ACTION_ID.N
-                  ? oneType.M.ACTION_NAME.S
-                  : null
-              ).filter(Boolean)[0];
+          else packageToBuild.type = newEventData.componentType.S;
+          break;
+        case "SEATool": {
+          const [, topic] = newEventData.GSI1pk.S.split("#");
+          let actionType;
+          if (!newEventData.ACTIONTYPES.NULL)
+            actionType = newEventData.ACTIONTYPES.L.map((oneType) =>
+              newEventData.STATE_PLAN.M.ACTION_TYPE.N === oneType.M.ACTION_ID.N
+                ? oneType.M.ACTION_NAME.S
+                : null
+            ).filter(Boolean)[0];
 
-            switch (topic) {
-              case "Medicaid_SPA":
-                packageToBuild.type = Workflow.ONEMAC_TYPE.MEDICAID_SPA;
-                break;
-              case "CHIP_SPA":
-                packageToBuild.type = Workflow.ONEMAC_TYPE.CHIP_SPA;
-                break;
-              case "1915b_waivers":
-                if (actionType === "Renew")
-                  packageToBuild.type = Workflow.ONEMAC_TYPE.WAIVER_RENEWAL;
-                else if (actionType === "Amend")
-                  packageToBuild.type = Workflow.ONEMAC_TYPE.WAIVER_AMENDMENT;
-                else if (actionType === "New")
-                  packageToBuild.type = Workflow.ONEMAC_TYPE.WAIVER_INITIAL;
-                break;
-              case "1915c_waivers":
-                if (actionType === "Amend")
-                  packageToBuild.type = Workflow.ONEMAC_TYPE.WAIVER_APP_K;
-                else if (actionType === "Renew")
-                  packageToBuild.type = Workflow.ONEMAC_TYPE.WAIVER_RENEWAL;
-                else if (actionType === "New")
-                  packageToBuild.type = Workflow.ONEMAC_TYPE.WAIVER_INITIAL;
-                break;
-              default:
-                break;
-            }
-            break;
+          switch (topic) {
+            case "Medicaid_SPA":
+              packageToBuild.type = Workflow.ONEMAC_TYPE.MEDICAID_SPA;
+              break;
+            case "CHIP_SPA":
+              packageToBuild.type = Workflow.ONEMAC_TYPE.CHIP_SPA;
+              break;
+            case "1915b_waivers":
+              if (actionType === "Renew")
+                packageToBuild.type = Workflow.ONEMAC_TYPE.WAIVER_RENEWAL;
+              else if (actionType === "Amend")
+                packageToBuild.type = Workflow.ONEMAC_TYPE.WAIVER_AMENDMENT;
+              else if (actionType === "New")
+                packageToBuild.type = Workflow.ONEMAC_TYPE.WAIVER_INITIAL;
+              break;
+            case "1915c_waivers":
+              if (actionType === "Amend")
+                packageToBuild.type = Workflow.ONEMAC_TYPE.WAIVER_APP_K;
+              else if (actionType === "Renew")
+                packageToBuild.type = Workflow.ONEMAC_TYPE.WAIVER_RENEWAL;
+              else if (actionType === "New")
+                packageToBuild.type = Workflow.ONEMAC_TYPE.WAIVER_INITIAL;
+              break;
+            default:
+              break;
           }
-          default:
-            console.log("source %s unknown", eventSource);
-            packageToBuild.id = null;
-            break;
+          break;
         }
-        if (!packageToBuild.type || !packageToBuild.id) {
+        default:
+          console.log("source %s unknown", eventSource);
+          packageToBuild.id = null;
+          break;
+      }
+      if (!packageToBuild.type || !packageToBuild.id) {
+        console.log(
+          "%s did not map? source: %s packageToBuild: %s",
+          inPK,
+          eventSource,
+          JSON.stringify(packageToBuild, null, 2)
+        );
+        return;
+      }
+      switch (packageToBuild.type) {
+        case Workflow.ONEMAC_TYPE.CHIP_SPA:
+        case Workflow.ONEMAC_TYPE.CHIP_SPA_RAI:
+        case Workflow.ONEMAC_TYPE.CHIP_SPA_WITHDRAW:
+          await buildChipSpa(packageToBuild.id);
+          break;
+        case Workflow.ONEMAC_TYPE.MEDICAID_SPA:
+        case Workflow.ONEMAC_TYPE.MEDICAID_SPA_RAI:
+        case Workflow.ONEMAC_TYPE.MEDICAID_SPA_WITHDRAW:
+          await buildMedicaidSpa(packageToBuild.id);
+          break;
+        case Workflow.ONEMAC_TYPE.WAIVER_INITIAL:
+        case Workflow.ONEMAC_TYPE.WAIVER_INITIAL_WITHDRAW:
+          await buildInitialWaiver(packageToBuild.id);
+          break;
+        case Workflow.ONEMAC_TYPE.WAIVER_RENEWAL:
+        case Workflow.ONEMAC_TYPE.WAIVER_RENEWAL_WITHDRAW:
+          await buildWaiverRenewal(packageToBuild.id);
+          break;
+        case Workflow.ONEMAC_TYPE.WAIVER_APP_K:
+        case Workflow.ONEMAC_TYPE.WAIVER_APP_K_RAI:
+        case Workflow.ONEMAC_TYPE.WAIVER_APP_K_WITHDRAW:
+          await buildWaiverAppendixK(packageToBuild.id);
+          break;
+        case Workflow.ONEMAC_TYPE.WAIVER_EXTENSION:
+          await buildWaiverExtension(packageToBuild.id);
+          break;
+        case Workflow.ONEMAC_TYPE.WAIVER_EXTENSION_B:
+          await buildWaiverExtension1915b(packageToBuild.id);
+          break;
+        case Workflow.ONEMAC_TYPE.WAIVER_EXTENSION_C:
+          await buildWaiverExtension1915c(packageToBuild.id);
+          break;
+        case Workflow.ONEMAC_TYPE.WAIVER_AMENDMENT:
+        case Workflow.ONEMAC_TYPE.WAIVER_AMENDMENT_WITHDRAW:
+          await buildWaiverAmendment(packageToBuild.id);
+          break;
+        default:
           console.log(
-            "%s did not map? source: %s packageToBuild: %s",
+            `%s with type <%s> has no build package function??`,
             inPK,
-            eventSource,
-            JSON.stringify(packageToBuild, null, 2)
+            packageToBuild.type
           );
-          return;
-        }
-        switch (packageToBuild.type) {
-          case Workflow.ONEMAC_TYPE.CHIP_SPA:
-          case Workflow.ONEMAC_TYPE.CHIP_SPA_RAI:
-          case Workflow.ONEMAC_TYPE.CHIP_SPA_WITHDRAW:
-            await buildChipSpa(packageToBuild.id);
-            break;
-          case Workflow.ONEMAC_TYPE.MEDICAID_SPA:
-          case Workflow.ONEMAC_TYPE.MEDICAID_SPA_RAI:
-          case Workflow.ONEMAC_TYPE.MEDICAID_SPA_WITHDRAW:
-            await buildMedicaidSpa(packageToBuild.id);
-            break;
-          case Workflow.ONEMAC_TYPE.WAIVER_INITIAL:
-          case Workflow.ONEMAC_TYPE.WAIVER_INITIAL_WITHDRAW:
-            await buildInitialWaiver(packageToBuild.id);
-            break;
-          case Workflow.ONEMAC_TYPE.WAIVER_RENEWAL:
-          case Workflow.ONEMAC_TYPE.WAIVER_RENEWAL_WITHDRAW:
-            await buildWaiverRenewal(packageToBuild.id);
-            break;
-          case Workflow.ONEMAC_TYPE.WAIVER_APP_K:
-          case Workflow.ONEMAC_TYPE.WAIVER_APP_K_RAI:
-          case Workflow.ONEMAC_TYPE.WAIVER_APP_K_WITHDRAW:
-            await buildWaiverAppendixK(packageToBuild.id);
-            break;
-          case Workflow.ONEMAC_TYPE.WAIVER_EXTENSION:
-            await buildWaiverExtension(packageToBuild.id);
-            break;
-          case Workflow.ONEMAC_TYPE.WAIVER_EXTENSION_B:
-            await buildWaiverExtension1915b(packageToBuild.id);
-            break;
-          case Workflow.ONEMAC_TYPE.WAIVER_EXTENSION_C:
-            await buildWaiverExtension1915c(packageToBuild.id);
-            break;
-          case Workflow.ONEMAC_TYPE.WAIVER_AMENDMENT:
-          case Workflow.ONEMAC_TYPE.WAIVER_AMENDMENT_WITHDRAW:
-            await buildWaiverAmendment(packageToBuild.id);
-            break;
-          default:
-            console.log(
-              `%s with type <%s> has no build package function??`,
-              inPK,
-              packageToBuild.type
-            );
-            break;
-        }
-      } else {
-        console.log(`skipping %s of: `, event.eventName, event.dynamodb);
+          break;
       }
     })
   );
