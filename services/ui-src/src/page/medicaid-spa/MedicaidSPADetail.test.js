9f752087f134fb965d28bb4ae13b03f0bed33ef8
bflynn-cms <bflynn@luckyrabbit.tech>
2023-12-11 09:59:47 -0600

    Oy2 26709 (#1424)
    * Updated status map to also account for substatus
    
    While I think having the status map include the substatuses is fine, it would also be prudent given a longer time horizon to rebaseline the actual status as well. However not a big deal since that is the purpose of the map, you can have  a different backend status vs what shows to the user.
    
    * set mapped status after call to actionDelegate
    
    * update all old statuses and migrate

--- services/ui-src/src/page/medicaid-spa/MedicaidSPADetail.test.js
+++ services/ui-src/src/page/medicaid-spa/MedicaidSPADetail.test.js
@@ -171,7 +171,7 @@ describe("Detail View Tests", () => {
 
     PackageApi.getDetail.mockResolvedValue({
       ...testDetail,
-      currentStatus: "RAI Response Withdraw Enabled",
+      currentStatus: "Withdraw Formal RAI Response Enabled",
       actions: ["Withdraw Formal RAI Response"],
     });
 
