f27d628ae6ea4726f095d79f87ab822f02d60f68
Kristin Grue <k.grue@theta-llc.com>
2024-01-11 11:47:40 -0500

    add an email service - put the entire SNS->Lambda in it for now

---
+++ services/email/handlers/processEmails.js
@@ -0,0 +1,9 @@
+console.log("Loading processEmails");
+
+export const main = async (event, context, callback) => {
+  console.log("Received event:", JSON.stringify(event, null, 4));
+
+  var message = event.Records[0].Sns.Message;
+  console.log("Message received from SNS:", message);
+  callback(null, "Success");
+};
