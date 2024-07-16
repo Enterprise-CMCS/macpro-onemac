2580b14a392b0036ad2a21228abc1db84100f27d
Andie Swift <andie.swift5@gmail.com>
2024-07-16 13:48:27 -0500

    added conditional text on the package details page for sub subs

--- services/ui-src/src/page/section/AdditionalInfoSection.tsx
+++ services/ui-src/src/page/section/AdditionalInfoSection.tsx
@@ -3,18 +3,27 @@ import React, { FC } from "react";
 
 export const AdditionalInfoSection: FC<{
   additionalInfo: string;
+  isSubSub?: boolean;
   id?: string;
-}> = ({ additionalInfo, id = "addl-info-base" }) => {
+}> = ({ additionalInfo, isSubSub, id = "addl-info-base" }) => {
   return (
     <>
       <section id={id} className="detail-section">
-        <h2>Additional Information</h2>
+        <h2>
+          {isSubSub
+            ? "Reason for subsequent submission"
+            : "Additional Information"}
+        </h2>
         <Review
           className="original-review-component preserve-spacing"
           headingLevel="2"
         >
+          {/* for sub subs' reasons MUST be submitted but just in case I added this switch */}
           {additionalInfo || (
-            <i>No Additional Information has been submitted.</i>
+            <i>
+              No {isSubSub ? "Reason" : "Additional Information"} has been
+              submitted.
+            </i>
           )}
         </Review>
       </section>
