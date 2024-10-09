2329d5c07583d70e9277d952cd45b903a3c0ff52
Alex Youssefinia <ayoussefinia@fearless.tech>
2024-10-09 12:37:38 -0500

    modify deploy script

--- deploy.sh
+++ deploy.sh
@@ -15,8 +15,10 @@ install_deps() {
 deploy() {
   pushd services/$1
   install_deps
-  touch ./services/source/handlers/dummyHandler.js
-  echo "exports.handler = async (event) => {   console.log('Dummy handler executed', event);  return { statusCode: 200, body: JSON.stringify({ message: 'Hello from dummyHandler!' }),};};" > ./services/source/handlers/dummyHandler.js
+  cd ./services/source/handlers/
+  touch dummyHandler.js
+  echo "exports.handler = async (event) => {   console.log('Dummy handler executed', event);  return { statusCode: 200, body: JSON.stringify({ message: 'Hello from dummyHandler!' }),};};" > dummyHandler.js
+  cd ../../../
   serverless deploy --stage $stage --force
   popd
 }
