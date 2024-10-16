80443dd4486a9fa46de9d8be2977cf7390fb99fd
Alex Youssefinia <ayoussefinia@fearless.tech>
2024-10-16 10:03:42 -0500

    added space where needed

--- services/ui-src/src/libs/formLib.tsx
+++ services/ui-src/src/libs/formLib.tsx
@@ -180,7 +180,7 @@ export const defaultSubsequentSubmissionIntroJSX = (
       submit this form, a confirmation email is sent to you and to CMS. CMS will
       use this content to review your package, and you will not be able to edit
       this form. If CMS needs any additional information, they will follow up by
-      email.
+      email. 
       <b>If you leave this page, you will lose your progress on this form.</b>
     </p>
   </>
