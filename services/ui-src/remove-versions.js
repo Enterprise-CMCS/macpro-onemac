157665d4f5342fe82cafa15aa508a66f564f9680
Alex Youssefinia <ayoussefinia@fearless.tech>
2024-10-16 08:19:16 -0500

    added remove versions file that gets run after build

---
+++ services/ui-src/remove-versions.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+const path = require('path');
+
+// Define the directory of the build output
+const buildDir = path.join(__dirname, 'build', 'static', 'js');
+
+// Define the regular expression to match the version pattern
+const regex = /\b\d+\.\d+\.\d+\b/g;
+
+// Function to remove the version from files
+const removeVersionFromFiles = (dir) => {
+  fs.readdir(dir, (err, files) => {
+    if (err) throw err;
+
+    files.forEach(file => {
+      const filePath = path.join(dir, file);
+
+      // Check if the file is a JavaScript file
+      if (file.endsWith('.js')) {
+        fs.readFile(filePath, 'utf8', (err, data) => {
+          if (err) throw err;
+
+          // Replace the version pattern
+          const result = data.replace(regex, '');
+
+          // Write the modified content back to the file
+          fs.writeFile(filePath, result, 'utf8', (err) => {
+            if (err) throw err;
+            console.log(`Removed version from ${file}`);
+          });
+        });
+      }
+    });
+  });
+};
+
+removeVersionFromFiles(buildDir);
