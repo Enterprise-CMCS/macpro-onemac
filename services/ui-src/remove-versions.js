78d0f2e3796dab470bf4e75c913cf9791a10fa35
Alex Youssefinia <ayoussefinia@fearless.tech>
2024-10-16 22:28:32 -0500

    4.xx.yy where xx is any number 17 or above

--- services/ui-src/remove-versions.js
+++ services/ui-src/remove-versions.js
@@ -4,8 +4,9 @@ const path = require('path');
 // Define the directory of the build output
 const buildDir = path.join(__dirname, 'build', 'static', 'js');
 
-// Define the regular expression to match the version pattern 4.17.xx
-const regex = /4\.17\.\d{2}/g; // Matches versions like 4.17.01, 4.17.99
+// Define the regular expression to match the version pattern 4.xx.yy
+// where xx is 17 or above and yy is any two-digit number
+const regex = /4\.(1[7-9]|[2-9][0-9])\.\d{2}/g; // Matches versions like 4.17.01, 4.25.99
 
 // Function to remove the version from files
 const removeVersionFromFiles = (dir) => {
