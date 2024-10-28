a3ad273c38ca9757bbf2590f87fb0f442c1b9540
Alex Youssefinia <ayoussefinia@fearless.tech>
2024-10-28 14:05:39 -0500

    correct misname

--- services/ui-src/src/containers/FAQ.tsx
+++ services/ui-src/src/containers/FAQ.tsx
@@ -10,7 +10,7 @@ import {
 import { Accordion, AccordionItem, Button } from "@cmsgov/design-system";
 import { MACCard } from "../components/MACCard";
 import { withLDProvider, useFlags} from 'launchdarkly-react-client-sdk';
-const clientId = process.env.AUNCH_DARKLY_CLIENT_ID;
+const clientId = process.env.REACT_APP_LD_CLIENT_ID;
 console.log("CLIENT_ID="+clientId)
 
 /** Refactored out for later extraction by cms-ux-lib. However, using this
