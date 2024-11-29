e479739621ffa6504408fcdd167aa275967b4a1d
Alex Youssefinia <ayoussefinia@fearless.tech>
2024-11-29 14:44:32 -0600

    modify lambda response for version 2

--- services/admin/handlers/addPropertyToJWT.js
+++ services/admin/handlers/addPropertyToJWT.js
@@ -3,12 +3,14 @@ exports.main = async (event) => {
 
     // Proceed with modifying the claims if needed
     if (event.request.userAttributes['custom:cms_roles'] === 'statesystemadmin') {
+
         event.response = event.response || {};
-        event.response.claimsOverrideDetails = event.response.claimsOverrideDetails || {};
-        event.response.claimsOverrideDetails.claimsToAddOrOverride = event.response.claimsOverrideDetails.claimsToAddOrOverride || {};
-        
+        event.response.claimsAndScopeOverrideDetails = event.response.claimsAndScopeOverrideDetails || {};
+        event.response.claimsAndScopeOverrideDetails.idTokenGeneration = event.response.claimsAndScopeOverrideDetails.idTokenGeneration || {};
+        event.response.claimsAndScopeOverrideDetails.idTokenGeneration.claimsToAddOrOverride = event.response.claimsAndScopeOverrideDetails.idTokenGeneration.claimsToAddOrOverride || {};
+
         // Add custom claim
-        event.response.claimsOverrideDetails.claimsToAddOrOverride['user_type'] = 'admin';
+        event.response.claimsAndScopeOverrideDetails.idTokenGeneration.claimsToAddOrOverride['user_type'] = 'admin';
     }
 
     // Log modified claims
