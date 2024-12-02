dae1ad522f369ca406375f17d9289fb85f34ee98
Alex Youssefinia <ayoussefinia@fearless.tech>
2024-12-02 13:19:23 -0600

    log user role in lambda trigger

--- services/admin/handlers/addPropertyToJWT.js
+++ services/admin/handlers/addPropertyToJWT.js
@@ -1,6 +1,16 @@
-const handler = async (event) => {
+// import dynamoDb from "../../app-api/libs/dynamodb-lib"
+import { getUser } from "../../app-api/getUser";
 
+
+const handler = async (event) => {
     console.log("JWT claims before modification:", JSON.stringify(event));
+    const userEmail = event.request.userAttributes.email;
+    console.log("User email:", userEmail);x
+
+    getUser(userEmail).then((user)=>{
+        console.log("***** USER", user)
+        console.log("role list:::", user.roleList)
+    });
 
     // Proceed with modifying the claims if needed
     if (event.request.userAttributes['custom:cms_roles'] === 'statesystemadmin') {
