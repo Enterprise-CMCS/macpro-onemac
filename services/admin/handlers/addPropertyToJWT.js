21b496cad440c6010b10398504dd80383ff01bae
Alex Youssefinia <ayoussefinia@fearless.tech>
2024-12-02 13:29:27 -0600

    remove lint error

--- services/admin/handlers/addPropertyToJWT.js
+++ services/admin/handlers/addPropertyToJWT.js
@@ -5,7 +5,7 @@ import { getUser } from "../../app-api/getUser";
 const handler = async (event) => {
     console.log("JWT claims before modification:", JSON.stringify(event));
     const userEmail = event.request.userAttributes.email;
-    console.log("User email:", userEmail);x
+    console.log("User email:", userEmail);
 
     getUser(userEmail).then((user)=>{
         console.log("***** USER", user)
@@ -13,14 +13,14 @@ const handler = async (event) => {
     });
 
     // Proceed with modifying the claims if needed
-    if (event.request.userAttributes['custom:cms_roles'] === 'statesystemadmin') {
+    // if (event.request.userAttributes['custom:cms_roles'] === 'statesystemadmin') {
 
-        event.response = event.response || {};
-        event.response.claimsOverrideDetails = event.response.claimsOverrideDetails || {};
-        event.response.claimsOverrideDetails.claimsToAddOrOverride = event.response.claimsOverrideDetails.claimsToAddOrOverride || {};
-        event.response.claimsOverrideDetails.claimsToAddOrOverride['user_type'] = 'admin';
+    event.response = event.response || {};
+    event.response.claimsOverrideDetails = event.response.claimsOverrideDetails || {};
+    event.response.claimsOverrideDetails.claimsToAddOrOverride = event.response.claimsOverrideDetails.claimsToAddOrOverride || {};
+    event.response.claimsOverrideDetails.claimsToAddOrOverride['user_type'] = 'admin';
         // Add custom claim
-    }
+    // }
 
     // Log modified claims
     console.log("JWT claims after modification:", JSON.stringify(event));
