2574960ed342a008113d28974101da53c670ff57
Alex Youssefinia <ayoussefinia@fearless.tech>
2024-12-11 22:16:34 -0600

    added batch function

---
+++ services/admin/handlers/batchUpdateCognitoUsers.js
@@ -0,0 +1,57 @@
+import AWS from "aws-sdk";
+const cognito = new AWS.CognitoIdentityServiceProvider();
+import { getUser } from "../../app-api/getUser";
+
+async function processCognitoUsers() {
+  const userPoolId = process.env.USER_POOL_ID;
+  console.log("user pool id: ", userPoolId)
+  let paginationToken = null;
+  
+  do {
+    const params = {
+      UserPoolId: userPoolId,
+      AttributesToGet: ['email'],
+      PaginationToken: paginationToken
+    };
+    
+    const listUsersResponse = await cognito.listUsers(params).promise();
+    
+    for (const user of listUsersResponse.Users) {
+      const emailAttribute = user.Attributes.find(attr => attr.Name === 'email');
+      if (emailAttribute) {
+        const userEmail = emailAttribute.Value;
+        
+        try {
+          const externalUser = await getUser(userEmail);
+          const roles = externalUser.roleList.map(role => role.role);
+          
+          await updateUserAttribute(userPoolId, user.Username, roles);
+        } catch (error) {
+          console.error(`Error processing user ${userEmail}:`, error);
+        }
+      }
+    }
+    
+    paginationToken = listUsersResponse.PaginationToken;
+  } while (paginationToken);
+}
+
+async function updateUserAttribute(userPoolId, username, roles) {
+  const params = {
+    UserPoolId: userPoolId,
+    Username: username,
+    UserAttributes: [
+      {
+        Name: 'custom:user_roles',
+        Value: JSON.stringify(roles)
+      }
+    ]
+  };
+  
+  await cognito.adminUpdateUserAttributes(params).promise();
+}
+
+export const main = async (event) => {
+    processCognitoUsers().catch(console.error);
+}
+
