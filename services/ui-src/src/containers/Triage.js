ece0cd3e8b0552800173d583aa419cd1e47bb768
Andie Swift <andie.swift5@gmail.com>
2024-12-30 09:38:46 -0600

    added mmdlNotification flag to hide/show the cards related to mmdl

--- services/ui-src/src/containers/Triage.js
+++ services/ui-src/src/containers/Triage.js
@@ -4,16 +4,26 @@ import { useLocation } from "react-router-dom";
 import PageTitleBar from "../components/PageTitleBar";
 import { choicesFromRoute } from "../libs/triageChoices";
 import { MACFieldsetCard, MACFieldsetCardOption } from "../components/MACCard";
+import { useFlags } from "launchdarkly-react-client-sdk";
 
 const Triage = () => {
   const location = useLocation();
   const [triageData, setTriageData] = useState(
     choicesFromRoute[location.pathname]
   );
+  const { mmdlNotification } = useFlags();
 
   useEffect(() => {
-    setTriageData(choicesFromRoute[location.pathname]);
-  }, [location]);
+    const tempTriageData = choicesFromRoute[location.pathname];
+    // if the mmdl Flag is set to true, cards related to MMDL will not appear
+    if (mmdlNotification) {
+      tempTriageData.choices = tempTriageData.choices.filter(
+        (choice) => !choice.description.includes("MMDL")
+      );
+      console.log("andie after", tempTriageData);
+    }
+    setTriageData(tempTriageData);
+  }, [location, mmdlNotification]);
 
   return (
     <>
