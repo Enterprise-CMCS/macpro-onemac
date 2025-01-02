b4e64dbb5c65085b813e8008d2b5be08e477ca6d
Andie Swift <andie.swift5@gmail.com>
2025-01-02 13:57:16 -0600

    lint fix

--- services/ui-src/src/containers/Triage.js
+++ services/ui-src/src/containers/Triage.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useMemo } from "react";
+import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
 import PageTitleBar from "../components/PageTitleBar";
